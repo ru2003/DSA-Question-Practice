0c27fbfb19796779c92f351240bc3c953334d034
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-08 07:31:39 +0530

    new changes

--- Apna College coding practice/LinearSearch/Findindex.java
+++ Apna College coding practice/LinearSearch/Findindex.java
@@ -1,7 +1,26 @@
 package LinearSearch;
 //find the index of an element .
-public class Findindex {
-    public static void main(String [] args){
-        
+
+public class Findindex{
+
+    public static int linearSearch(int numbers[] , int key){
+        for (int i=0; i<numbers.length; i++){
+            if(numbers[i] == key){
+                return i;
+            }  
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        int numbers[] = {2,3,45,67,7,42,1,};
+        int key = 10;
+
+        int index = linearSearch(numbers,key);
+        if(index == -1){
+            System.out.println("NOT FOUND");
+        }else{
+            System.out.println("Key is at index:"+ index);
+        }
+
     }
 }
