b199cc5a017b55420ac9e47796d2182ac8bf6a4e
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-09 01:05:35 +0530

    new changes

--- Apna College coding practice/Binary Search/first.java
+++ Apna College coding practice/Binary Search/first.java
@@ -1,10 +1,35 @@
 
 /* time complexity ==> k=log2n 
  * tc directly proportional to the log2n that is ==> O(log n)
+ * time complexity of binary search is always less and the code of binary search always performs well.
 */
-public class first {
+public class First {
+    public static int binarySearch(int numbers[] , int key){
+         int start =0, end=numbers.length-1;
+         while(start <= end) {
+            int mid = (start + end)/2;
+
+            //comparisons
+            if(numbers[mid] == key) {
+                return mid;
+            }
+            if(numbers[mid] < key){ //right
+                 start = mid+1;
+            }else{ //left
+                end = mid-1;
+
+            }
+         }
+         return -1;
+        }
+    
+
     public static void main(String[] args){
+        int numbers[] = {1,2,3,4,5,6,7,8};
+        int key = 2;
+    System.out.println("index for the key is:" + binarySearch(numbers,key));
 
     }
     
 }
+
---
+++ Apna College coding practice/Integer Questions/Armstrongnumber.java
@@ -0,0 +1,14 @@
+
+//Armstrong number.
+
+public class Armstrongnumber {
+
+
+    public static void main(String[] args){
+        int numbers[] = {2,4,6,8,10,12};
+        int key = 10;
+
+        
+    }
+    
+}
