f91fc38e5239f644a09c2812289161ec77816761
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-08 08:17:26 +0530

    new changes

--- Apna College coding practice/LinearSearch/SmallestNumber.java
+++ Apna College coding practice/LinearSearch/SmallestNumber.java
@@ -2,9 +2,19 @@ package LinearSearch;
 
 
 public class SmallestNumber {
+    public static int getSmallest( int numbers[]){
+    int smallest = Integer.MAX_VALUE;
+    for(int i=0; i<numbers.length; i++){
+        if(smallest > numbers[i]) {
+            smallest= numbers[i];
+        }
+    }
+    return smallest;
+    }
     public static void main(String[] args){
-        int numbers[] {1,2,3,4,5,5,6};
-        
+        int numbers[] ={1,2,3,4,5,5,6};
+        System.out.println("the smalles number is:" + getSmallest(numbers));
+
     }
     
 }
