8b4204c74cee3b0b4037304be542a601bb4ec22a
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-09 08:05:59 +0530

    new changes

---
+++ Apna College coding practice/Arrays/PairsinArray.java
@@ -0,0 +1,29 @@
+package Arrays;
+/* 
+ * [2,4,6,8,10] ==> all the pairs 
+ * (2,4) (2,6) (2,8) (2,10)
+ * (4,6) (4,8) (4,10)
+ * (6,8) (6,10)
+ * (8,10)
+ */
+public class PairsinArray {
+   public static void printPairs(int numbers[]){
+    for(int i=0; i<numbers.length; i++){
+        int curr = numbers[i];
+        for(int j=i+1; j<numbers.length; j++){
+            System.out.print("(" + curr + "," + numbers[j] + ")");
+            
+        }
+        System.out.println();
+    }
+   }
+
+
+
+
+
+    public static void main(String[] args){
+        int numbers[] = {2,4,6,8,10};
+        printPairs(numbers);
+    }
+}
