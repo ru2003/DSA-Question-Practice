d6c38a790bb45072a0ec1f8d3b82ded7afde604f
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-09 08:08:23 +0530

    new changes

--- Apna College coding practice/Arrays/PairsinArray.java
+++ Apna College coding practice/Arrays/PairsinArray.java
@@ -8,14 +8,17 @@ package Arrays;
  */
 public class PairsinArray {
    public static void printPairs(int numbers[]){
+    int tp = 0;
     for(int i=0; i<numbers.length; i++){
         int curr = numbers[i];
         for(int j=i+1; j<numbers.length; j++){
             System.out.print("(" + curr + "," + numbers[j] + ")");
+            tp++;
             
         }
         System.out.println();
     }
+    System.out.println("total pairs:" + tp);
    }
 
 
