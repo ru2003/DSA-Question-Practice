cde8634e60304e9260ac0cbf3a1bf315e4a2beab
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-11 18:25:49 +0530

    new changes

--- Apna College coding practice/TwoDArray/Spiralmatrix.java
+++ Apna College coding practice/TwoDArray/Spiralmatrix.java
@@ -1,5 +1,19 @@
 package TwoDArray;
 //matrix spriral ke form me print karwana hai .
-public class Spiralmatrix {
+public class Spiralmatrix { 
+    public static void printSpiral(int matrix [] []) {
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = matrix.length-1;
+        int endCol = matrix[0].length-1;
+
+        while(startRow <= endRow && startCol <= endCol) {
+            
+        }
+    
+    }
+    public static void main(String[] args){
+
+    }
     
 }
