eb281f1bb3b385712ef34afc2567b7847723c01d
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-11 18:37:03 +0530

    new changes

--- Apna College coding practice/TwoDArray/Spiralmatrix.java
+++ Apna College coding practice/TwoDArray/Spiralmatrix.java
@@ -8,10 +8,17 @@ public class Spiralmatrix {
         int endCol = matrix[0].length-1;
 
         while(startRow <= endRow && startCol <= endCol) {
-            
+            //top -->column
+            for(int j=startCol; j<=endCol; j++){
+                System.out.println(matrix[startRow][j]+" ");
+            }
+
+            //right
+            for(int i=startRow+1; i<=endRow; i++) {
+                System.out.println(matrix[i] [endCol] + " ");
+            }
         }
-    
-    }
+     }
     public static void main(String[] args){
 
     }
