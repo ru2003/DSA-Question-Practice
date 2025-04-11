d4139846a978941b79271a471603ae2766d0cef0
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-11 08:02:19 +0530

    new changes

---
+++ Apna College coding practice/TwoDArray/First.java
@@ -0,0 +1,14 @@
+package TwoDArray;
+import java.util.*;
+/* @-D array -->  suppose n*m  --> n= rows and m =columns  */
+public class First {
+    public static void main(String[] args){
+        int matrix [] [] = new int[3][3];
+        int n = matrix.length, m = matrix[0].length;
+       //rows==> matrix.length & columns==>matrix[0].length.
+
+       Scanner sc = new Scanner(System.in);
+
+    }
+    
+}
