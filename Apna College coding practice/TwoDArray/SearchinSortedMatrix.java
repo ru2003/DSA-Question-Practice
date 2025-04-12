4a960945a7a0845ccaf2588402fe0755d0131d25
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-12 23:31:45 +0530

    new changes

--- Apna College coding practice/TwoDArray/SearchinSortedMatrix.java
+++ Apna College coding practice/TwoDArray/SearchinSortedMatrix.java
@@ -7,9 +7,21 @@ package TwoDArray;
 public class SearchinSortedMatrix  {
         public static boolean staircaseSearch(int matrix[] [] , int key) {
             int row = 0,  col=matrix[0].length-1;
-            while(row < matrix.length && col >= 0) {
+
+            while(row < matrix.length && col >= 0){
+                if(matrix[row] [col] == key){
                 System.out.println("found key at(" + row + "," + col+ ")");
+                return true;
             }
+            else if(key < matrix[row][col]) {
+                col--;
+            }
+            else{
+                row++;
+            }
+            }
+            System.out.println("key not found!");
+            return false;
         }
 
 
@@ -21,7 +33,8 @@ public class SearchinSortedMatrix  {
             {27,29,37,48},
             {32,33,39,50}
         };
-        int ket = 32;
+        int key = 32;
+        staircaseSearch(matrix, 33);
     }
     
 }
