123cbc278168821db6621bb07651f50a1825cde4
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-13 19:39:58 +0530

    new changes

--- Apna College coding practice/CountingSort/first.java
+++ Apna College coding practice/CountingSort/first.java
@@ -11,12 +11,33 @@ public class first {
         largest = Math.max(largest, arr[i]);
     }
     int count[] = new int[largest+1];
+    for(int i=0; i<arr.length; i++) {
+        count[arr[i]]++;
+    }
+    //sorting
+    int j = 0;
+    for(int i=0; i<count.length; i++) {
+        while(count[i] > 0) {
+            arr[j] = i;
+            j++;
+            count[i]--;
+        }
+      }
    }
+
+   public static void printArr(int arr[]) {
+    for(int i=0; i<arr.length; i++) {
+        System.out.println(arr[i] + " ");
+    }
+   }
+
+   
+
 
     
     public static void main(String [] args) {
-        Integer arr[] = {5,4,1,3,2};
-        insertionSort(arr);
+        int arr[] = {5,4,1,3,2};
+        countingSort(arr);
         printArr(arr);
     }
 }
