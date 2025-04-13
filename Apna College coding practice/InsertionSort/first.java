80de04f2fe9319dc79fe8ea42bfb052bc27da443
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-13 12:33:38 +0530

    new changes

--- Apna College coding practice/InsertionSort/first.java
+++ Apna College coding practice/InsertionSort/first.java
@@ -6,19 +6,30 @@ package InsertionSort;
 public class first {
     public static void insertionSort(int arr[]) { 
         for(int i=1; i<arr.length; i++) {
-            int curr = i;
+            int curr = arr[i];
+            
             int prev = i-1;
             //finding out the correct pos to insert
-            while(prev >= 0 && arr[prev] > arr[curr]) {
+            while(prev >= 0 && arr[prev] > curr) {
                 arr[prev+1] = arr[prev];
                 prev--;
             }
+            //insertion 
+            arr[prev+1] = curr;
         }
-        
-
     }
+    public static void  printArr( int curr[]){
+        for(int i=0; i<curr.length; i++){
+            System.out.println(curr[i] + " ");
+        }
+        System.out.println();
+    }
+    
     public static void main(String[] args) {
-
+        int arr[] = {5,4,1,3,2};
+        insertionSort(arr);
+        printArr(arr);
+ 
     }
     
 }
