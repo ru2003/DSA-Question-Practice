84eb6067a7e34e959e30fe42be25db62db44f18a
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-13 08:17:33 +0530

    new changes

--- Apna College coding practice/InsertionSort/first.java
+++ Apna College coding practice/InsertionSort/first.java
@@ -1,8 +1,19 @@
 package InsertionSort;
-/* Pick an element (from unsorted part) and place in the right position in sorted part. */
+/* Pick an element (from unsorted part) and place in the right position in sorted part.
+ * (n-1) ==> agr n element me se agr (n-1) elemnt ko bhi sahi jagah pe pohcha diya toh bhi array sorted ho jayega.
+ */
 
 public class first {
     public static void insertionSort(int arr[]) { 
+        for(int i=1; i<arr.length; i++) {
+            int curr = i;
+            int prev = i-1;
+            //finding out the correct pos to insert
+            while(prev >= 0 && arr[prev] > arr[curr]) {
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+        }
         
 
     }
