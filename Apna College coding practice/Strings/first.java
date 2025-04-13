d2ed7c10d2540afd13d4cc26e076f7fff1a655e1
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-13 20:15:00 +0530

    new changes

---
+++ Apna College coding practice/Strings/first.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class first {
+    public static void main(String args[]) {
+        char arr[] = {'a' , 'b' , 'c' , 'd'};
+        String str = "abcd";
+        String str2 = new String("xyz");
+        //Strings are immutable 
+    }
+    
+}
