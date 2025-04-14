a7823d96ed748b96a2c255fb3bfddff104eaa453
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-14 17:35:00 +0530

    new changes

--- Apna College coding practice/BitwiseManipulation/AND.java
+++ Apna College coding practice/BitwiseManipulation/AND.java
@@ -2,7 +2,7 @@ package BitwiseManipulation;
 
 public class AND {
     public static void main(String args[]) {
-        System.out.println((5 & 6));
+        System.out.println((5 & 6));//Jab bhi bitwise operator ka use krna hai tabhi , single symbol ka use krna hai double ka nhi .
     }
     
 }
---
+++ Apna College coding practice/BitwiseManipulation/OR.java
@@ -0,0 +1,7 @@
+package BitwiseManipulation;
+/* BINARY OR ==> "|"
+ * bitwise me single operator use krte hai souble nhi . rmeber this .
+ */
+public class OR {
+    
+}
