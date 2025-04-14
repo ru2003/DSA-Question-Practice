bbfb16788f9a337fc1bf97d7d912094bdd9c6876
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-14 17:49:05 +0530

    new cahnges

---
+++ Apna College coding practice/BitwiseManipulation/OnesCompliment.java
@@ -0,0 +1,10 @@
+package BitwiseManipulation;
+/*
+ * binary ones compliment ==> NOT ==> ( ~ )
+ */
+public class OnesCompliment {
+  public static void main(String[] args) {
+    System.out.println((~5));
+  }
+    
+}
