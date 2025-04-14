e3520aeaf1fece6675abda9f9458d67973fedf77
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-14 17:40:01 +0530

    new changes

--- Apna College coding practice/BitwiseManipulation/OR.java
+++ Apna College coding practice/BitwiseManipulation/OR.java
@@ -3,5 +3,8 @@ package BitwiseManipulation;
  * bitwise me single operator use krte hai souble nhi . rmeber this .
  */
 public class OR {
+    public static void  main(String[] args) {
+        System.out.println((5|6));
+    }
     
 }
