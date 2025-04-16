5c825bebdf9182cf8b152934c62c970c49bfd56e
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-16 16:46:16 +0530

    new changes

--- Apna College coding practice/BitwiseManipulation/ClearithBit.java
+++ Apna College coding practice/BitwiseManipulation/ClearithBit.java
@@ -1,5 +1,15 @@
 package BitwiseManipulation;
 
 public class ClearithBit {
+    public static int cleartheBit(int n, int i) {
+        int bitMask = ~(1 << i);
+        return n & bitMask; 
+    }
+    public static void main(String[] args) {
+        int n=13;
+        int i=2;
+        System.out.println();
+
+    }
     
 }
