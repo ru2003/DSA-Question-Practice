9ae5b000b02327981e7aaf00ed7a8a2cea7d7c42
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-16 15:27:00 +0530

    new changes

---
+++ Apna College coding practice/BitwiseManipulation/ClearithBit.java
@@ -0,0 +1,5 @@
+package BitwiseManipulation;
+
+public class ClearithBit {
+    
+}
--- Apna College coding practice/BitwiseManipulation/SetIthBit.java
+++ Apna College coding practice/BitwiseManipulation/SetIthBit.java
@@ -2,8 +2,12 @@ package BitwiseManipulation;
 
 public class SetIthBit {
     public static int setIthBit(int num, int i) {
-        int bitmask = 1 << i; // Create a bitmask with 1 at the i-th position
-        return num | bitmask; // Use OR to set the i-th bit
+        int bitmask = 1 << i; // Create a bitmask with 1 at the i-th position  ===>this shifts the 1 to the left by i positions.
+        return num | bitmask; /*  Use OR to set the i-th bit  
+        -->a binary number with 1 at a specific position and 0 elsewhere .It helps target the bit you to changes.
+        thi gives us a bitmask with only the "i" th bit set to 1.
+        ===>this ensures the i-th bit of num is set to 1.without affecting the rest of the number.
+        */
     }
 
     public static void main(String[] args) {
