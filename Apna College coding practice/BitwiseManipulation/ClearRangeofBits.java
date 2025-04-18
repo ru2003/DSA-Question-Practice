2b2d5145047614659b0a60758f37fa5715bb279b
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-18 17:23:11 +0530

    new changes

--- Apna College coding practice/BitwiseManipulation/ClearRangeofBits.java
+++ Apna College coding practice/BitwiseManipulation/ClearRangeofBits.java
@@ -1,6 +1,9 @@
 package BitwiseManipulation;
-
+/*java me class name hakseha capital letter se lena chaiye 
+ * and functions ke nam small letters se.
+ * aisa nhi karenge toh as such kuch hoga nhi but it is a good practice.
+ */
 public class ClearRangeofBits {
-    
+
     
 }
---
+++ Apna College coding practice/ObjectorientedProgramming/Ooops.java
@@ -0,0 +1,18 @@
+package ObjectorientedProgramming;
+
+public class Ooops {
+    
+}
+
+
+class Pen {
+    String color;
+    int tip;
+    void setColor(String newColor) {
+        color = newColor;
+    }
+
+    void setTip(int newTip) {
+        tip = newTip;
+    }
+}
