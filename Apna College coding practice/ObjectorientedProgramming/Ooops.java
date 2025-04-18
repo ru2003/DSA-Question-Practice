cfe630812d8060dc75ceb699e7ea6cdeab93a3a3
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-18 18:38:04 +0530

    new changes

--- Apna College coding practice/ObjectorientedProgramming/Ooops.java
+++ Apna College coding practice/ObjectorientedProgramming/Ooops.java
@@ -1,6 +1,13 @@
 package ObjectorientedProgramming;
 
 public class Ooops {
+    /* by convention ye main function  public hona important hai , and access specifieer void hona jaruri hain. */
+    public static void main(String[] args) {
+         /*new ==> Pen class ka p1 object create kiya and then new keyword se jagah bana rhe hai jaha sari properties store hogi   */
+         Pen p1 = new Pen();
+         p1.setColor("Blue");
+         System.out.println(p1.color);
+    }
     
 }
 
@@ -17,7 +24,7 @@ class Pen {
     }
 }
 
-classStudent {
+class Student {
     String name;
     int age;
     float percentage; //cgpa
