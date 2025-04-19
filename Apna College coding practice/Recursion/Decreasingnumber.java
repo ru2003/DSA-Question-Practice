c036a9221ef3e66c131584398b380ad2e998d4da
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-19 21:52:33 +0530

    new changes

--- Apna College coding practice/Recursion/Decreasingnumber.java
+++ Apna College coding practice/Recursion/Decreasingnumber.java
@@ -2,7 +2,12 @@ package Recursion;
 
 public class Decreasingnumber {
     public static void printDec(int n) {
-        
+         if(n == 1) {
+            System.out.println(n);
+            return ;
+         }
+         System.out.println(n + " ");
+         printDec(n-1);
     }
 
     public static void main(String [] args) {
