96e45f3399b7122b57de5c4b555e08e8b8e7713f
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-20 10:21:14 +0530

    new chanes

--- Apna College coding practice/Recursion/Increasing.java
+++ Apna College coding practice/Recursion/Increasing.java
@@ -1,7 +1,11 @@
 package Recursion;
-
+//it is very important to know that in which line there will be recusrion call and in which statemtn there will be print statement .
 public class Increasing {
+/*Before call → happens as you go deeper (top-down).
 
+After call → happens as you return (bottom-up).
+
+ */
     public static void printInc(int n) {
         if (n ==1) {
             System.out.println(n + " ");
@@ -13,10 +17,6 @@ public class Increasing {
     public static void main(String [] args) {
         int n=10;
         printInc(n);
-
-
-
-
     }
     
 }
