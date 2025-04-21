69e1119d21808b5b7cc86d6747a76bdbcb005c93
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-21 08:52:04 +0530

    new changes

---
+++ Apna College coding practice/Recursion/NaturalNumbersSum.java
@@ -0,0 +1,19 @@
+package Recursion;
+//find the sum of n natural numbers using recursion.
+public class NaturalNumbersSum {
+
+    public static int calcSum(int n) {
+        if(n==1) {
+            return 1;
+        }
+
+        int Snm1 = calcSum(n-1);
+        int Sn = n +Snm1;
+        return Sn;
+    }
+    public static void main(String[] args){
+        int n = 5;
+        System.out.println(calcSum(n));
+    }
+    
+}
