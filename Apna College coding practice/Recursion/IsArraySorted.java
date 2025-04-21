53b7ce5c33a4e45bd111eeb82e7ea2b263e73e29
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-21 16:55:49 +0530

    new changes

--- Apna College coding practice/Recursion/IsArraySorted.java
+++ Apna College coding practice/Recursion/IsArraySorted.java
@@ -10,10 +10,20 @@ public class IsArraySorted {
             return false;
         }
         return isSorted(arr, i+1);
+        /*array enter hota hai then check krte hai first loop last tk jata nhi ,
+        last tk jata nhi tab tk run krte rho then later
+        originally true samjte hai ,  then false hua toh exit then later.
+        agle wale ke liye check krte krte  jate hain.
+        */
  
     }
     public static void main(String[] args) {
-
+          int arr[] = {1,2,3,9,4};
+        if(isSorted(arr, 0)){
+            System.out.println("The array is sorted:");
+        }else{
+            System.out.println("The array is not sorted.");
+        }
     }
     
 }
