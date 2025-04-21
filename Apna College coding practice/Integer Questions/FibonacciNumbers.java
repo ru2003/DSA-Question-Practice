b577d5d6c373bbda9de4147cd12b031b9a368f90
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-21 15:41:48 +0530

    new changes

--- Apna College coding practice/Integer Questions/FibonacciNumbers.java
+++ Apna College coding practice/Integer Questions/FibonacciNumbers.java
@@ -3,10 +3,17 @@ import java.util.Scanner;
 public class FibonacciNumbers {
     
     public static void printFibonacci(int n) {
+        int firstterm = 0;
+        int lastterm = 1;
+
+       //Print the first 'count' Fibonacci numbers
+       for( int i= 0; i< n ;i++) {
+           
+       }
 
     }
     public static void main(String[] args) {
-         
+        
        
     }
 }
