b850ca8559ed287ae248d048934d7898a45d79aa
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-21 09:39:37 +0530

    new changes

---
+++ Apna College coding practice/Integer Questions/FibonacciNumbers.java
@@ -0,0 +1,7 @@
+public class FibonacciNumbers {
+    
+    public static void main(String[] args) {
+         
+       
+    }
+}
--- Apna College coding practice/Recursion/NaturalNumbersSum.java
+++ Apna College coding practice/Recursion/NaturalNumbersSum.java
@@ -1,7 +1,10 @@
 package Recursion;
 //find the sum of n natural numbers using recursion.
 public class NaturalNumbersSum {
-
+/*
+ * A ladder where you go up step by step (recursive call) 
+ * and then come down step by step(returning from call).
+ */
     public static int calcSum(int n) {
         if(n==1) {
             return 1;
@@ -11,6 +14,11 @@ System.out.println("agge ke direction me");
         System.out.println("stack me ulte ate time");
         int Sn = n +Snm1;
         return Sn;
+        /*
+         * int snm11 = calcSum(n-1); ==> waits until it gets result from the next recursive call
+         * ***Only after it gets the value , it goes to n Sn=n+Snm1;
+         * So thats why first line feels like it runs multiple time - because it keeps calling , and doesnt move ahead until the function below finishes.
+         */
     }
     public static void main(String[] args){
         int n = 5;
@@ -18,3 +26,16 @@ System.out.println("agge ke direction me");
     }
     
 }
+/*
+ * Imagine you're opening boxes one inside another:
+
+You open box 3 → inside is box 2 → inside is box 1
+
+Box 1 gives you 1
+
+Box 2 adds 2 + 1 = 3
+
+Box 3 adds 3 + 3 = 6
+
+
+ */
