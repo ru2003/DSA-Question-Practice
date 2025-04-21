4a4cf5444f63ccedb57aaec12453c12c11001f02
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-21 16:40:43 +0530

    new changes

---
+++ Apna College coding practice/Recursion/IsArraySorted.java
@@ -0,0 +1,12 @@
+package Recursion;
+//check of the given array is osrted or not.
+public class IsArraySorted {
+    public static boolean isSorted(int arr[] , int i) {
+
+        
+    }
+    public static void main(String[] args) {
+
+    }
+    
+}
