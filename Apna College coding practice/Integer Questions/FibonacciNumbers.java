b350a08a10927fab7aa3d3912a16b9207c674bb5
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-21 15:32:27 +0530

    new changes

--- Apna College coding practice/Integer Questions/FibonacciNumbers.java
+++ Apna College coding practice/Integer Questions/FibonacciNumbers.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class FibonacciNumbers {
     
+    public static void printFibonacci(int n) {
+
+    }
     public static void main(String[] args) {
          
        
