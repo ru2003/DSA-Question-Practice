fb3e5ba05b0154b6e34a286528bafb870add8600
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-22 08:20:14 +0530

    new changes

--- Apna College coding practice/Recursion/FirstOcuurence.java
+++ Apna College coding practice/Recursion/FirstOcuurence.java
@@ -1,6 +1,11 @@
 package Recursion;
 
-//WAF to find the first occurence of an element in an array.
+// WAF to find the first occurence of an element in an array.
+// given key first time konse inde pe occur krega , woh index return krna hai.
 public class FirstOcuurence {
+
+    public static void main(String[] args) {
+        
+    }
     
 }
