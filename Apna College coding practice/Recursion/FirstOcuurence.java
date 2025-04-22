0c774a7cd696b52eba9de0aefa6477e35144912f
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-22 08:28:29 +0530

    new changes

--- Apna College coding practice/Recursion/FirstOcuurence.java
+++ Apna College coding practice/Recursion/FirstOcuurence.java
@@ -4,8 +4,19 @@ package Recursion;
 // given key first time konse inde pe occur krega , woh index return krna hai.
 public class FirstOcuurence {
 
+    public static int firtOccurenceofelement(int arr[] , int key , int i) {
+        //base case which is requried to stop the function.
+        if(i == arr.length) {
+            return -1;
+        }
+        if (arr [i] == key) {
+            return i;
+        }//in recursion this is the only 1 case which is getting chencked
+
+        return firtOccurenceofelement( arr, key , i+1);//after that its just a assumption that ki baki ke bache hue check ho jayenge.
+    }
     public static void main(String[] args) {
-        
+
     }
     
 }
