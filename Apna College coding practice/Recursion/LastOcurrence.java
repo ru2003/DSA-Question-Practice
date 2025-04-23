338805584754db105f23db940d59ac95f7a82b60
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-23 11:35:51 +0530

    new changes

--- Apna College coding practice/Recursion/LastOcurrence.java
+++ Apna College coding practice/Recursion/LastOcurrence.java
@@ -1,8 +1,20 @@
 package Recursion;
-
+//WAF to find the last occurence of an element in an array.jo key chaiye woh sabse jyada last konse index me ayi hain.
 public class LastOcurrence {
+    public static int lastOccurenc(int arr[] , int key,int i) {
+            if(i==arr.length) {
+                return -1;
+            }
+            int isFound = lastOccurenc(arr, key,i+1);
+            if(isFound == -1 && arr[i] == key) {
+                return i;
+            }
+            return isFound;
+
+    }
     public static void main(String[] args) {
-        
+         int arr[] = {4,6,2,1,6,3,2,6};
+         System.out.println(lastOccurenc(arr, 02, 0));
     }
     
 }
