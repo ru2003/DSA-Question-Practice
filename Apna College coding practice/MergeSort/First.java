f43d5fe776f1fc101ef7c5cb580c1fe1264aecc4
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-24 19:36:13 +0530

    new changes

--- Apna College coding practice/MergeSort/First.java
+++ Apna College coding practice/MergeSort/First.java
@@ -1,15 +1,18 @@
 package MergeSort;
 
 public class First {
-   public static void main(int arr[]) {
-    for(int i=0; i<arr.length; i++) {
-        System.out.println(arr[i] + " ");
+   public static void printArr(int arr[]) {
+    for(int i=0; i<arr.length; i++) { 
+        System.out.print(arr[i] + " ");
     }
     System.out.println();
    }
+   
+
 
     public static void main(String[] args) {
-        int arr[] = {6,3,9,5,2,8};
+        int arr[] = {6,3,9,5,2,8};2
+
     }
     
 }
