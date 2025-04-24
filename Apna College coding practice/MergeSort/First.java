7a84d4f84f9511804f705d1f93ddac62f80ae41d
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-25 02:36:14 +0530

    new changes

--- Apna College coding practice/MergeSort/First.java
+++ Apna College coding practice/MergeSort/First.java
@@ -11,8 +11,9 @@ public class First {
    public static void mergeSort(int arr[] , int si, int ei) {
          //work
          int mid = si + (ei - si)/2; //(si+ei)/2
-         mergeSort(arr,si,mid); //left part
+         mergeSort(arr,si,mid); //left part , recursive function hai woh. recursively left chhote part ko sort kr dega. 
          mergeSort(arr, mid+1, ei);//right part
+         merge(arr,si,mid,ei);
    }
    
    
@@ -20,7 +21,11 @@ public class First {
     public static void main(String[] args) {
         int arr[] = {6 , 3 , 9 , 5 , 2 , 8};
         mergeSort(arr,0,arr.length-1);
+        
 
+    }
+    public static void merge(int arr[], int si, int mid, int e) {
+        
     }
     
 }
