afc2d56d73bb44b63b14d833a69c4689998b760e
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-26 00:47:11 +0530

    new changes

--- Apna College coding practice/QuickSort/first.java
+++ Apna College coding practice/QuickSort/first.java
@@ -18,6 +18,20 @@ public class first {
         }
         System.out.println();
     }
+
+    public static void quickSort(int arr[], int si, int ei) {
+        //last element
+        int pIdx = partition(arr, si,ei);
+        quickSort(arr, si,pIdx-1);
+        quickSort(arr, pIdx+1, ei);
+
+
+    }
+    public static void partition(int arr[], int si, int ei) {
+
+    }
+
+
     public static void main(String[] args) {
         int arr[] = {6,3,9,8,2,5};
     
