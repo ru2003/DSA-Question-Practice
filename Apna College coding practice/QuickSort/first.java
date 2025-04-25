adc3d3eb5d6124cdfa6ac0de0a1029c5bb027cc5
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-25 17:24:26 +0530

    new changes

--- Apna College coding practice/QuickSort/first.java
+++ Apna College coding practice/QuickSort/first.java
@@ -1,8 +1,18 @@
 package QuickSort;
+/*average O(nlogn)
+ * worst O(n^2)
+ * space(1)
+ */
+//fast sorting chaiye but hamare pass dene ke liye space nhi hai.
+//pivot and partition
+/*
+ * pivot and partition 
+ * 1)choose pivot --> last element 
+ */
 
 public class first {
     public static void main(String[] args) {
-        
+
     }
     
 }
