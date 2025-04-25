439efddf18dbca853c0d0282ec768e499eb0459f
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-25 10:11:37 +0530

    new changes

--- Apna College coding practice/MergeSort/First.java
+++ Apna College coding practice/MergeSort/First.java
@@ -18,7 +18,25 @@ public class First {
          mergeSort(arr, mid+1, ei);//right part
          merge(arr,si,mid,ei);
    }
-   
+   public static void merge(int arr[], int si, int mid, int ei) {
+    //left(0,3)=4 right(4,6) =3 -->6-0+1
+    int temp[]= new int[ei-si+1];
+    int i = si;//iterator for left part
+    int j = mid+1;//iterator for right part
+    int  k = 0;//iterator for temp arr
+     while(i <= mid && j <= ei) {
+        if(arr[i] < arr[j]) {
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }else {
+            temp[k] = arr[j];
+            j++;
+            k++;
+        }
+     }
+}
+
    
 
     public static void main(String[] args) {
@@ -27,11 +45,5 @@ public class First {
         
 
     }
-    public static void merge(int arr[], int si, int mid, int ei) {
-        //left(0,3)=4 right(4,6) =3 -->6-0+1
-        int temp[]= new int[ei-si+1];
-        int i = si;//iterator for left part
-        int j = mid+1;//iterator for right part
-    }
-    
+   
 }
