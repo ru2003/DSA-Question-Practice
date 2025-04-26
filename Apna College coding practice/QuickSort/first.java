6edb9b45d87384523eef5f6df0406fc4c5220e88
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-27 00:35:02 +0530

    new changes

--- Apna College coding practice/QuickSort/first.java
+++ Apna College coding practice/QuickSort/first.java
@@ -1,4 +1,4 @@
-package QuickSort;
+
 /*average O(nlogn)
  * worst O(n^2)
  * space(1)
@@ -9,32 +9,56 @@ package QuickSort;
  * pivot and partition 
  * 1)choose pivot --> last element 
  */
+package QuickSort;
 
 public class first {
 
+    // Function to print the array
     public static void printArr(int arr[]) {
-        for (int i=0; i<arr.length; i++){
-            System.out.println(arr[i] + " ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
 
+    // Main QuickSort function
     public static void quickSort(int arr[], int si, int ei) {
-        //last element
-        int pIdx = partition(arr, si,ei);
-        quickSort(arr, si,pIdx-1);
-        quickSort(arr, pIdx+1, ei);
-
-
-    }
-    public static void partition(int arr[], int si, int ei) {
-
+        if (si >= ei) {
+            return;
+        }
+        int pIdx = partition(arr, si, ei);
+        quickSort(arr, si, pIdx - 1); // Sort left subarray
+        quickSort(arr, pIdx + 1, ei); // Sort right subarray
     }
 
+    // Partition function using Lomuto partition scheme
+    public static int partition(int arr[], int si, int ei) {
+        int pivot = arr[ei];
+        int i = si - 1;
 
+        for (int j = si; j < ei; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // Swap arr[i+1] and arr[ei] (pivot)
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[ei];
+        arr[ei] = temp;
+
+        return i; // Return the partitioning index
+    }
+
+    // Main method to test the QuickSort algorithm
     public static void main(String[] args) {
-        int arr[] = {6,3,9,8,2,5};
-    
+        int arr[] = {6, 3, 9, 8, 2, 5};
+        quickSort(arr, 0, arr.length - 1);
+        printArr(arr);
     }
-      
 }
