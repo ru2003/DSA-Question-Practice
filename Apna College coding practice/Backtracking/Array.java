98ef4019b7aea9f15926dc13fc875d3c692d4f49
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-29 10:01:04 +0530

    new changes

--- Apna College coding practice/Backtracking/Array.java
+++ Apna College coding practice/Backtracking/Array.java
@@ -5,6 +5,13 @@ public class Array {
 
     }
 
+    public static void printArr(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args){
         int arr[] = new int[5];
         changeArr(arr, 0,1);
