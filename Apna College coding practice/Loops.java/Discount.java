fee352babfeca41a4bbbd56140096c6ed8a9b6a3
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-29 23:16:11 +0530

    new changes

---
+++ Apna College coding practice/Loops.java/Discount.java
@@ -0,0 +1,8 @@
+/* : Print a bill for a customer that calculates the total sum of
+ prices and applies 10% discount if the bill is more than 500.
+
+*/
+
+public class Discount {
+    
+}
--- Apna College coding practice/Loops.java/Product.java
+++ Apna College coding practice/Loops.java/Product.java
@@ -1,4 +1,4 @@
-/*You are given a list of products with their prices. 
+/* Que ==>You are given a list of products with their prices. 
 Print each product and its price in the format: "Product Name - Price".*/
 
 public class Product {
@@ -7,8 +7,13 @@ public class Product {
          // arrays to store the products name and their corresponding price.
          String[] product ={"Laptop" , " mobile", " headphones" , " keyboard"};
          int[] prices = {120000, 450000, 650000, 75000};
+/* NotE ==> using a parallel arrays ,both the arrays must have same length  
+ * using a for loop to print a each product and its price.
+ * products[i] gives the product name at index i
 
-         //using a for loop to print a each produc and its price
+prices[i] gives the corresponding price at index i
+   */
+        
          for(int i=0; i<product.length; i++) {
             System.out.println(product[i] + "- " + prices[i]);
          }
