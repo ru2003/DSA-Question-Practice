761df919642cb4c50ededb19cd2decce4d90f8bc
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-29 23:36:25 +0530

    new changes

--- Apna College coding practice/Loops.java/Discount.java
+++ Apna College coding practice/Loops.java/Discount.java
@@ -6,7 +6,12 @@
 public class Discount {
 
     public static void main(String[] args) {
+       int arr[] = { 100,200,300,600};
 
+       int sum = 0;
+       for(int i=0; i<arr.length; i++) {
+           sum += arr[i];
+       }
     }
     
 }
