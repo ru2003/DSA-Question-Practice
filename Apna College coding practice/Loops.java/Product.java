5f1ad97873f0073fa634b89c9c17f2a83ee7b816
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-29 22:57:40 +0530

    new changes

--- Apna College coding practice/Loops.java/Product.java
+++ Apna College coding practice/Loops.java/Product.java
@@ -1,5 +1,10 @@
-//You are given a list of products with their prices. Print each product and its price in the format: "Product Name - Price".
+/*You are given a list of products with their prices. 
+Print each product and its price in the format: "Product Name - Price".*/
 
 public class Product {
+
+    public static void main(String [] args) {
+        
+    }
     
 }
