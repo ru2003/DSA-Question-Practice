d10a6e15fadcd67c9a551943cb73bf60d6b58915
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-29 23:06:08 +0530

    new changes

--- Apna College coding practice/Loops.java/Product.java
+++ Apna College coding practice/Loops.java/Product.java
@@ -4,7 +4,14 @@ Print each product and its price in the format: "Product Name - Price".*/
 public class Product {
 
     public static void main(String [] args) {
-        
+         // arrays to store the products name and their corresponding price.
+         String[] product ={"Laptop" , " mobile", " headphones" , " keyboard"};
+         int[] prices = {120000, 450000, 650000, 75000};
+
+         //using a for loop to print a each produc and its price
+         for(int i=0; i<product.length; i++) {
+            System.out.println();
+         }
     }
     
 }
