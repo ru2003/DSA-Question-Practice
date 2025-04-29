6f01a718ebd71be72f21b325763b2f5b56ce6af0
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-29 10:21:28 +0530

    new changes

--- Apna College coding practice/Backtracking/Array.java
+++ Apna College coding practice/Backtracking/Array.java
@@ -1,16 +1,18 @@
 package Backtracking;
-
+//like array print krna hai 1 to 5 and print krte time -2 krna hai har value se and then use print krna hain .
+//time complexity O(n)
 public class Array {
     public static void changeArr(int arr[], int i, int val) {
      //base case
       if(i == arr.length ) {
         printArr(arr);
-        return ;
+        return ; 
       }
      //recursion
      arr[i] = val;
-     changeArr(arr, i+1,val+1);
-     arr[i] = arr[i] -2;
+     changeArr(arr, i+1,val+1);//fnx call step
+     arr[i] = arr[i] -2;//backtracking step
+     //backtracking function call ke bad hi hoti hai ,pehle kabhi nhi hoti .
     }
 
     public static void printArr(int arr[]) {
@@ -18,7 +20,7 @@ public class Array {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
-    }
+    } 
 
     public static void main(String[] args){
         int arr[] = new int[5];
---
+++ Apna College coding practice/Backtracking/Subset.java
@@ -0,0 +1,7 @@
+package Backtracking;
+/* Find and print all the subsets of a given number
+ * " abc" ==> a,b,c,ab,ac,bc,abc," "
+ */
+public class Subset {
+    
+}
