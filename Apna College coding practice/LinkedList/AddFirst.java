5343875ffb10a0da49f790d57222bcc06fdea5dd
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-30 17:07:22 +0530

    new changes

--- Apna College coding practice/LinkedList/AddFirst.java
+++ Apna College coding practice/LinkedList/AddFirst.java
@@ -2,6 +2,19 @@ package LinkedList;
 //add in the Linked List
 public class AddFirst {
 
+  public static class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+  }
+
+  public static Node head;
+  public static Node tail;
+
 
 
     public static void main(String[] args) {
