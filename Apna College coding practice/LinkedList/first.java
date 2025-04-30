b22352c4a327ab531d0dababd092a5ef803a40ca
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-30 14:21:15 +0530

    new changes

--- Apna College coding practice/LinkedList/first.java
+++ Apna College coding practice/LinkedList/first.java
@@ -1,7 +1,22 @@
 package LinkedList;
-/* Linked list is a linear data structure where elements , called nodes, are connected using pointers .
- * unlike arrays, linked list do not store elements in contiguous memory locations
+/*- Linked list is a {linear data} structure where elements , called nodes, are connected using pointers .
+ * -unlike arrays, linked list do not store elements in contiguous memory locations.instead each node contains data and reference to the next node in the sequence.
+ * 
+ * 
+ * 
+ * *******Advantages of Linked List*****
+ * 1)dynamic size -linked lists can easiy grow or shrink in size by allocating or dellocating memory as needed.
+ * 2)3efficient insertions/deletions--adding or removing nodes doesnt require shifting elemnts, as in arrays.
+ * 3)memory utilization --memory is allocated only when required ,reducing wastage.
+ * 
+ * ************Disadvantages of Linked List******
+ * 1)sequential access -> unlike arrays, linked lists do not allow direct access to elemnts via indices;traversal is necessary
+ * 2)extra memory -> each node requires additional memory for the pointers
+ * 3)complexity -> operations like reverse traversal or accessing previous nodes required more logic or a different list tupe.
  */
 public class first {
+    public static void main(String[] args) {
+        
+    }
     
 }
