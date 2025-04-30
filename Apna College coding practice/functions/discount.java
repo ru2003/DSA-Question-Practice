22e34483843c1b95d7b3bbc3aa0abbe5f8540d50
ru2003 <ruchitanchaudhari145@gmail.com>
2025-04-30 10:05:03 +0530

    new changes

--- Apna College coding practice/functions/discount.java
+++ Apna College coding practice/functions/discount.java
@@ -1,5 +1,10 @@
 package functions;
-
+/*Write a function calculate_discount(price, discount_percent) that
+ returns the price after applying discount. Use it to print bills for 5 customers. */
 public class discount {
+
+    public static void main (String[] args) {
+        
+    }
     
 }
