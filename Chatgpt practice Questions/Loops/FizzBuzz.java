4748aee5e6ebd29c2bd9395d80cfc9bc4fd7c917
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-04 09:52:11 +0530

    new changes

--- Chatgpt practice Questions/Loops/FizzBuzz.java
+++ Chatgpt practice Questions/Loops/FizzBuzz.java
@@ -13,15 +13,17 @@
          System.out.println("Enter the value of N:");
          int N = sc.nextInt();
 
-         for(int i=1 ; i<=N ;i++){
-            if(i % 3 == 0 ){
+         // Loop from 1 to N
+        for (int i = 1; i <= N; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {//this condition should come first always
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz");
-            }else if(i % 5 == 0 ) {
+            } else if (i % 5 == 0) {
                 System.out.println("Buzz");
-            }else if(i % 3 == 0 && i % 5 ==0 ){
-                
-
+            } else {
+                System.out.println(i);
             }
-         }
+        }
     }
  }
