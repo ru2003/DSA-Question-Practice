30183cf2babd1bf7d0ec213bfc8468fb8d53a840
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-04 11:09:17 +0530

    new changes

--- Chatgpt practice Questions/Loops/_5LargestofThree.java
+++ Chatgpt practice Questions/Loops/_5LargestofThree.java
@@ -2,6 +2,13 @@
 
 public class _5LargestofThree {
     public static void main(String[] args) {
-        
+         int a=5;
+         int b= 4;
+         int c=2;
+         if(a>b  && a>c){
+            System.out.println("The greates of three number is:" + a);
+         }else if(b>1 && b>c) {
+            
+         }
     }
 }
