61dbda549ecfb85748e820dda023372b17597318
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-04 10:47:58 +0530

    new changes

--- Chatgpt practice Questions/Loops/_4SumOfEvenNumbers.java
+++ Chatgpt practice Questions/Loops/_4SumOfEvenNumbers.java
@@ -8,9 +8,11 @@ public class _4SumOfEvenNumbers {
          int sum =0;
          for(int i=2 ; i<=100;i++){
             if(i % 2 ==0){
+                sum += i;
 
             }
          }
+         System.out.println("The sum of even numbers between the 1 to 100:"+sum);
     }
     
 }
