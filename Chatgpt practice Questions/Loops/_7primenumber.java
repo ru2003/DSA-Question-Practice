cccb9e05fe7c151b262208edd6cca525524a935f
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-04 11:22:52 +0530

    new changes

--- Chatgpt practice Questions/Loops/_7primenumber.java
+++ Chatgpt practice Questions/Loops/_7primenumber.java
@@ -1,7 +1,12 @@
 /*Check if a given number is prime. */
 
+
+import java.util.Scanner;
 public class _7primenumber {
     public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
 
+          System.out.println("Enter the number:");
+          int num =sc.nextInt();
     }
 }
