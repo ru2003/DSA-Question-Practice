f66d416c093d1d0a3b496b0a8c738d6d34a52910
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-04 09:36:36 +0530

    new changes

---
+++ Chatgpt practice Questions/Loops/FizzBuzz.java
@@ -0,0 +1,12 @@
+/*Print all numbers from 1 to N, but for multiples of 3 print "Fizz",
+ for 5 print "Buzz", and for 3 and 5 both print "FizzBuzz". (⭐ FizzBuzz 
+ is a famous interview question) in java
+ */
+
+ import java.util.Scanner;
+
+ public class FizzBuzz{
+    public static void main(String[] args) {
+        
+    }
+ }
