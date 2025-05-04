ef6229e6389e0e42013881d91f018458f7d922e8
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-04 10:45:00 +0530

    new changes

--- Chatgpt practice Questions/Loops/_4SumOfEvenNumbers.java
+++ Chatgpt practice Questions/Loops/_4SumOfEvenNumbers.java
@@ -4,5 +4,13 @@
  */
 
 public class _4SumOfEvenNumbers {
+    public static void main(String[] args) {
+         int sum =0;
+         for(int i=2 ; i<=100;i++){
+            if(i % 2 ==0){
+
+            }
+         }
+    }
     
 }
