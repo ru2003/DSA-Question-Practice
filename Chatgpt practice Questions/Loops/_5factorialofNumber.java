9a71317b564e492c328a2620488cc2689ced5a1a
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-04 10:52:33 +0530

    new changes

--- Chatgpt practice Questions/Loops/_5factorialofNumber.java
+++ Chatgpt practice Questions/Loops/_5factorialofNumber.java
@@ -5,6 +5,11 @@
 
 public class _5factorialofNumber {
     public static void main(String[] args) {
-
+         
+        int num = 5;
+        int factorial =0;
+        for(int i=1;i<=5;i++){
+            int factorial =  
+        }
     }
 }
