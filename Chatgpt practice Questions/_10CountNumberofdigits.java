f53ba1ab5873e87f4ba8f135150bf410740e11e3
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-05 12:31:41 +0530

    new changes

---
+++ Chatgpt practice Questions/_10CountNumberofdigits.java
@@ -0,0 +1,7 @@
+/* 10.Count the number of digits in a number (without converting to string). */
+
+public class _10CountNumberofdigits {
+    public static void main (String[] args) {
+        
+    }
+}
