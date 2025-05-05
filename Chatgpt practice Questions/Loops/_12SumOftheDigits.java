0edb8e3afd340d55081ac47a8411bc9174ee0634
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-05 14:42:38 +0530

    new

--- Chatgpt practice Questions/Loops/_12SumOftheDigits.java
+++ Chatgpt practice Questions/Loops/_12SumOftheDigits.java
@@ -1,9 +1,15 @@
 
 /*12.Calculate sum of digits of a number (e.g., 123 → 1+2+3=6).
+Step  num	digit (num % 10)	sum	    num after num /= 10
+1	  123	           3       	0 + 3 = 3	       12
+2	  12		       2	    3 + 2 = 5          1
+3	  1		           1	    5 + 1 = 6          0 → loop ends
+
+
  */
 public class _12SumOftheDigits {
     
     public static void main(String[] args) {
-
+           
     }
 }
