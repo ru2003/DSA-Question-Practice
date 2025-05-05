1cf1073027a08aa35677ddb1a9a34e52713e6609
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-05 12:20:13 +0530

    new changes

--- Chatgpt practice Questions/Loops/_8rightangletriangle.java
+++ Chatgpt practice Questions/Loops/_8rightangletriangle.java
@@ -10,6 +10,12 @@ Copy code
 
 public class _8rightangletriangle {
     public static void main(String[] args) {
-        
+        int num =4;
+        for(int i=1; i<=4;i++) {
+            for(int j=1;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
