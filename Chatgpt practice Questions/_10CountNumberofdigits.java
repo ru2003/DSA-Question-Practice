923010aaafc1717465305908b6b7f88934e405b1
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-05 13:25:32 +0530

    new changes

--- Chatgpt practice Questions/_10CountNumberofdigits.java
+++ Chatgpt practice Questions/_10CountNumberofdigits.java
@@ -5,7 +5,11 @@ public class _10CountNumberofdigits {
     public static int countDigit(int num) {
           if(num == 0) 
           return 1;
-          
+          num = Math.abs(num);
+          int count =0;
+         
+         
+
     }
 
     public static void main (String[] args) {
