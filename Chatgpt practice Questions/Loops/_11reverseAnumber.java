5791c3f1f9e7b2504f8601f5f29f2cc0bad4ac8b
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-05 14:14:53 +0530

    new changes

--- Chatgpt practice Questions/Loops/_11reverseAnumber.java
+++ Chatgpt practice Questions/Loops/_11reverseAnumber.java
@@ -1,9 +1,19 @@
 /*Reverse a number (e.g., input: 1234 → output: 4321) */
+/*
+ * num % 10 → Get last digit
 
+num /= 10 → Remove last digit (to move to the next one)
+ */
 public class _11reverseAnumber {
     
     public static int ReverseNumb(int num) {
-        
+
+        int reversed =0;
+        while(num !=0){
+            
+        }
+
+
     }
     public static void main(String[] args) {
 
