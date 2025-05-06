1910f37c4e7b52b730673afb2f270fec8a4b348a
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-07 00:52:38 +0530

    new changes

--- Chatgpt practice Questions/Functions and Methods/_1Discountprice.java
+++ Chatgpt practice Questions/Functions and Methods/_1Discountprice.java
@@ -3,14 +3,16 @@
  that returns the price after applying discount. Use it to print bills
   for 5 customers. */
 
-  public class _1Discountprice{
+import java.util.Scanner;
+
+public class _1Discountprice{
 
     public static int calculate_discount(double price,double discountPercent) {//Anything that can be calculated inside, like discountAmount, should be kept inside the method.
-      double 
-
-        
+      double  discountAmount = price * discountPercent/100;
+      return price - discountAmount;
     }
        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
        }
   }
