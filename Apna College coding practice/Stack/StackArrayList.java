da1d47c680ddaf783c6f91054fca2bc4f43d801f
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-06 23:16:31 +0530

    new changes

--- Apna College coding practice/Stack/StackArrayList.java
+++ Apna College coding practice/Stack/StackArrayList.java
@@ -1,13 +1,18 @@
-package Stack;
-import java.util.ArrayList;
+package Stack;//Declares that this class is part of the Stack package.
+import java.util.ArrayList;//Imports the ArrayList class from the Java standard library, which will be used to store stack elements.
 public class StackArrayList {
 
-        static class Stack {
-            static ArrayList<Integer> list = new ArrayList<>();
-            public static boolean isEmpty() {
+        static class Stack {//Defines a nested static class named Stack. This class contains the stack's data and methods.
+            static ArrayList<Integer> list = new ArrayList<>();//Initializes a static ArrayList to hold integer elements of the stack.
+            //This setup is a starting point for implementing a stack data structure using an ArrayList.
+            public static boolean isEmpty() {//: Checks if the stack is empty by verifying if the size of the list is zero.
                 return list.size() ==0;
             }
             //push
+            public static void push (int data)// list ka jo .add function hota hai woh list ke akahri index pr apne data ko kr deta hain . stack ke top pr jo hamara data hota hain woh add hogya .
+              {
+                list.add(data);
+              }
         }
     
 
