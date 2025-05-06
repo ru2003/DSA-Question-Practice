bbb0860ee6915df4d12f4dfaa2103e6fcd77fb2e
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-07 01:28:13 +0530

    new changes

--- Chatgpt practice Questions/Functions/_2LeapYearOrNot.java
+++ Chatgpt practice Questions/Functions/_2LeapYearOrNot.java
@@ -1,7 +1,13 @@
 package Functions ;
-
+/*
+2.Write a function to check if a given year is a leap year or not. */
 public class _2LeapYearOrNot {
+
+    public static boolean leapyear(int year ) {
+       boolean result = ( year % 4 ==0  && year % 100 !=0  || (year % 100 ==0));
+       return result;
+    }
     public static void main(String[] args) {
-        
+
     }
 }
