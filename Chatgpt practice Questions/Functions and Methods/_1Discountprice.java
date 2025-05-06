b6f7a96e2384c9047d2e5500c59cbfdaa8c8986b
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-07 00:54:44 +0530

    new changes

--- Chatgpt practice Questions/Functions and Methods/_1Discountprice.java
+++ Chatgpt practice Questions/Functions and Methods/_1Discountprice.java
@@ -14,5 +14,9 @@ public class _1Discountprice{
        public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        for(int i=0;i<=5;i++) {
+            System.out.println("Customer" + i + ":");
+        }
+
        }
   }
