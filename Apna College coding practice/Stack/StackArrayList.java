1fc9a658d3f7c598352d183c2975da3eaa5ac86a
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-06 23:28:54 +0530

    new changes

--- Apna College coding practice/Stack/StackArrayList.java
+++ Apna College coding practice/Stack/StackArrayList.java
@@ -32,6 +32,7 @@ public class StackArrayList {
            Stack s = new Stack();
            s.push(1);
            s.push(2);
+           s.push(3);
 
            while(!s.isEmpty()) {
             System.out.println(s.peek());
