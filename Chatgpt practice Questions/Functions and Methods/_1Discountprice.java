8f9ecd06268cac63468b0a05ca6ef5977b168903
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-07 00:19:05 +0530

    new changes

---
+++ Chatgpt practice Questions/Functions and Methods/_1Discountprice.java
@@ -0,0 +1,8 @@
+
+/*1. Write a function calculate_discount(price, discount_percent)
+ that returns the price after applying discount. Use it to print bills
+  for 5 customers. */
+
+  public class _1Discountprice{
+    
+  }
