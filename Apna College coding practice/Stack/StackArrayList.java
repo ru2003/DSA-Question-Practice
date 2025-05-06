0923b863f6b7ccd6767f13364dfbc63f294fcf1e
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-06 22:57:25 +0530

    new changes

--- Apna College coding practice/Stack/StackArrayList.java
+++ Apna College coding practice/Stack/StackArrayList.java
@@ -1,11 +1,11 @@
 package Stack;
-
+import java.util.ArrayList;
 public class StackArrayList {
 
-    
+
 
     public static void main(String[] args) {
-
+        static ArrayList<Integer> list = new ArrayList<>();
     }
     
 }
