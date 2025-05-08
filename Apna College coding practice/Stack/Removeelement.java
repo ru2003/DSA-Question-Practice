25fa92d519cd79c99b38e06677837a3a9474e786
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-08 10:29:46 +0530

    new

--- Apna College coding practice/Stack/Removeelement.java
+++ Apna College coding practice/Stack/Removeelement.java
@@ -202,7 +202,13 @@ public class Removeelement {
             Stack s = new Stack();
             s.push(1);
             s.push(2);
-            
+            s.push(3);
+
+
+            while(!s.isEmpty()) {
+              System.out.println(s.peek());
+            }
+
         }
     
 }
