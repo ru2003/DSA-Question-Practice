a8b8274a3d44facad1848f288c3a6d718a05307c
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-08 11:12:47 +0530

    new changes

--- Apna College coding practice/Stack/CollectionFramework.java
+++ Apna College coding practice/Stack/CollectionFramework.java
@@ -1,9 +1,17 @@
 package Stack;
-
+import java.util.*;
 public class CollectionFramework {
 
     public static void main(String[] args) {
-        
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()) {
+            System.out.println(s.peek());
+        }
+    
     }
     
 }
