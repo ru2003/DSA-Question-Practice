47a3a8de149723fdb3cd616cf11bcea86d9467a6
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-08 12:06:45 +0530

    new changes

--- Chatgpt practice Questions/Functions/_3maximumofThree.java
+++ Chatgpt practice Questions/Functions/_3maximumofThree.java
@@ -14,9 +14,8 @@ public class _3maximumofThree{
           
     }
     public static void main(String[] args) {
-           int a = 2;
-           int b = 3;
-           int c = 4;
-           System.out.println(maximofThree());
+           int a =5;
+           int b =6;
+           int c =9;
     }
 }
