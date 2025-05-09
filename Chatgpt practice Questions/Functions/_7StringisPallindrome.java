5c2c4a6eb976b99a200a81013780b274c8eda6d8
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-10 01:31:46 +0530

    new changes

--- Chatgpt practice Questions/Functions/_7StringisPallindrome.java
+++ Chatgpt practice Questions/Functions/_7StringisPallindrome.java
@@ -3,8 +3,16 @@
 public class _7StringisPallindrome {
 
     public static boolean isPallindrome(String str) {
+       
 
-        
+        int left =0;
+        int right=str.length()-1;
+
+        //comparing left and right of both the ends
+        while(left<right){
+            
+        }
+
     }
     public static void main(String[] args) {
       String input = "Madam";
