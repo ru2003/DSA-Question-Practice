99e2fe52ec80f8605e042474cc45c50b276f411a
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-10 00:08:46 +0530

    new change

--- Chatgpt practice Questions/Functions/_5EvenorOdd.java
+++ Chatgpt practice Questions/Functions/_5EvenorOdd.java
@@ -1,7 +1,11 @@
 /*5.Create a function is_prime(number) that returns True/False.
  */
 public class _5EvenorOdd {
-    public static void main(String[] args) {
+
+    public static int  main(int num) {
         
+    }
+    public static void main(String[] args) {
+
     }
 }
