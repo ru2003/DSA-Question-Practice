bf26d49a97ddac476dcc1d6ff6a9a8d88640f180
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-10 01:20:28 +0530

    new changes

--- Chatgpt practice Questions/Functions/_7StringisPallindrome.java
+++ Chatgpt practice Questions/Functions/_7StringisPallindrome.java
@@ -1,7 +1,8 @@
 /* 7.Create a function to check if a string is a palindrome.*/
 
 public class _7StringisPallindrome {
+
     public static void main(String[] args) {
-        
+      String input = "Madam";
     }
 }
