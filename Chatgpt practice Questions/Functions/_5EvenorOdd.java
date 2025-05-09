d8a83e7971e18a0d314477108eb6a569e6e1f5d7
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-10 00:29:36 +0530

    new change

--- Chatgpt practice Questions/Functions/_5EvenorOdd.java
+++ Chatgpt practice Questions/Functions/_5EvenorOdd.java
@@ -1,9 +1,17 @@
 /*5.Create a function is_prime(number) that returns True/False.
+Example:
+For number = 29:
+
+i = 2 to 5 (since √29 ≈ 5.38)
+
+29 is not divisible by 2, 3, 4, or 5
+✅ So, no divisor found → it’s a prime.
  */
 public class _5EvenorOdd {
 
-    public static int  main(int num) {
-         if(num )
+    public static boolean  is_prime(int num) {
+
+         
     }
     public static void main(String[] args) {
 
