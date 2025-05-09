db6acd376a130ec208abcfe24504b9910df841d6
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-09 23:40:30 +0530

    new change

--- Chatgpt practice Questions/Functions/_4vowelsnumber.java
+++ Chatgpt practice Questions/Functions/_4vowelsnumber.java
@@ -4,9 +4,11 @@ public class _4vowelsnumber {
 
     public static int  numberofVowels( String str){
          int count  = 0;
-         str = str.toLowerCase();
+         str = str.toLowerCase();//to handle upper case.
 
          for (int i =0; i<str.length();i++){
+            char ch = str.charAt(i); //get each character.
+
 
          }
 
