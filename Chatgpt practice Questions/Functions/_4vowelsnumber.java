619be4309837bc9abe2bd7d84f5c53eb3272e5e0
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-10 00:07:35 +0530

    new change

--- Chatgpt practice Questions/Functions/_4vowelsnumber.java
+++ Chatgpt practice Questions/Functions/_4vowelsnumber.java
@@ -1,18 +1,21 @@
 /*4.Write a function to return the number of vowels in a string. */
 
 public class _4vowelsnumber {
-
+/*Java passes the value, not the variable name. */
     public static int  numberofVowels( String str){
          int count  = 0;
          str = str.toLowerCase();//to handle upper case.
 
          for (int i =0; i<str.length();i++){
+            /*if (str.charAt(i) == 'a') { -->to get the element at index this is method
+              // do something
+            } */
             char ch = str.charAt(i); //get each character.
+            //We're storing that character in a character variable named ch.
             
             if(ch =='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
                 count++;
             }
-
          }
          return count;
     }
@@ -20,6 +23,7 @@ public class _4vowelsnumber {
     public static void main(String[] args) {
            String input = "hello world";
            int vowelcount = numberofVowels(input);
+           System.out.println(vowelcount);
     }
     
 }
---
+++ Chatgpt practice Questions/Functions/_5EvenorOdd.java
@@ -0,0 +1,5 @@
+/*5.Create a function is_prime(number) that returns True/False.
+ */
+public class _5EvenorOdd {
+    
+}
