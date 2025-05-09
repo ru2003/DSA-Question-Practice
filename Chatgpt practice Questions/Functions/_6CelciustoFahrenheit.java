0cd7d60faf2c9af1af3a0dedd3f68f1f43d71919
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-10 01:08:53 +0530

    new change

--- Chatgpt practice Questions/Functions/_6CelciustoFahrenheit.java
+++ Chatgpt practice Questions/Functions/_6CelciustoFahrenheit.java
@@ -2,9 +2,13 @@
  */
 
 public class _6CelciustoFahrenheit {
-    
+      public static int celciusconvertor(int num) {
+         int result = (num*9/5) +32;
+         return result;
+      }
 
     public static void main(String[] args) {
-        
+         int number = 35;
+         System.out.println(" The tempperature after converting into fahrenheit:"+celciusconvertor(number));
     }
 }
