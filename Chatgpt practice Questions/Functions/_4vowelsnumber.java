b46339183880ec2b71c36521a82623b057d14693
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-09 23:28:14 +0530

    new change

--- Chatgpt practice Questions/Functions/_4vowelsnumber.java
+++ Chatgpt practice Questions/Functions/_4vowelsnumber.java
@@ -3,11 +3,14 @@
 public class _4vowelsnumber {
 
     public static char numberofVowels( char a; char e; char i ; char o; char u ){
-        if(char == 'a' || char == 'e')
+        if(char == 'a' || char == 'e' || char == 'i' || char =='o' , char =='u'){
+            return vowels;
+        }
     }
 
     public static void main(String[] args) {
-
+           char name = 'a';
+           if(n)
     }
     
 }
