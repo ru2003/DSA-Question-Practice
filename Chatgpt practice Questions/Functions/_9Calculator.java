0194462f322a92e2f4394dd1f0260816a7430686
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-10 10:44:45 +0530

    new changes

--- Chatgpt practice Questions/Functions/_9Calculator.java
+++ Chatgpt practice Questions/Functions/_9Calculator.java
@@ -1,9 +1,9 @@
 /*9.Create a calculator using functions: add, subtract, multiply, divide. */
-
+import java.util.Scanner;
 public class _9Calculator {
 
-    public static int calculator(int output) {
-
+    public static double add(double a, double b) {
+        return a+b;
     }
     
     public static void main(String[] args) {
