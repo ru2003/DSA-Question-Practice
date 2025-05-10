709658a8605a76156ced28d85f1c0f50713967a5
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-10 10:45:09 +0530

    new changes

--- Chatgpt practice Questions/Functions/_9Calculator.java
+++ Chatgpt practice Questions/Functions/_9Calculator.java
@@ -5,6 +5,9 @@ public class _9Calculator {
     public static double add(double a, double b) {
         return a+b;
     }
+    public static double substraction(double a, double b) {
+        return a-b;
+    }
     
     public static void main(String[] args) {
 
