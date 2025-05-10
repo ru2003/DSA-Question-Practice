1f4d9609a862d5a02d96c37af002da2cef14bede
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-10 11:02:25 +0530

    new changes

--- Chatgpt practice Questions/Functions/_10nCrCombinationofNumbers.java
+++ Chatgpt practice Questions/Functions/_10nCrCombinationofNumbers.java
@@ -1,7 +1,10 @@
 /* 10.Write a function to compute nCr (combinations) of two numbers.*/
 public class _10nCrCombinationofNumbers {
     
-    public static void main(String[] args) {
+    public static int combinations(int num) {
         
+    }
+    public static void main(String[] args) {
+
     }
 }
