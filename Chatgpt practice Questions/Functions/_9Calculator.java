2ba6fea93fc231de71b9ff3b8396fd27d1b92d3a
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-10 10:49:26 +0530

    new changes

--- Chatgpt practice Questions/Functions/_9Calculator.java
+++ Chatgpt practice Questions/Functions/_9Calculator.java
@@ -11,9 +11,16 @@ public class _9Calculator {
     public static double multiplication(double a, double b) {
         return a*b;
     }
+    public static double division(double a, double b) {
+        if(b == 0) {
+            throw new ArithmeticException("dividion by zero is not allowed");
+        }
+        return a / b ;
+    }
+    
  
     
     public static void main(String[] args) {
-
+       Scanner sc = new Scanner(System.in);
     }
 }
