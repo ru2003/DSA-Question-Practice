e8a246d54a8f98b050d2d1970b9dc0dfe6ebb6ec
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-11 01:06:19 +0530

    new changes

--- Chatgpt practice Questions/Functions/_11StringAnagram.java
+++ Chatgpt practice Questions/Functions/_11StringAnagram.java
@@ -1,12 +1,13 @@
 /* 11.Create a function is_anagram(str1, str2) that checks if two strings are anagrams.
 
  */
+import java.util.Arrays;
 public class _11StringAnagram {
      public static boolean AnagramChecker(String str1, String str2) {
 
         //function to check of strings ara anagrams or not
-        str1 = str1.replceAll("/s" , "").toLowerCase();
-        
+        str1 = str1.replaceAll("/s" , "").toLowerCase();
+
      }
     public static void main(String[] args) {
 
