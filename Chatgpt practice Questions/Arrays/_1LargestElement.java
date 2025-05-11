bcd4edbbad4745f3b98ddc05d5297ae8ef92626d
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-11 11:22:37 +0530

    new changes

--- Chatgpt practice Questions/Arrays/_1LargestElement.java
+++ Chatgpt practice Questions/Arrays/_1LargestElement.java
@@ -1,5 +1,12 @@
 package Arrays;
 /*1.Find the largest element in an array. */
 public class _1LargestElement {
-    
+     
+    public static int largestelement(int[] arr) {
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
