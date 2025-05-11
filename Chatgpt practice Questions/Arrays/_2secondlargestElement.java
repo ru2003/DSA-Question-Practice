767cf3d448ff734c787bb1ff15863cdda764bfac
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-11 12:07:37 +0530

    new changes

--- Chatgpt practice Questions/Arrays/_2secondlargestElement.java
+++ Chatgpt practice Questions/Arrays/_2secondlargestElement.java
@@ -2,6 +2,9 @@ package Arrays;
 /*2.Find the second largest element in an array. */
 public class _2secondlargestElement {
     
+    public static int secondlargest(int[] arr) {
+        
+    }
     public static void main(String[] args) {
         
     }
