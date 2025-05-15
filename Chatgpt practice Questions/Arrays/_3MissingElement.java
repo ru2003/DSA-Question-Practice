73887be61b5bd1d2352510643ae2e7b9bad527ef
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-15 23:32:08 +0530

    new changes

--- Chatgpt practice Questions/Arrays/_3MissingElement.java
+++ Chatgpt practice Questions/Arrays/_3MissingElement.java
@@ -11,8 +11,10 @@ public class _3MissingElement {
             actualSUm += num;
         }
 
+        return expectedSum - actualSUm;
+
     }
     public static void main(String[] args) {
-
+       int[] array = {1,2,3,4,5,6,9,10};
     }
 }
