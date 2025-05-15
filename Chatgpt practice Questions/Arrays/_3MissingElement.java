d829447cfde68725449e68c790b1460104bf91eb
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-15 23:30:20 +0530

    new changes

--- Chatgpt practice Questions/Arrays/_3MissingElement.java
+++ Chatgpt practice Questions/Arrays/_3MissingElement.java
@@ -3,8 +3,9 @@ package Arrays;
 Find the missing number in an array containing numbers from 1 to n (one missing). */
 public class _3MissingElement {
     
-    public static int missingElements(int[] arr) {
-        
+    public static int missingElements(int[] arr , int n)  {
+        int expectedSum = n*(n+1) / 2;
+
     }
     public static void main(String[] args) {
 
