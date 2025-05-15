0ece734c46605560771f9ca8bfe9cecd510d8a4b
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-16 00:03:50 +0530

    new changes

---
+++ Chatgpt practice Questions/Arrays/_5RotateanArray.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class _5RotateanArray {
+    
+    public static int rotationOfArrya(int[] arr) {
+        
+    }
+    public static void main(String[] args) {
+
+    }
+}
