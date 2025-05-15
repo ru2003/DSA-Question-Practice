4da53f3c341479a3922ada102bd2c56070c19486
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-15 23:49:27 +0530

    new changes

--- Chatgpt practice Questions/Arrays/_4DuplicatesNumber.java
+++ Chatgpt practice Questions/Arrays/_4DuplicatesNumber.java
@@ -5,7 +5,12 @@ public class _4DuplicatesNumber {
     public static int duplicates(int[] arr) {
          
         for(int i=0;i<arr.length;i++) {
-            
+            for(int j=i+1;j<arr.length;i++) {
+                if(arr[i] == arr[j]) {
+                     return arr[i];
+                }         
+            }
+            return arr[i];
         }
 
     }
