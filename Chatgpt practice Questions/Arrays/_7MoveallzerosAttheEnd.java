ed08791747eb27df4ab56faa17afa068cfb3555e
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-17 00:00:14 +0530

    new changes

--- Chatgpt practice Questions/Arrays/_7MoveallzerosAttheEnd.java
+++ Chatgpt practice Questions/Arrays/_7MoveallzerosAttheEnd.java
@@ -3,9 +3,15 @@ package Arrays;
 public class _7MoveallzerosAttheEnd {
     public static void moveZeros(int[] arr) {
         int index = 0;
+
+        //step1 : move all non-zero elements to the front
+        for(int  i=0; i<arr.length;i++) {
+            
+        }
+
     }
 
     public static void main(String[] args) {
-
+         
     }
 }
