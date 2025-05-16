d7622fc69778964d592a4625c6c917b9766f874e
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-16 13:37:30 +0530

    new changes

--- Chatgpt practice Questions/Arrays/_5RotateanArray.java
+++ Chatgpt practice Questions/Arrays/_5RotateanArray.java
@@ -1,9 +1,11 @@
 package Arrays;
+/* Rotate an array to the right by k steps.
 
+*/
 public class _5RotateanArray {
     
     public static int rotationOfArrya(int[] arr) {
-        
+
     }
     public static void main(String[] args) {
 
---
+++ Chatgpt practice Questions/Arrays/_6FirstNonRepeating.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class _6FirstNonRepeating {
+    
+    public static int firstNonRepeating(int[] arr) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
