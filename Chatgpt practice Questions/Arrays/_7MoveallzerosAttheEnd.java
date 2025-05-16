8554180ff271f5bb53d078ed14b629ba26e31760
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-17 00:08:39 +0530

    new changes

--- Chatgpt practice Questions/Arrays/_7MoveallzerosAttheEnd.java
+++ Chatgpt practice Questions/Arrays/_7MoveallzerosAttheEnd.java
@@ -25,9 +25,13 @@ public class _7MoveallzerosAttheEnd {
         System.out.println("Before:");
         for(int num :array) {
             System.out.println(num + " ");
+             }
             moveZeros(array);
 
-            System.out.println("\nAfter :");
-        }
+            System.out.println("\nAfter : ");
+            for(int num :array){
+              System.out.println(num + " ");
+            }
+       
     }
 }
