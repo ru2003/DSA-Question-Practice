03a4b06b378eda75d3f80ee62c52ab10f599da73
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-16 20:34:14 +0530

    new changes

--- Chatgpt practice Questions/Arrays/_6FirstNonRepeating.java
+++ Chatgpt practice Questions/Arrays/_6FirstNonRepeating.java
@@ -1,8 +1,16 @@
 package Arrays;
 
+import java.util.HashMap;
+
+/* logic ==>
+ * first count how many times each number appears using hashmap.
+ * then go thorugh the arraya gain and return the first number that appears only once
+ */
 public class _6FirstNonRepeating {
     
     public static int firstNonRepeating(int[] arr) {
+
+        HashMap<Integer , Integer > countMap = new HashMap<>();
 
     }
     public static void main(String[] args) {
