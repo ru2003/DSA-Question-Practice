813dc89466e0f046cec27d68a06a2258bce91d46
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-17 13:37:36 +0530

    new changes

--- Chatgpt practice Questions/Arrays/_8MergeSortedArray.java
+++ Chatgpt practice Questions/Arrays/_8MergeSortedArray.java
@@ -6,15 +6,6 @@ public class _8MergeSortedArray {
  
 
 
-  public static void main(String[] args) {
-       int[] array = {2,5,6,2,7,9};
   
-      System.out.println("Before:"+ Arrays.toString(array));
-
-      //sort the arrya in ascending order
-      Arrays.sort(array);
-
-      System.out.println("after" + Arrays.toString(array));
-      
   }   
-}
+
---
+++ Chatgpt practice Questions/Arrays/sortingarray.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class sortingarray {
+    public static void main(String[] args) {
+       int[] array = {2,5,6,2,7,9};
+  
+      System.out.println("Before:"+ Arrays.toString(array));
+
+      //sort the arrya in ascending order
+      Arrays.sort(array);
+
+      System.out.println("after" + Arrays.toString(array));
+    }
+}
