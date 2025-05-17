aabee5f9bdd657ddcf71abe012988b2d3b4da246
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-17 13:34:09 +0530

    new changes

--- Chatgpt practice Questions/Arrays/_8MergeSortedArray.java
+++ Chatgpt practice Questions/Arrays/_8MergeSortedArray.java
@@ -2,9 +2,14 @@ package Arrays;
 
 import java.util.Arrays;
 public class _8MergeSortedArray {
-    
+
+ 
+
 
   public static void main(String[] args) {
+       int[] array = {2,5,6,2,7,9};
+  
+      System.out.println("Before:"+ Arrays.toString(array));
       
   }   
 }
