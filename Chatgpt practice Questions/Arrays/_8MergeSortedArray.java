df129649d518e9c7ae0eeccd9d7d2e39d48ef4e6
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-18 14:16:08 +0530

    new changes

--- Chatgpt practice Questions/Arrays/_8MergeSortedArray.java
+++ Chatgpt practice Questions/Arrays/_8MergeSortedArray.java
@@ -6,11 +6,11 @@ public class _8MergeSortedArray {
   public static int[] mergingthearray(int[] arr1, int[] arr2) {
        int n = arr1.length;
        int m = arr2.length;
-       int[] merged = new int[n+m];
+       int[] merged = new int[n+m]; //this creates a new array named merged that will store all the elements from both arr1 and arr2.
 
        int i=0, j=0, k=0; 
        //merge both arrays
-       while(i<n && i<m) {
+       while(i<n && j<m) {
         if(arr1[i] < arr2[j]){
           merged[k++] = arr1[i++];
         } else {
@@ -34,7 +34,8 @@ public class _8MergeSortedArray {
     int[] array1 = {1,2,5,8};
     int[] array2 = {3,4,6,7};
     int[] merged = mergingthearray(array1, array2);
-
+   
+    System.out.println("The merges=d Sorted array is: " + Arrays.toString(merged));
   }
 
   
---
+++ Chatgpt practice Questions/Arrays/_9isArraySorted.java
@@ -0,0 +1,8 @@
+package Arrays;
+//Check if an array is sorted (ascending order).
+public class _9isArraySorted {
+    
+    public static void main(String[] args) {
+        
+    }
+}
