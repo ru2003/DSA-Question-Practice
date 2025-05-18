fddf4ce98f32a08808f61b7c21f4ee59b81c4568
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-18 17:14:46 +0530

    new changes

---
+++ Chatgpt practice Questions/Arrays/_10CountElementOccurence.java
@@ -0,0 +1,11 @@
+package Arrays;
+//10.Count the number of occurrences of a specific element in an array.
+public class _10CountElementOccurence {
+    
+    public static int countElement(int[] arr, int key) {
+        
+    }
+    public static void main(String[] args) {
+
+    }
+}
--- Chatgpt practice Questions/Arrays/_9isArraySorted.java
+++ Chatgpt practice Questions/Arrays/_9isArraySorted.java
@@ -3,7 +3,7 @@ package Arrays;
 public class _9isArraySorted {
     
     public static boolean isArraysorted(int[] arr) {
-          for(int i=0; i<arr.length;i++){
+          for(int i=0; i<(arr.length-1);i++){
             if(arr [i] > arr [i+1]){
                 return false;
             }
@@ -13,7 +13,11 @@ public class _9isArraySorted {
     public static void main(String[] args) {
         int[] array = {2,3,4,5,7,10};
         if(isArraysorted(array)){
-            System.out.println("");
+            System.out.println("The array is sorted.");
+        }else{
+            System.out.println("The array is not sorted");
         }
+
+     
     }
 }
