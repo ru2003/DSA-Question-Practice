7579f464895edc2c9d2b4ef12a67abaf5fcf6372
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-18 12:11:29 +0530

    new changes

--- Chatgpt practice Questions/Arrays/_8MergeSortedArray.java
+++ Chatgpt practice Questions/Arrays/_8MergeSortedArray.java
@@ -3,7 +3,7 @@ package Arrays;
 import java.util.Arrays;
 public class _8MergeSortedArray {
  
-  public static void mergingthearray(int[] arr1, int[] arr2) {
+  public static int[] mergingthearray(int[] arr1, int[] arr2) {
        int n = arr1.length;
        int m = arr2.length;
        int[] merged = new int[n+m];
@@ -11,10 +11,22 @@ public class _8MergeSortedArray {
        int i=0, j=0, k=0; 
        //merge both arrays
        while(i<n) {
-        if(arr1[i] < arr2[j]);{
+        if(arr1[i] < arr2[j]){
           merged[k++] = arr1[i++];
+        } else {
+          merged[k++] = arr2[j++];
         }
        }
+
+      //copy remaining elements
+      while(i<n && j<m) {
+        if(arr1[i] < arr2[j]) {
+          merged[k++] = arr2[i++];
+         }
+      }
+      
+      return merged;
+
   }
  
   public static void main(String[] args) {
