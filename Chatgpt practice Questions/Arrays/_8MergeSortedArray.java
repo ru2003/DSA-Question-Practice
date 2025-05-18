8d986e82b7f506bea0d531785495f09ee5685015
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-18 12:02:38 +0530

    new changes

--- Chatgpt practice Questions/Arrays/_8MergeSortedArray.java
+++ Chatgpt practice Questions/Arrays/_8MergeSortedArray.java
@@ -7,6 +7,8 @@ public class _8MergeSortedArray {
        int n = arr1.length;
        int m = arr2.length;
        int[] merged = new int[n+m];
+
+       int i=0; j=0; k=0; 
   }
  
   public static void main(String[] args) {
