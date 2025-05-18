db36d1efce5d7ecef7fac912a83be90babb9c3fb
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-18 16:35:30 +0530

    new changes

--- Chatgpt practice Questions/Arrays/_9isArraySorted.java
+++ Chatgpt practice Questions/Arrays/_9isArraySorted.java
@@ -2,7 +2,15 @@ package Arrays;
 //Check if an array is sorted (ascending order).
 public class _9isArraySorted {
     
+    public static boolean isArraysorted(int[] arr) {
+          for(int i=0; i<arr.length;i++){
+            if(arr [i] > arr [i+1]){
+                return false;
+            }
+          }
+          return true;
+    }
     public static void main(String[] args) {
-        
+
     }
 }
