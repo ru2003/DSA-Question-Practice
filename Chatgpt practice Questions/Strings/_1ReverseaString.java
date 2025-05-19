9d2ddf5b4225a86311827f9b2cd52ae8959dafa8
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-19 22:36:58 +0530

    new changes

--- Chatgpt practice Questions/Strings/_1ReverseaString.java
+++ Chatgpt practice Questions/Strings/_1ReverseaString.java
@@ -2,7 +2,10 @@ package Strings;
 //strings are immutable -once created they cannot be changed.
 public class _1ReverseaString {
     
+    public static String reverseString(String str) {
+
+    }
     public static void main(String[] args) {
-        
+
     }
 }
