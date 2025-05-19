32100d765d970161214c1638476cff3f5780197b
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-19 23:20:54 +0530

    new changes

--- Chatgpt practice Questions/Strings/_2MostFrequentCharacter.java
+++ Chatgpt practice Questions/Strings/_2MostFrequentCharacter.java
@@ -2,7 +2,19 @@ package Strings;
 import java.util.HashMap;
 //2.Find the most frequent character in a string.
 public class _2MostFrequentCharacter {
-    public static void main(String[] args) {
+    public static String freqCount(String str) {
+        HashMap<Character, Integer> freqMap = new HashMap<>();
 
+        //count frequency of each character
+        for(char ch : str.toCharArray()) {
+            freqMap.put(ch,freqMap.getOrDefault(ch,0) +1);
+        }
+        char mostFrequent = ' ' ;
+        int maxCount =0;
     }
+    public static void main(String[] args) {
+         
+        
+    }
+
 }
