31c280bfb4c04d32e0639b8c82b40f8feee24cf5
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-19 23:07:47 +0530

    new changes

--- Chatgpt practice Questions/Strings/_2MostFrequentCharacter.java
+++ Chatgpt practice Questions/Strings/_2MostFrequentCharacter.java
@@ -1,7 +1,8 @@
 package Strings;
+import java.util.HashMap;
 //2.Find the most frequent character in a string.
 public class _2MostFrequentCharacter {
     public static void main(String[] args) {
-        
+
     }
 }
