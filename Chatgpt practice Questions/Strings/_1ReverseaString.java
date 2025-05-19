34e770ec44e0765c1f314cf112bb9b2639175810
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-19 22:40:31 +0530

    new changes

--- Chatgpt practice Questions/Strings/_1ReverseaString.java
+++ Chatgpt practice Questions/Strings/_1ReverseaString.java
@@ -7,8 +7,9 @@ public class _1ReverseaString {
 
             //loop from end to start
             for(int i = str.length()-1;i>=0;i--) {
-                
+                reversed += str.charAt(i);//append character in reverse order.
             }
+            return reversed;
     }
     public static void main(String[] args) {
 
