b721b20b81ba326bc3c7aece0af9f0226bd758c0
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-21 01:44:39 +0530

    new changes

--- Chatgpt practice Questions/Strings/_3CounttheNumberofWords.java
+++ Chatgpt practice Questions/Strings/_3CounttheNumberofWords.java
@@ -2,7 +2,10 @@ package Strings;
 
 //Count the number of words in a given string.
 public class _3CounttheNumberofWords {
-    public static void main(String[] args){
+    public static int countWords(String str) {
         
+    }
+    public static void main(String[] args){
+
     }
 }
