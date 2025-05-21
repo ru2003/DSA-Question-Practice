9e2aadd88d8d1e1868a868184c7ed5d8232d2ef4
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-22 02:41:04 +0530

    new changes

--- Chatgpt practice Questions/Strings/_5StringsareRotation.java
+++ Chatgpt practice Questions/Strings/_5StringsareRotation.java
@@ -2,6 +2,10 @@ package Strings;
 //5.Check if two strings are rotations of each other.
 
 public class _5StringsareRotation {
+
+    public static boolean StringRotation(String str) {
+        
+    }
     
     public static void main(String[] args) {
         
