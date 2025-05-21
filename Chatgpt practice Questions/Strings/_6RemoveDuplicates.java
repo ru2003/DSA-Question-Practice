194a297782e12fa9d97927cafe9bbb1c424565c3
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-22 03:14:15 +0530

    new changes

--- Chatgpt practice Questions/Strings/_6RemoveDuplicates.java
+++ Chatgpt practice Questions/Strings/_6RemoveDuplicates.java
@@ -1,5 +1,13 @@
 package Strings;
 //Remove all duplicate characters from a string.
 public class _6RemoveDuplicates {
+
+    public static String removeDuplicates(String str){
+        
+    }
+  public static void main (String[] args) {
+
+  }
+
     
 }
