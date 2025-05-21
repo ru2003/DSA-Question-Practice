eb74aee186b16ab0a5ce230e2af45d2927a75f3c
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-22 01:49:11 +0530

    new changes

--- Chatgpt practice Questions/Strings/_3CounttheNumberofWords.java
+++ Chatgpt practice Questions/Strings/_3CounttheNumberofWords.java
@@ -9,8 +9,13 @@ public class _3CounttheNumberofWords {
             return 0;
         }
         //split the string by one or more spaces using regex
-        String[] words = str.trim().split("//s+");
-
+        String[] words = str.trim().split("\\s+");
+        //1)  //s ==> split wherever thaeres one or more whitespace characters.
+        // 2) even thoough there were multiple spaces , it doesnt leave epty strings in between - it treats all  the spaces as one big spliter.
+        //3) here \t and  \n are whitespace too
+        //note  ==> if we wrote "s+" ==java would treat it as the letter 's' not whitespace
+        //in java script a single backslash \ is an escape character
+        // so to write a literal backslash in the regex we use double backslash \\s+
         return words.length;
 
     }
