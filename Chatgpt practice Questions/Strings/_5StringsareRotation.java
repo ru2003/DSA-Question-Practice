47f11662e0bc2347d43adc7fef21dbc14be58695
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-22 03:05:33 +0530

    new changes

--- Chatgpt practice Questions/Strings/_5StringsareRotation.java
+++ Chatgpt practice Questions/Strings/_5StringsareRotation.java
@@ -1,10 +1,24 @@
 package Strings;
 //5.Check if two strings are rotations of each other.
+/*
+ *  If we rotate "abcd" left by 2 positions:
+
+    "abcd" → "bcda" → "cdab"
+
+    Now it becomes "cdab" which is equal to s2.
+    So, "cdab" is a rotation of "abcd".
+
+ */
 
 public class _5StringsareRotation {
 
-    public static boolean StringRotation(String str) {
-        
+    public static boolean StringRotation(String str1, String str2) {
+        //if length are not equal , they cant be rotaions
+        if(str1.length() != str2.length()){
+            return false;
+        }
+       
+
     }
     
     public static void main(String[] args) {
