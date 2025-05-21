950e4378ef02767928982915e82aac331e39539d
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-22 02:02:16 +0530

    new changes

---
+++ Chatgpt practice Questions/Strings/_4Capitalizewords.java
@@ -0,0 +1,12 @@
+package Strings;
+//Capitalize the first letter of each word in a string.
+public class _4Capitalizewords {
+    
+    public static int capitalizeWords(String str) {
+        
+    }
+    public static void main(String[] args) {
+
+    }
+    
+}
