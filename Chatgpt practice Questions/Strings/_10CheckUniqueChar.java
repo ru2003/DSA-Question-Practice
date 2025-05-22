49b132b2aec1ca5f70952e669d11c3d0e1c79e07
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-22 22:25:09 +0530

    new changes

--- Chatgpt practice Questions/Strings/_10CheckUniqueChar.java
+++ Chatgpt practice Questions/Strings/_10CheckUniqueChar.java
@@ -1,10 +1,12 @@
 package Strings;
 //10.Check if a string has all unique characters.
-
+import java.util.HashSet;
 public class _10CheckUniqueChar {
 
     public static boolean CheckUniqueChar(String str) {
-        
+
+        HashSet<Character> seen
+    
     }
 
     public static void main(String[] args) {
