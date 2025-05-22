5dcf3e9299e6762edc2daf5e0ee2a878dfeb6ed5
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-22 19:52:06 +0530

    new changes

--- Chatgpt practice Questions/Strings/_7LongestWord.java
+++ Chatgpt practice Questions/Strings/_7LongestWord.java
@@ -3,6 +3,14 @@ package Strings;
 public class _7LongestWord {
     public static String LongestWord(String str) {
          String[] words = str.trim().split("\\s+");
+          
+         String longestword = " ";
+         //treat as a array =>jaise ki array me longest word use kiya jata hai waise hi 
+         //minimum declare krne ka and then anntr mg beta 
+         for(String word:words) {
+            if(word.length() > )
+         }
+
     }
     public static void main(String[] args) {
 
