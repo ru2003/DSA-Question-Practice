e7458d8abdf58b3103523c16d319ebeabbd8e0de
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-22 19:47:38 +0530

    new changes

--- Chatgpt practice Questions/Strings/_7LongestWord.java
+++ Chatgpt practice Questions/Strings/_7LongestWord.java
@@ -2,7 +2,7 @@ package Strings;
 
 public class _7LongestWord {
     public static String LongestWord(String str) {
-        
+         String[] words = str.trim().split("\\s+");
     }
     public static void main(String[] args) {
 
