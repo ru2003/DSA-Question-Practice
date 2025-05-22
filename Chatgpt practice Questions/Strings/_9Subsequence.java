7e27cb7e9a28b4c825c964f8d0e4e09d73ed2662
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-22 19:59:45 +0530

    new changes

---
+++ Chatgpt practice Questions/Strings/_9Subsequence.java
@@ -0,0 +1,12 @@
+package Strings;
+//9.Check if one string is a subsequence of another.
+public class _9Subsequence {
+    public static String subsequence(String str) {
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+    
+}
