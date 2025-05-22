73388870cd33f20e572eda0681dea3c28a9893cf
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-22 19:16:40 +0530

    new changes

--- Chatgpt practice Questions/Strings/_6RemoveDuplicates.java
+++ Chatgpt practice Questions/Strings/_6RemoveDuplicates.java
@@ -1,9 +1,15 @@
 package Strings;
+
+import java.util.LinkedHashSet;
+
 //Remove all duplicate characters from a string.
 public class _6RemoveDuplicates {
 
     public static String removeDuplicates(String str){
-        
+
+        //step :1 create a linkedHashset to store character in order
+        LinkedHashSet<Character> set = new LinkedHashSet<>();
+
     }
   public static void main (String[] args) {
 
