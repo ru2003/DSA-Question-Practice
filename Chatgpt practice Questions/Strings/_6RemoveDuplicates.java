0a5dba34fe653d04dd6f49924c00e7d917a6ebc2
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-22 19:45:15 +0530

    new changes

--- Chatgpt practice Questions/Strings/_6RemoveDuplicates.java
+++ Chatgpt practice Questions/Strings/_6RemoveDuplicates.java
@@ -8,15 +8,16 @@ public class _6RemoveDuplicates {
     public static String removeDuplicates(String str){
 
         //step :1 create a linkedHashset to store character in order
-        LinkedHashSet<Character> set = new LinkedHashSet<>();
-
+        LinkedHashSet<Character> set = new LinkedHashSet<>(); //a "Set" in java only stores unique values so if u dont try to add the same character twice,it will keep only first one.
+         //LinkedhashSet ==> insted of normal hashSet because it maintains the insertioni order (so the output looks natural.)
         //step 2: Loop through each character of the String
         for( char ch: str.toCharArray()) {
             set.add(ch);//if ch already exists in set , it wont be added again.
+            //duplicates are automaticallly ignored.
         }
 
         //step 3: build the result string with unique character
-        StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder(); //used to build the string
         for(char ch:set) {
             result.append(ch);
         }
@@ -34,5 +35,13 @@ public class _6RemoveDuplicates {
         System.out.println("String after removing duplicates: " + output);
   }
 
-    
+    /*
+     ** Regular String	           ** StringBuilder
+        Immutable	                   Mutable
+    Slower for repeated ops   	Faster for repeated ops
+    Creates new objects      	Modifies the same object
+   Not memory-efficient	           Memory-efficient
+
+
+     */
 }
---
+++ Chatgpt practice Questions/Strings/_7LongestWord.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class _7LongestWord {
+    public static String LongestWord(String str) {
+        
+    }
+    public static void main(String[] args) {
+
+    }
+    
+}
