00fca515e7bcda5122702f42686e277405e7adde
ru2003 <ruchitanchaudhari145@gmail.com>
2025-05-22 19:57:42 +0530

    new changes

--- Chatgpt practice Questions/Strings/_8CompresstheString.java
+++ Chatgpt practice Questions/Strings/_8CompresstheString.java
@@ -1,5 +1,13 @@
 package Strings;
 //8.Compress the string (e.g., "aaabbcc" → "a3b2c2")
 public class _8CompresstheString {
+
+    public static String CompresstheString(String str) {
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
     
 }
