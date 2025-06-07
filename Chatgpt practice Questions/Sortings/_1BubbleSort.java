f2c380281c5fcf4feb405281abd223558c3039f9
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-07 23:33:47 +0530

    new changes

--- Chatgpt practice Questions/Sortings/_1BubbleSort.java
+++ Chatgpt practice Questions/Sortings/_1BubbleSort.java
@@ -3,6 +3,9 @@ package Sortings;
 public class _1BubbleSort {
     
     public static void main(String[] args) {
+        int n = arr.length;
+        boolean swapped;
+         
         
     }
 }
