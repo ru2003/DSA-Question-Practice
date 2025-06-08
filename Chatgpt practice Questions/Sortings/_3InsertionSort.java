cd801d0def040ffcd67e0151049eb0b99ad07385
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-08 14:01:21 +0530

    new changes

--- Chatgpt practice Questions/Sortings/_3InsertionSort.java
+++ Chatgpt practice Questions/Sortings/_3InsertionSort.java
@@ -3,6 +3,11 @@ package Sortings;
 public class _3InsertionSort {
     int n = arr.length;
 
+    for (int i=1;i<n;i++){
+        int current = arr[i];
+        int j=i-1;
+    }
+
 
     public static void insertionSort(int[] arr){
 
