6f471081d7a1d05986d7998433fe003495e2a905
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-08 15:50:33 +0530

    new changes

--- Chatgpt practice Questions/Sortings/_3InsertionSort.java
+++ Chatgpt practice Questions/Sortings/_3InsertionSort.java
@@ -1,26 +1,41 @@
 package Sortings;
 
 public class _3InsertionSort {
-    int n = arr.length;
+    public static void insertioniSort(int[] arr) {
+         int n = arr.length;
 
-    for (int i=1;i<n;i++){
-        int current = arr[i];
-        int j=i-1;
+        for (int i = 1; i < n; i++) {
+            int current = arr[i];   // The element we want to insert
+            int j = i - 1;
 
-        //shift elements of arr[0..i-1] that are greater than current
+            // Shift elements of arr[0..i-1] that are greater than current
+            while (j >= 0 && arr[j] > current) {
+                arr[j + 1] = arr[j];  // Shift right
+                j--;
+            }
 
-        while(j >= 0 && arr[j] > current) {
-            arr[j+1] = arr[j];
-            j--;
+            arr[j + 1] = current; // Insert at the right position
         }
         
 
-
     }
+}
     
 
     public static void insertionSort(int[] arr){
+      int[] arr = {9,4,5,3,2};
 
+      System.out.println("Original Array: ");
+      for(int num:arr) {
+        System.out.println(num+ " ");
+      }
+      insertionSort(arr);
+
+      System.out.println("\n\n Sorted array using Insertion Sort:");
+      for(int num:arr) {
+        System.out.println(num+ " ");
+      }
     }
     
 }
+
