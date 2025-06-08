c7d90843739e5bc049725473d2958413b2bf32fe
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-08 10:08:07 +0530

    new changes

--- Chatgpt practice Questions/Sortings/_2SelectionSort.java
+++ Chatgpt practice Questions/Sortings/_2SelectionSort.java
@@ -1,5 +1,18 @@
 package Sortings;
+/*Selection Sort is a simple sorting algorithm that works by:
 
+Repeatedly finding the minimum element from the unsorted part of the array,
+
+Swapping it with the first unsorted element.
+
+
+ * 
+ */
 public class _2SelectionSort {
+    public static void selectionsort(int[] arr) {
+        
+    }
+
+
     
 }
