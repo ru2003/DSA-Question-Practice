0f891f2b1aacdbd693c9eb1f1bbb646b84203e55
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-08 15:46:00 +0530

    new changes

--- Chatgpt practice Questions/Sortings/_3InsertionSort.java
+++ Chatgpt practice Questions/Sortings/_3InsertionSort.java
@@ -6,8 +6,18 @@ public class _3InsertionSort {
     for (int i=1;i<n;i++){
         int current = arr[i];
         int j=i-1;
-    }
 
+        //shift elements of arr[0..i-1] that are greater than current
+
+        while(j >= 0 && arr[j] > current) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        
+
+
+    }
+    
 
     public static void insertionSort(int[] arr){
 
