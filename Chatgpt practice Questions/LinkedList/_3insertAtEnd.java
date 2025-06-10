40c62e8ce933ff63a4fa8646d8b5d2dc7fb91c26
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-10 19:42:35 +0530

    new changes

--- Chatgpt practice Questions/LinkedList/_3insertAtEnd.java
+++ Chatgpt practice Questions/LinkedList/_3insertAtEnd.java
@@ -1,5 +1,18 @@
 package LinkedList;
-
+//3. Insert a node at the end.
 public class _3insertAtEnd {
+
+
+    class Node{
+        int data;
+        Node next;
+
+        //constructor to create the node
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+
+        }
+    }
     
 }
