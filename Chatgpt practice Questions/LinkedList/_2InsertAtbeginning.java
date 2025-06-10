bd9ccc4f5237de97aca717e8be116674aa8acb16
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-10 18:51:52 +0530

    new changes

--- Chatgpt practice Questions/LinkedList/_2InsertAtbeginning.java
+++ Chatgpt practice Questions/LinkedList/_2InsertAtbeginning.java
@@ -16,9 +16,14 @@ public class _2InsertAtbeginning {
 
     //method to insert a node at the beginning
     public  void insertatthebeginning(int data) {
-        Node newNode = new Node(data);
-        newNode.next = head;
-        head = newNode;
+        Node newNode = new Node(data);//create a new node
+        newNode.next = head;//link new node to current head
+        head = newNode;//make new node the new head
+    }
+
+    //method to print the list
+    public void printlist() {
+        Node current = head;
     }
     
 }
