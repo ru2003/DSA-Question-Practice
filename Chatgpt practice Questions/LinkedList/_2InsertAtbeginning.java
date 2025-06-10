96c0a28baaf5925ee7e61b91a4d6cdc8764f969b
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-10 18:28:02 +0530

    new changes

--- Chatgpt practice Questions/LinkedList/_2InsertAtbeginning.java
+++ Chatgpt practice Questions/LinkedList/_2InsertAtbeginning.java
@@ -3,6 +3,11 @@ package LinkedList;
 class Node{
     int data;
     Node next;
+    
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
 }
 public class _2InsertAtbeginning {
     
