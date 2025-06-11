37f44b27b01decb660bf279b892af10dbc1a4ead
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-11 19:42:36 +0530

    new changes

--- Chatgpt practice Questions/LinkedList/_5DeletefirstNode.java
+++ Chatgpt practice Questions/LinkedList/_5DeletefirstNode.java
@@ -1,7 +1,26 @@
 package LinkedList;
 
 //5. Delete the first node of a linked list.
+
+class Node{
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 public class _5DeletefirstNode {
-    
+     Node head;
+
+     //method to delete the first Node
+     public void deleteFirst() {
+        //if the list is empty do nothing
+        if(head == null){
+            System.out.println("List is already empty.");
+        }
+     }
+
     
 }
