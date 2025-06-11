94d7fa645336cdd57e6abd318e0d4af9554b2947
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-11 10:06:24 +0530

    new changes

--- Chatgpt practice Questions/LinkedList/_3insertAtEnd.java
+++ Chatgpt practice Questions/LinkedList/_3insertAtEnd.java
@@ -17,10 +17,17 @@ class Node{
 
 public class _3insertAtEnd {
 
-          Node head;//head of the list
+        Node head;//head of the list
       public void insertAtEnd(int data) {
         //create a new node with given data
         Node newNode = new Node(data);
+
+        //If the list is empty , make the new node as head
+        if(head == null) {
+            head = newNode;
+            return;
+        }
+        
       }
 
         //linked list class
