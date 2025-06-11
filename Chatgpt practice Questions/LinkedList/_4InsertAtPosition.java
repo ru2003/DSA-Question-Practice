e95b01c22f6cd6b65b407e6f8dadbe970f31b929
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-11 12:50:20 +0530

    new changes

--- Chatgpt practice Questions/LinkedList/_4InsertAtPosition.java
+++ Chatgpt practice Questions/LinkedList/_4InsertAtPosition.java
@@ -1,9 +1,19 @@
 package LinkedList;
 
+
+class Node{
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
 public class _4InsertAtPosition {
 
     public static void main(String[] args){
-        
+
     }
     
 }
