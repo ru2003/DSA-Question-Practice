9827f85b632aa1fd4efcef74263e3ac49656530d
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-11 16:20:57 +0530

    new changes

--- Chatgpt practice Questions/LinkedList/_4InsertAtPosition.java
+++ Chatgpt practice Questions/LinkedList/_4InsertAtPosition.java
@@ -13,6 +13,21 @@ class Node{
 public class _4InsertAtPosition {
      Node head;
      
+     //insert a node at specific position
+     public void inserttheNodeAtPosition(int data , int position){ 
+        Node newNode = new Node(data);  
+        
+     //if position is 0,insert at head   
+    if(position == 0){
+        newNode.next = head;
+        head = newNode;
+        return;
+     }
+
+     //teaverse to the node before the insertion point
+     Node current=head;
+
+     }
 
 
     public static void main(String[] args){
