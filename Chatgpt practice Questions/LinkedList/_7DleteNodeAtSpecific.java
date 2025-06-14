b312260843e92f2536b94a7716fc3c8728b71842
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-14 16:41:41 +0530

    new changes

--- Chatgpt practice Questions/LinkedList/_7DleteNodeAtSpecific.java
+++ Chatgpt practice Questions/LinkedList/_7DleteNodeAtSpecific.java
@@ -5,8 +5,18 @@ class Node {
     int data;
     Node next;
     
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
 }
 public class _7DleteNodeAtSpecific {
+
+    Node head;
+    public void DeleteNode(int position) {
+        
+    }
+
 
     public static void main(String[] args) {
         
