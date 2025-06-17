8270bb63a44133b8160d480b50343fa1e9451950
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-17 08:04:13 +0530

    new changes

--- Chatgpt practice Questions/LinkedList/_9SearchAnElement.java
+++ Chatgpt practice Questions/LinkedList/_9SearchAnElement.java
@@ -10,6 +10,30 @@ class Node{
         this.next = null;
     }
 }
+
+//method to add a new node at the front of the list
 public class _9SearchAnElement {
+    Node head;
+
+    public void push(int data){
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }    
     
+    //1.iterative method to search an element
+    public boolean SearchAnElemnt(int key) {
+
+     Node current = head;
+     while(current != null){
+        if(current.data == key){
+            return true;
+        }
+     }
+     
+     }
+
+
+    }
+
 }
