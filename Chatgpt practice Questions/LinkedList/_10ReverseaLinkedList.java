228090d7dc8d4f503b1ea6c702f7df2107288807
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-18 22:05:50 +0530

    new changes

--- Chatgpt practice Questions/LinkedList/_10ReverseaLinkedList.java
+++ Chatgpt practice Questions/LinkedList/_10ReverseaLinkedList.java
@@ -12,4 +12,34 @@ class Node{
 }
 public class _10ReverseaLinkedList {
     Node head;
+
+    //Add node to front of list
+    public void push(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+    //iterative reverse method 
+    public void reverse() {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+
+        while (current != null){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+    //print the linked list 
+    public void printlist(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data+ " ");
+            temp = temp.next;
+        }
+    }
 }
