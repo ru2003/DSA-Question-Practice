6d5734cbf471ff6917d44c1c882e2007049bb5fe
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-18 09:37:37 +0530

    new changes

--- Chatgpt practice Questions/LinkedList/_10ReverseaLinkedList.java
+++ Chatgpt practice Questions/LinkedList/_10ReverseaLinkedList.java
@@ -1,5 +1,15 @@
 package LinkedList;
 
+class Node{
+    int data;
+    Node next;
+
+    public  Node(int data){
+        this.data = data;
+        this.next = null;
+
+    }
+}
 public class _10ReverseaLinkedList {
     
 }
