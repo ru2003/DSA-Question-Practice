fd755d20e351b8e75f6017d179c4dbc550b36974
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-19 15:04:52 +0530

    new changes

--- Chatgpt practice Questions/LinkedList/_11MiddleofLinkedList.java
+++ Chatgpt practice Questions/LinkedList/_11MiddleofLinkedList.java
@@ -1,6 +1,19 @@
 package LinkedList;
 //11. Find the middle of the linked list.
 //(Use slow and fast pointer approach)
+
+
+class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        
+    }
+}
 public class _11MiddleofLinkedList {
     
+
+    public static void main(String[] args) {
+
+    }
 }
