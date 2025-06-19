35f84c6b4ce47bf013221c1e6e0044df483a99fb
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-19 18:22:12 +0530

    new changes

--- Chatgpt practice Questions/LinkedList/_14PallindromeorNot.java
+++ Chatgpt practice Questions/LinkedList/_14PallindromeorNot.java
@@ -1,5 +1,14 @@
 package LinkedList;
 //checke whether the linked list is pallindrome or not
+class Node{
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
 public class _14PallindromeorNot {
     
 }
