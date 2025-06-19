de5fe53f64e9134d9dcc68a930d7790a6225578a
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-19 18:11:49 +0530

    new changes

--- Chatgpt practice Questions/LinkedList/_13RemoveaLinkedlist.java
+++ Chatgpt practice Questions/LinkedList/_13RemoveaLinkedlist.java
@@ -1,5 +1,19 @@
 package LinkedList;
 //13. Remove a loop in a linked list.
+
+class Node{
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
 public class _13RemoveaLinkedlist {
+
+    public static void main(String[]args){
+
+    }
     
 }
