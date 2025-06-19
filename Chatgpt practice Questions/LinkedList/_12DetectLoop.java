90a2bb7671ef1a05854eebe168f1db1246a4b67f
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-19 18:03:11 +0530

    new changes

---
+++ Chatgpt practice Questions/LinkedList/_12DetectLoop.java
@@ -0,0 +1,22 @@
+package LinkedList;
+/*12. Detect a loop in a linked list.
+(Floyd’s Cycle Detection Algorithm) */
+
+class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data = data;
+    }
+
+}
+    
+
+public class _12DetectLoop {
+
+
+    public static void main(String[] args){
+
+    }
+    
+}
