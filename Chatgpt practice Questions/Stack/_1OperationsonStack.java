eab865166506b15dccce8225dafdc3327eded436
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-20 08:34:02 +0530

    new changes

--- Chatgpt practice Questions/Stack/_1OperationsonStack.java
+++ Chatgpt practice Questions/Stack/_1OperationsonStack.java
@@ -6,6 +6,10 @@ public class _1OperationsonStack {
     public static void main(String[] args){
         Stack<Integer> stack = new Stack<>();
 
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
     }
     
 }
