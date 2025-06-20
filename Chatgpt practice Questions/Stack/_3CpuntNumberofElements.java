b0ae42bf86538cfce914dee03ee4500bc0a690c2
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-20 10:16:26 +0530

    new changes

--- Chatgpt practice Questions/Stack/_3CpuntNumberofElements.java
+++ Chatgpt practice Questions/Stack/_3CpuntNumberofElements.java
@@ -1,5 +1,8 @@
 package Stack;
 //3. Count the number of elements in a stack
+import java.util.Stack;
 public class _3CpuntNumberofElements {
-    
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+    }
 }
