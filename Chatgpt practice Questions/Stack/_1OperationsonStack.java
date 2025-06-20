9bfbb3fa0914039ce2af2f4d1ef45eb2ba418e4f
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-20 08:33:18 +0530

    new changes

--- Chatgpt practice Questions/Stack/_1OperationsonStack.java
+++ Chatgpt practice Questions/Stack/_1OperationsonStack.java
@@ -3,5 +3,9 @@ package Stack;
 //(Implement these functions manually using array or list) 
 import java.util.Stack;
 public class _1OperationsonStack {
+    public static void main(String[] args){
+        Stack<Integer> stack = new Stack<>();
+
+    }
     
 }
