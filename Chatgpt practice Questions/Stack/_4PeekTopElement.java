34d10b99fdc23a5a84a2b53bd174fb604361fbdc
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-24 14:50:27 +0530

    new

--- Chatgpt practice Questions/Stack/_4PeekTopElement.java
+++ Chatgpt practice Questions/Stack/_4PeekTopElement.java
@@ -2,5 +2,16 @@ package Stack;
 //. Peek the top element of a stack
 import java.util.Stack;
 public class _4PeekTopElement {
+    public static void main(String[] args){
+        Stack<Integer> stack = new Stack<>();
+
+           stack.push(10);
+           stack.push(20);
+           stack.push(30);
+
+           System.out.println("Top element is: " +stack.peek());
+    
+    }
+
     
 }
---
+++ Chatgpt practice Questions/Stack/_5ReverseaStack.java
@@ -0,0 +1,5 @@
+package Stack;
+
+public class _5ReverseaStack {
+    
+}
