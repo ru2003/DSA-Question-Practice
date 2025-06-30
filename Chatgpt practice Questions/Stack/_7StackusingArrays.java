d778e1ae70f8996604fdaca67cdc75d0adcfe41f
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-30 20:41:12 +0530

    new

--- Chatgpt practice Questions/Stack/_7StackusingArrays.java
+++ Chatgpt practice Questions/Stack/_7StackusingArrays.java
@@ -6,11 +6,20 @@ class Stack{
     int maxsize;
     int[] stackArray;
     int top;
-
+    
+    //constructor
     Stack(int size){
         maxsize = size;
         stackArray = new int[maxsize];
         top = -1;
+    }
+
+    //push element onto stack
+    void push(int value) {
+        if(top == maxsize-1){
+            System.out.println("Stack Overflow");
+            return;
+        }
     }
 }
 public class _7StackusingArrays {
