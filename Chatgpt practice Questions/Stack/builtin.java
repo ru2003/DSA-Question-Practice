205c7537d960f56031d574353ad16e65d4082e65
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-30 20:05:05 +0530

    new

--- Chatgpt practice Questions/Stack/builtin.java
+++ Chatgpt practice Questions/Stack/builtin.java
@@ -5,6 +5,8 @@ public class builtin{
     public static void main(String[] args){
        Stack<Integer> st = new Stack<>();
 
-       
+       st.push(100);
+       st.push(200);
+       st.push(300);
     }
 }
