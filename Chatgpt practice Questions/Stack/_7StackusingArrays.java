d596d84094ddda33407f7106887fb528ab953624
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-30 20:48:11 +0530

    new

--- Chatgpt practice Questions/Stack/_7StackusingArrays.java
+++ Chatgpt practice Questions/Stack/_7StackusingArrays.java
@@ -57,5 +57,18 @@ class Stack{
     }
 }
 public class _7StackusingArrays {
+
+    public static void main(String args[]){
+        Stack stack = new Stack(5);
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);//output stack: 10 20 30 40
+
+        System.out.println("Top: " + stack.peek());
+
+
+    }
     
 }
