fc9bcde2e52a1df9a04dcb177ca35fecbb3e7bc4
ru2003 <ruchitanchaudhari145@gmail.com>
2025-06-30 20:46:09 +0530

    new

--- Chatgpt practice Questions/Stack/_7StackusingArrays.java
+++ Chatgpt practice Questions/Stack/_7StackusingArrays.java
@@ -20,6 +20,40 @@ class Stack{
             System.out.println("Stack Overflow");
             return;
         }
+        stackArray[++top] = value;
+    }
+
+    //pop elment from stack
+    int pop() {
+        if(isEmpty()) {
+            System.out.println("Stack Underflow");
+            return -1;
+        }
+        return stackArray[top--];
+    }
+
+    //peek at the top elment
+    int peek() {
+        if(isEmpty()) {
+            System.out.println("Stack is Empty: ");
+            return -1;
+        }
+        return stackArray[top];
+    }
+
+
+    //peek if stack is empty
+    boolean isEmpty() {
+        return top == -1;
+    }
+
+    //display stack elment
+    void display() {
+        if(isEmpty()) {
+            System.out.println("Stack is empty");
+            return;
+        }
+        System.out.println();
     }
 }
 public class _7StackusingArrays {
