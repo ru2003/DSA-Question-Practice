2979983ccfa2780b5678a34e850d07c52fd81752
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-01 09:47:43 +0530

    new

--- Chatgpt practice Questions/Stack/_8StackusinLinkedList.java
+++ Chatgpt practice Questions/Stack/_8StackusinLinkedList.java
@@ -1,3 +1,22 @@
+//
+//8. Implement a Stack using Linked List
+/*No fixed size (dynamic memory).
+
+Efficient for push/pop (O(1) time).
+
+Top of stack is the head of the linked list. */
+
+//node class to represent each element in the stack
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
 public class _8StackusinLinkedList {
     
 }
