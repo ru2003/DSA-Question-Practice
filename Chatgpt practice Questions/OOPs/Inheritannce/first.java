448f97b1d9fa36ca1911b68eec402626a9b242f3
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-02 14:26:23 +0530

    new changes

---
+++ Chatgpt practice Questions/OOPs/Inheritannce/first.java
@@ -0,0 +1,18 @@
+package OOPs.Inheritannce;
+
+
+class Animal{
+    void eat(){
+        System.out.println("This animal eats food");
+    }
+}
+
+
+class Dog extends Animal{
+    void bark(){
+        System.out.println("The dog barks.");
+    }
+}
+public class first {
+    
+}
