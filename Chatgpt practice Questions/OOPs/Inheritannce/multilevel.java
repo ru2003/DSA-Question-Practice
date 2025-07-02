68dd92a5fb96863dc9d7dd0ad380c587599fc8c1
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-02 14:44:33 +0530

    new changes

--- Chatgpt practice Questions/OOPs/Inheritannce/multilevel.java
+++ Chatgpt practice Questions/OOPs/Inheritannce/multilevel.java
@@ -1,5 +1,26 @@
 package OOPs.Inheritannce;
 
+
+
+
+class Animal{
+    void eat() {
+        System.out.println("Animal eats food");
+    }
+}
+
+class Dog extends Animal{
+    void bark(){
+        System.out.println("Dog barks");
+    }
+}
+
+class puppy extends Dog{
+    void weep(){
+        System.out.println("Puppy weeps");
+    }
+
+}
 public class multilevel {
-    
+
 }
