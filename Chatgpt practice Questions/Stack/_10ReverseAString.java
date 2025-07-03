f3c51b522e88ff80d05ec3bac7310cfcabd0bffa
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-03 15:38:58 +0530

    new changes

--- Chatgpt practice Questions/Stack/_10ReverseAString.java
+++ Chatgpt practice Questions/Stack/_10ReverseAString.java
@@ -1,3 +1,13 @@
+//Reverse a String using Stack
+/*
+ *- Stack follows LIFO (Last In First Out).
+
+  - So if you push all characters of a string into a stack and then pop them one by one, you get the reverse order.
+
+ 
+ */
+
+
 public class _10ReverseAString {
     
 }
