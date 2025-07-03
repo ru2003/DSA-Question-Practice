6170f78296e3394e7d04f9a18312e63e77ac0cb1
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-03 10:23:03 +0530

    new changes

--- Chatgpt practice Questions/Stack/_9twoStacks.java
+++ Chatgpt practice Questions/Stack/_9twoStacks.java
@@ -1,3 +1,38 @@
+//9. Implement Two Stacks in an Array
+/*
+ * Both stacks do not overflow unless the entire array is full.
+They grow in opposite directions:
+
+Stack 1 grows from left to right
+
+Stack 2 grows from right to left
+
+✅ Real-Life Analogy
+Imagine a single bookshelf 📚 where:
+
+Stack 1 keeps adding books from the left side
+
+Stack 2 adds books from the right side
+
+Both should stop when there is no space left in between
+
+
+ */
+
+ class TwoStacks {
+    int[] arr;
+    int size;
+    int top1, top2;
+
+    //constructor
+    TwoStacks(int n) {
+        size = n;
+        arr = new int[n];
+        top1 = -1;
+        top2 = n;    
+    }
+
+ }
 public class _9twoStacks {
     
 }
