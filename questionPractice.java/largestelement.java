a44e7610d3094cdff1e1f9c6f0f6f1e6f248547e
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-12 21:49:56 +0530

    new changes

--- questionPractice.java/largestelement.java
+++ questionPractice.java/largestelement.java
@@ -2,8 +2,11 @@ public class largestelement{
     public static int arr(int[] arr){
         int max = arr[0];
         for(int i=0;i<arr.length;i++){
-            
+            if(arr[i] > max){
+                max=arr[i];
+            }
         }
+        return max;
 
     }
     public static void main(String[] args){
