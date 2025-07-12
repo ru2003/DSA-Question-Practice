d82f2ce63c77d1e3ffde942d19b278a03f1bce3b
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-12 21:48:40 +0530

    new changes

--- questionPractice.java/largestelement.java
+++ questionPractice.java/largestelement.java
@@ -1,5 +1,12 @@
 public class largestelement{
+    public static int arr(int[] arr){
+        int max = arr[0];
+        for(int i=0;i<arr.length;i++){
+            
+        }
+
+    }
     public static void main(String[] args){
-        
+         
     }
 }
