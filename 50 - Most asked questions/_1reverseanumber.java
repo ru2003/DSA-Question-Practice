c752d8d4ea6865154ef04dd1f70072f9eb3da3ea
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-14 22:44:15 +0530

    new changes

--- 50 - Most asked questions/_1reverseanumber.java
+++ 50 - Most asked questions/_1reverseanumber.java
@@ -1,6 +1,6 @@
 //1. Write a code to reverse a number
-import java.util.Scanner;
 
+import java.util.Scanner;
 public class _1reverseanumber{
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
@@ -11,12 +11,12 @@ public class _1reverseanumber{
          int reversed = 0;
          
          while(number != 0) {
-            int digit  = number %10;
-            reversed = reversed * 10 + digit;
-            number /= 10;
+            int digit = number % 10; //takes the last digit of the number 
+            reversed = reversed * 10 + digit; //shifts the existing reversed number left by one digit and adds the new digit to it.
+            number /= 10;//removes the last digit from the original number by integer division.
          }
 
-         System.out.println("REversed number: " + reversed);
+         System.out.println("Reversed number: " + reversed);
 
     }
 }
---
+++ 50 - Most asked questions/_2Fibonaciiseries.java
@@ -0,0 +1,12 @@
+//2. Write the code to find the Fibonacci series upto the nth term.
+
+public class _2Fibonaciiseries {
+     public static void main (String[] args){
+         int start = 0 ;
+         int end  = 1;
+         int num = 10;
+         for(int i=0;i<=num-1;i++){
+            
+         }
+     }
+}
