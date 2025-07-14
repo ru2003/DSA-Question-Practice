df445d746b4bbc202162fd1b968b6a2c0dba52bc
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-14 17:27:42 +0530

    new changes

--- 50 - Most asked questions/_1reverseanumber.java
+++ 50 - Most asked questions/_1reverseanumber.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 
 public class _1reverseanumber{
     public static void main(String[] args) {
-         
+         Scanner sc = new Scanner(System.in);
+
+         System.out.println("Enter the number to reverse: ");
 
     }
 }
