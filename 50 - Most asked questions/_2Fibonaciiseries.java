27c82013fbfd4d5e6b0fe1af9200d7316629f7ea
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-14 23:29:17 +0530

    new changes

--- 50 - Most asked questions/_2Fibonaciiseries.java
+++ 50 - Most asked questions/_2Fibonaciiseries.java
@@ -2,11 +2,11 @@
 
 public class _2Fibonaciiseries {
      public static void main (String[] args){
-         int start = 0 ;
-         int end  = 1;
+         int firstname = 0 ;
+         int secondname  = 1;
          int num = 10;
          for(int i=0;i<=num-1;i++){
-            
+            System.out.println(s);
          }
      }
 }
