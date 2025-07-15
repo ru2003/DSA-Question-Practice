cfa96a478145148096571255ac291f762ace1bf5
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-16 02:16:58 +0530

    new

--- 50 - Most asked questions/_6PallindromeorNot.java
+++ 50 - Most asked questions/_6PallindromeorNot.java
@@ -1,7 +1,7 @@
 //5. Write code to Check if two strings are Anagram or not
-
+import java.util.Scanner;
 public class _6PallindromeorNot {
     public static void main(String[] args){
-        
+
     }
 }
