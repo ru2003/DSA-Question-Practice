f074e851490645fb6b91415fc1b4887e9c2a7ab4
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-16 01:58:59 +0530

    new

--- 50 - Most asked questions/_5Anagrams.java
+++ 50 - Most asked questions/_5Anagrams.java
@@ -1,6 +1,22 @@
 //5. Write code to Check if two strings are Anagram or not
 public class _5Anagrams {
+
+    public static boolean isAnagram(String str1, String str2){
+        //remove whitespace and covert to lowercase for fairness
+        str1 = str1.replaceAll("\\s" , "").toLowerCase();
+        str2 = str2.replaceAll("\\s","").toLowerCase();
+
+        //if element is of different length remove it 
+        if(str1.length() != str2.length()) {
+           return false;
+        }
+
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+    }
     public static void main(String[]args){
-        
+
     }
 }
