2ac2283d2d372223b7539c1191bd5e417f43c388
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-16 01:47:41 +0530

    new

--- 50 - Most asked questions/_4PerfectNumber.java
+++ 50 - Most asked questions/_4PerfectNumber.java
@@ -1,14 +1,27 @@
 //4. Write code of  Perfect number
+import java.util.Scanner;
 public class _4PerfectNumber {
 
-    public static boolean PerfectNumber(int num) {
-         for(int i=1;i<=num;i++){
+    public static void PerfectNumber(int num) {
+          int count =0;
+         for(int i=1;i<num;i++){
             if(num % i == 0){
-                
+                 count += i;
             }
+         }
+         if(count == num){
+            System.out.println("It is a perfect number.");
+         }
+         else{
+            System.out.println("It is not a perfect number.");
          }
     }
     public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+    
+      System.out.println("Enter a number to check whether it is perfect number or not: ");
+      int num = sc.nextInt();
+       PerfectNumber( num);
 
     }
     
