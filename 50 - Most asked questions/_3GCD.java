460226b433817bda9e04c1b07c20324ce7191300
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-15 21:22:02 +0530

    new changes

--- 50 - Most asked questions/_3GCD.java
+++ 50 - Most asked questions/_3GCD.java
@@ -1,11 +1,18 @@
 //3. Write code of Greatest Common Divisor 
 /*This problem asks to find the Greatest Common Divisor (GCD) of two given numbers. The GCD of two numbers is the largest positive integer that divides both numbers without leaving a remainder. */
+
+import java.util.Scanner;
 public class _3GCD {
 
     public static int findGCD(int a, int b){
-        
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
     public static void main(String[] args){
-         
+         Scanner sc = new Scanner(System.in);
     }
 }
