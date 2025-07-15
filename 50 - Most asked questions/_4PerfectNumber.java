c434080dce16c9a86c4f57d29ba39c837804d6c6
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-16 01:26:25 +0530

    new changes

---
+++ 50 - Most asked questions/_4PerfectNumber.java
@@ -0,0 +1,15 @@
+//4. Write code of  Perfect number
+public class _4PerfectNumber {
+
+    public static boolean PerfectNumber(int num) {
+         for(int i=1;i<=num;i++){
+            if(num % i == 0){
+                
+            }
+         }
+    }
+    public static void main(String[] args){
+
+    }
+    
+}
--- 50 - Most asked questions/_PerfectNumber.java
+++
@@ -1,7 +0,0 @@
-//4. Write code of  Perfect number
-public class _PerfectNumber {
-    public static void main(String[] args){
-        
-    }
-    
-}
