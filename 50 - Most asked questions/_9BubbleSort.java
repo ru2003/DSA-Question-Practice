14f9aeef055fba2e971ac4ce91429d9c8bac35fd
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-16 16:20:59 +0530

    new

--- 50 - Most asked questions/_9BubbleSort.java
+++ 50 - Most asked questions/_9BubbleSort.java
@@ -1,7 +1,11 @@
 //9. Write a code for bubble sort
 public class _9BubbleSort {
-    public static void main(String[] args){
+
+    public static void bubbleSort(int[] arr) {
         
+    }
+    public static void main(String[] args){
+
     }
     
 }
