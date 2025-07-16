179fce00b783f01e3aa45932ba08398e89446136
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-16 12:31:50 +0530

    new

--- 50 - Most asked questions/_6PallindromeorNot.java
+++ 50 - Most asked questions/_6PallindromeorNot.java
@@ -3,6 +3,18 @@ import java.util.Scanner;
 public class _6PallindromeorNot {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
+        System.out.println("Enter a string: ");
+        String s = sc.next();
+        String reversed = "";
+
+        for(int i = s.length()-1;i>=0;i--){
+            reversed +=s.charAt(i);
+        }
+
+        if(s.equalsIgnoreCase(reversed)){
+            System.out.println("It is pallindrome.");
+        }else{
+            System.out.println("It is not pallindrome");
+        }
     }
 }
---
+++ 50 - Most asked questions/_7FrequencyofCharacter.java
@@ -0,0 +1,8 @@
+
+//7. Write code to Calculate frequency of characters in a string
+public class _7FrequencyofCharacter {
+    public static void main(String[] args){
+        
+    }
+    
+}
