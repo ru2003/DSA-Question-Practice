41fb006fc7099ac69ae0855fec9c97eacc16649f
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-16 16:32:42 +0530

    new

--- 50 - Most asked questions/_9BubbleSort.java
+++ 50 - Most asked questions/_9BubbleSort.java
@@ -4,8 +4,31 @@ public class _9BubbleSort {
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
         boolean swapped;
+
+        //outer loop for passes
+        for(int  i=0; i<n-1; i++){
+            swapped = false;
+
+            //inner loop for comparing adjacent elemnts
+            for(int j=0; j<n-1-i; j++) {
+                if(arr[j] > arr[j+1]) {
+                    //swap elements if out of order
+                    int temp = arr[i];
+                    arr[j] = arr[j+1];
+                    swapped = true;
+                }
+            }
+
+            //optimization :stop early if no swaps occured
+            if(!swapped)
+            break;
+        }
     }
     public static void main(String[] args){
+        int[] arr= {5,3,6,3,7};
+        bubbleSort(arr);
+
+        System.out.println("sorted array: ");
 
     }
     
