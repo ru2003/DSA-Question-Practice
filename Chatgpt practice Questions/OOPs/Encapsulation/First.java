7e3eec7ddb5ac247648d09c2cb08887715733804
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-17 17:38:15 +0530

    new changes

--- Chatgpt practice Questions/OOPs/Encapsulation/First.java
+++ Chatgpt practice Questions/OOPs/Encapsulation/First.java
@@ -4,6 +4,32 @@ public class First{
    //Private fields: data hiding
    private String accountHolderName;
    private double balance;
+   
+   //constructor
+   public First(String name, double intialBalance) {
+          this.accountHolderName = name;
+          if(initialBalance > 0) {
+            this.balance = initialBalance;
+          }else{
+            this.balance = 0;
+          }
+   }
+
+   //public getter for name
+   public String getAccountHoldername() {
+    return accountHolderName ;
+   }
+   //public setter for name
+   public void setAccountHolderName(String name){
+    this.accountHolderName = name;
+   }
+//Deposit method with validation
+public void deposit(doubke amount) {
+    if(amount > 0){
+        balance += amount;
+        System.out.println("Amount deposited successfully!");
+    }
+}
 
 
 
