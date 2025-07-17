b0f89d89fd793269334aca2e9ccd9a872cce425f
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-17 10:22:01 +0530

    new

--- 50 - Most asked questions/_10MergeSort.java
+++ 50 - Most asked questions/_10MergeSort.java
@@ -6,6 +6,9 @@ public class _10MergeSort {
     public static void  mergeSort(int[] arr, int left, int right) {
         if(left < right){
             int mid = left + (right - left) / 2;
+
+            mergeSort(arr, left,mid);
+            mergeSort(arr, mid+1,right);
             
         }
     }
