5e6cb6380b60f74ad6aaa977a01fc62d133bf09f
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-17 10:21:15 +0530

    new

--- 50 - Most asked questions/_10MergeSort.java
+++ 50 - Most asked questions/_10MergeSort.java
@@ -1,7 +1,15 @@
-//10. How is the merge sort algorithm implemented?
+//10. How is the merge sort algorithm implemented? 
 
 public class _10MergeSort {
+
+
+    public static void  mergeSort(int[] arr, int left, int right) {
+        if(left < right){
+            int mid = left + (right - left) / 2;
+            
+        }
+    }
     public static void main(String[] args){
-        
+
     }
 }
