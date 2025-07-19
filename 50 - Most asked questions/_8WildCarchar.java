a316192de31bcead57e97d986c053fa9226f4f4b
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-20 01:16:00 +0530

    new changes

--- 50 - Most asked questions/_8WildCarchar.java
+++ 50 - Most asked questions/_8WildCarchar.java
@@ -1,3 +1,6 @@
+//8. Write code to check if two strings match where one string contains wildcard characters
 public class _8WildCarchar {
-    
+    public static void main(String []args){
+        
+    }
 }
