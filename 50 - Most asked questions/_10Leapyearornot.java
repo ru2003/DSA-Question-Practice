3482aac4aa3a863ef9ddab3fd6946a19fe799496
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-20 08:10:43 +0530

    new changes

--- 50 - Most asked questions/_10Leapyearornot.java
+++ 50 - Most asked questions/_10Leapyearornot.java
@@ -1,4 +1,8 @@
 //11. Write to code to check whether a given year is leap year or not.
+import java.util.Scanner;
 public class _10Leapyearornot {
-    
+    public static void main(String[] args){
+
+        
+    }
 }
