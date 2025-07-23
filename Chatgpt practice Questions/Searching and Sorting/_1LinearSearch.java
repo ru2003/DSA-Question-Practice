9d3c6ae636e09eed05101fbdd5ca0014bf5ea32f
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-24 00:58:12 +0530

    new changes

--- Chatgpt practice Questions/Searching and Sorting/_1LinearSearch.java
+++ Chatgpt practice Questions/Searching and Sorting/_1LinearSearch.java
@@ -1,6 +1,20 @@
-
+//1. Find Element in an Array
+ //Given an array and a key, return its index or -1.
 public class _1LinearSearch{
+
+    public static int linearSearc(int[] arr , int key){
+        for(int i=0;i<=arr.length-1 ;i++){
+            if(arr[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args){
+          
+
+        int[] arr = {2,4,5,6,77,65,90};
+        int key = 4;
         
     }
 }
