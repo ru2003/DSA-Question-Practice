202a9905aa2a83f61671c9f65de14f38a5473b55
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-24 08:43:58 +0530

    new changes

--- Chatgpt practice Questions/OOPs/Encapsulation/First1.java
+++ Chatgpt practice Questions/OOPs/Encapsulation/First1.java
@@ -1,8 +1,14 @@
 
 public class First1 {
-    //make variables private:data hiding
+    //step 1: make variables private:data hiding
     private String name;
     private int age;
 
-    
+    //step 2 : Create public methods to access/update data
+    public void setName(String name){
+        
+    }
+
+
+
 }
