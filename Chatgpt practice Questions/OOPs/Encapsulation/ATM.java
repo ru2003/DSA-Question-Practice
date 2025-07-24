0fc8ad77908cd5d1be49438b2fcb6096425bca79
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-24 10:39:11 +0530

    new changes

---
+++ Chatgpt practice Questions/OOPs/Encapsulation/ATM.java
@@ -0,0 +1,16 @@
+// ATM hides all the complex internal logic (data hiding).
+
+//You interact only with a simple interface (buttons/screen).
+
+//It allows only valid operations and prevents misuse (e.g., can't withdraw more than balance).
+
+
+public class ATM {
+
+    //private data (encapsulation)
+    private double balance;
+    public static void main(String[] args){
+
+    }
+    
+}
--- Chatgpt practice Questions/OOPs/Encapsulation/First1.java
+++ Chatgpt practice Questions/OOPs/Encapsulation/First1.java
@@ -1,5 +1,5 @@
 
-public class first {
+ class Student {
     //step 1: make variables private:data hiding
     private String name;
     private int age;
@@ -26,7 +26,7 @@ public class first {
 public class First1{
     public static void main(String[] args){
          //step 3: create an object
-         first s1 = new first();
+         Student s1 = new Student();
 
          //step 4: set and get data using methods
          s1.setName("Ruchita");
@@ -36,3 +36,25 @@ public class First1{
          System.out.println("Student Age: "+ s1.getAge());
     }
 }
+
+/* getter and setters:
+ * 1)getters and setters allowed controlled access,not direct or blind access.
+ * 2)without getter and setter anyone can set anything , but with you controlled wht is allowed.
+ * 3)✅ Top Reasons We Use Getters and Setters:
+ * -->data hiding -> we keep variables private and use public methods (getter/setter) so:
+ * -outside code cant directly access or change the data
+ * -the class itself controls how and when data is modified or viewed
+ * 4)2. ✅ Validation Before Setting
+ * -you can add logic to prevent invalid data
+ * 5)🧠 Read-only / Write-only Access
+   -you can create only getter(no setter) - makes variable read only
+   -you can create only setter(ni getter) - makes variable write only
+ */
+
+ /*
+  * 1)data hiding = protects internal variables from outside misuse
+  2)validation = prevents setting invalid data
+  3)read/write control= allows custom access levels
+  4)flexibility = internal implementation can change without affecting external code
+
+  */
