1ad347b2134460faff21b811b56684310508058a
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-26 08:36:12 +0530

    new changes

---
+++ Chatgpt practice Questions/Searching and Sorting/_2CountOccurence.java
@@ -0,0 +1,19 @@
+/*2. Count Occurrences of an Element
+➤ Count how many times a number appears in an array. */
+
+public class _2CountOccurence {
+
+    public static int count(int[] arr , int target){
+         int count = 0;
+         for(int num : arr){
+            if (arr[i] == target ){
+                count++;
+            }
+         }
+    }
+    public static void main(String []args){
+
+
+    }
+    
+}
