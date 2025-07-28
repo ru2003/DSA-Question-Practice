73dc8abbbc85128d7562907aab276624eac5ae4e
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-28 15:03:56 +0530

    new changes

--- Real world question practice/_2ATMMachine.java
+++ Real world question practice/_2ATMMachine.java
@@ -10,6 +10,8 @@ public class _2ATMMachine {
 
         while(true) {
             System.out.println("\n1. Check Balance\n2. Withdrawn\n3. Deposit\n4. Exit");
+            System.out.println("Choose an option: ");
+            int choice = sc.nextInt();
         }
     }
 }
