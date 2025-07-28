50e20ee4c9eb97887300386a54dc5f4a15d0d203
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-28 14:51:17 +0530

    new changes

--- Real world question practice/_2ATMMachine.java
+++ Real world question practice/_2ATMMachine.java
@@ -1,9 +1,9 @@
 /* Concepts used: if-else, while loop, switch, conditionals
 
 🧠 Use Case: Handle withdrawal, balance check, and deposit using a menu-driven program.*/
-
+import java.util.*;
 public class _2ATMMachine {
     public static void main(String[] args){
-        
+         
     }
 }
