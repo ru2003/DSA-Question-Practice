5d39d0d6a9460035b26a842edbe183ef6fa510a2
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-28 17:33:21 +0530

    new changes

--- Real world question practice/_2ATMMachine.java
+++ Real world question practice/_2ATMMachine.java
@@ -12,6 +12,24 @@ public class _2ATMMachine {
             System.out.println("\n1. Check Balance\n2. Withdrawn\n3. Deposit\n4. Exit");
             System.out.println("Choose an option: ");
             int choice = sc.nextInt();
+
+            switch(choice) {
+                case 1: 
+                System.out.println("Current balance: Rs" + balance);
+                break;
+
+                case 2:
+                System.out.println("Enter amount to withdraw: ");
+                int withdraw = sc.nextInt();
+                if (withdraw <= balance) {
+                    balance -= withdraw;
+                    System.out.println("Withdraw: Rs" + withdraw);
+                }else{
+                    System.out.println("Insufficient Balance.");
+                }
+                break;
+               
+            }
         }
     }
 }
