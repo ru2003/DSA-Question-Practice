39c9af0b5eb435f9a87fad9e6e3f384271594d4e
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-28 15:03:07 +0530

    new changes

--- Real world question practice/_2ATMMachine.java
+++ Real world question practice/_2ATMMachine.java
@@ -6,7 +6,10 @@ import java.util.*;
 public class _2ATMMachine {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
+        int balance = 1000;
 
+        while(true) {
+            System.out.println("\n1. Check Balance\n2. Withdrawn\n3. Deposit\n4. Exit");
+        }
     }
 }
