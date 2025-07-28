b55bdd7fbce990fa8a1b5064f1efd412d6bafb65
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-28 12:43:35 +0530

    new changes

--- Real world question practice/_1vendingMachine.java
+++ Real world question practice/_1vendingMachine.java
@@ -9,5 +9,11 @@ public class _1vendingMachine{
         int item = sc.nextInt();
 
         System.out.println("Enter the quantity: ");
+        int qty = sc.nextInt();
+
+        int price = 0;
+        switch(item){
+            case 1: price = 20; break;
+        }
     }
 }
