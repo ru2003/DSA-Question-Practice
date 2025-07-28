9f26c2b062c06d6db12782568f88e8c9501e484e
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-28 14:43:33 +0530

    new changes

--- Real world question practice/_1vendingMachine.java
+++ Real world question practice/_1vendingMachine.java
@@ -1,13 +1,15 @@
-
 import java.util.*;
+/*Concepts used: switch-case, if-else, loops, arrays
+
+🧠 Use Case: Simulate a vending machine where a user selects a product and quantity, and the machine returns the price. */
 public class _1vendingMachine{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("1.Chips - Rs20\n 2. Choclate - Rs30\n 3.Sode - Rs40");
+        
+        System.out.println("1.Chips - Rs20\n2.Choclate - Rs30\n3.Sode - Rs40");
         System.out.println("Enter item number: ");
         int item = sc.nextInt();
-
+        
         System.out.println("Enter the quantity: ");
         int qty = sc.nextInt();
 
@@ -20,6 +22,7 @@ public class _1vendingMachine{
             System.out.println("Invalid item");
             return ;
         }
+        
         int total = price * qty;
         System.out.println("Total amount to pay: Rs." + total );
     }
---
+++ Real world question practice/_2ATMMachine.java
@@ -0,0 +1,9 @@
+/* Concepts used: if-else, while loop, switch, conditionals
+
+🧠 Use Case: Handle withdrawal, balance check, and deposit using a menu-driven program.*/
+
+public class _2ATMMachine {
+    public static void main(String[] args){
+        
+    }
+}
