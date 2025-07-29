4a647e3d7ad2d3231d2ce413314b625d53d335d6
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-29 09:20:01 +0530

    new changes

--- Real world question practice/_5DuplicatesElemenrFinder.java
+++ Real world question practice/_5DuplicatesElemenrFinder.java
@@ -3,6 +3,6 @@
 🧠 Use Case: Find all duplicates in an array.*/
 public class _5DuplicatesElemenrFinder {
     public static void main(String[] args){
-        
+        int[] arr = {1,2,2,3,4,5,6,5,8};
     }
 }
