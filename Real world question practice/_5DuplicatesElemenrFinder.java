31e738ce1babb17d3485a2a83b2d78ad51537e95
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-29 09:20:35 +0530

    new cahnges

--- Real world question practice/_5DuplicatesElemenrFinder.java
+++ Real world question practice/_5DuplicatesElemenrFinder.java
@@ -4,5 +4,6 @@
 public class _5DuplicatesElemenrFinder {
     public static void main(String[] args){
         int[] arr = {1,2,2,3,4,5,6,5,8};
+        System.out.println("dupliactes Element: ");
     }
 }
