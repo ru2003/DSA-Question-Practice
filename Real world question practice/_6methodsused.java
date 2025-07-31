cd69ba417aeca807799d98145132d6d0b6a2783d
ru2003 <ruchitanchaudhari145@gmail.com>
2025-07-31 08:28:01 +0530

    new changes

--- Real world question practice/_6methodsused.java
+++ Real world question practice/_6methodsused.java
@@ -5,9 +5,39 @@ public class _6methodsused {
      static Scanner sc = new Scanner(System.in);
      static int totalBill = 0;
 
-     
+
 
     public static void main(String[] args){
-          
+        String choice; 
+        
+        do{
+            displayMenu();
+            takeOrder();
+            System.out.println("Do you want to order more? (yes/no): ");
+            choice = sc.next();
+        }while(choice.equalsIgnoreCase("yes"));
+
+            calculateBill();
+
+
+            
 }
+         static void displayMenu(){
+                System.out.println("===MENU====");
+                System.out.println("1.Pizza - 100");
+                System.out.println("2.burger - 150");
+                System.out.println("3.Coke - 200");
+                System.out.println("4.Coffee - 250");
+                System.out.println("================");
+
+            }
+
+
+
+
+
+
+
+
+
 }
