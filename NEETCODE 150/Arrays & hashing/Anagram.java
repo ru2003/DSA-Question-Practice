c2b043aa7909bdda5a520835934657d5d7ebb4d7
ru2003 <ruchitanchaudhari145@gmail.com>
2025-08-01 22:43:37 +0530

    new changes

--- NEETCODE 150/Arrays & hashing/Anagram.java
+++ NEETCODE 150/Arrays & hashing/Anagram.java
@@ -4,6 +4,11 @@ public class Anagram {
 
     public boolean isAnagram(String str1, String str2){
          str1 = str1.replaceAll("\\s","").toLowerCase();
+         str2 = str2.replaceAll("\\s","").toLowerCase();
+
+        if(str1.length() != str2.length()){
+            return true;
+        }
          
     }
   public static void main(String[] args){
