f77240802f7de5984765bb60849b673d5847357f
ru2003 <ruchitanchaudhari145@gmail.com>
2025-08-01 22:46:53 +0530

    new changes

--- NEETCODE 150/Arrays & hashing/Anagram.java
+++ NEETCODE 150/Arrays & hashing/Anagram.java
@@ -12,6 +12,11 @@ public class Anagram {
 
         char[] arr1 = str1.toCharArray();
         char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1,arr2);
          
     }
   public static void main(String[] args){
