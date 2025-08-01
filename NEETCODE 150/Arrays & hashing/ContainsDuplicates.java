d85cef6b3311203746e7b376e5cfdad0903b1ddd
ru2003 <ruchitanchaudhari145@gmail.com>
2025-08-01 20:02:50 +0530

    new changes

--- NEETCODE 150/Arrays & hashing/ContainsDuplicates.java
+++ NEETCODE 150/Arrays & hashing/ContainsDuplicates.java
@@ -15,17 +15,64 @@ Output: false
 /* 2 - approches 
  * 1.brute force -> using 2 loops
  * 2.hashmap approch
+ * 
+ * --->static method belongs to the class .
+ * --->non- static methods belong to the object(instance) of the class
+ * 
+ * the main method is static which means it runs without creating an object of the class.so inside main ,if you try to call a non static method
+ * java doesnt know which object the method should run on - because need an object.
+ * 
+ * 1.make the method static (so it belongs to the class)
+ * 2.create an object(then call the method)
  */
 
+
+/* -----Brute force approch ------
 public class ContainsDuplicates{
     public boolean hasDuplicates(int[] arr){
         for(int i=0;i<arr.length-1;i++){
             for(int j=i+1;j<arr.length;j++){
-
+                if(arr[i] == arr[j]){
+                    return true;
+                }
             }
         }
+        return false;
     }
     public static void main(String[] args){
+
+        int[] arr1 = {1,2,3,4,5,6,6};
+        ContainsDuplicates cd = new ContainsDuplicates();
+        boolean result = cd.hasDuplicates(arr1);
+        System.out.println("Contains Duplicates?: " + result);
+         
+         
           
     }
 }
+    */
+
+//optimised version 
+
+import java.util.HashSet;
+
+public class ContainsDuplicates{
+
+    public boolean hasDuplicates(int[] nums){
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            if(set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+    public static void main(String[] args){
+          
+        int[] arr = {1,2,3,4,5,5,6};
+        
+        
+    }
+}
+
