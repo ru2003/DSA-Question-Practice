8e06beae0a8d52a3b55ff628a53ebf6d8946d913
ru2003 <ruchitanchaudhari145@gmail.com>
2025-08-01 08:28:50 +0530

    new changes

---
+++ Real world question practice/_8bankAccount.java
@@ -0,0 +1,24 @@
+/*
+ * ✅ 3. Bank Account System
+🧠 Simulate a bank account with deposit, withdraw, and check balance.
+
+Use: while loop for menu
+
+switch-case for user actions
+
+Create methods like:
+
+deposit()
+
+withdraw()
+
+checkBalance()
+ */
+
+public class _8bankAccount {
+
+    public static void main(String[] args){
+        
+    }
+    
+}
