a94a1a7dd79138cff566ed8c69946f0b80b97045
ru2003 <ruchitanchaudhari145@gmail.com>
2025-08-01 15:06:27 +0530

    new changes

---
+++ NEETCODE 150/Arrays & hashing/ContainsDuplicates.java
@@ -0,0 +1,31 @@
+/*Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+
+Input: nums = [1, 2, 3, 3]
+
+Output: true
+
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+
+Output: false
+*/
+/* 2 - approches 
+ * 1.brute force -> using 2 loops
+ * 2.hashmap approch
+ */
+
+public class ContainsDuplicates{
+    public boolean hasDuplicates(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j<arr.length;j++){
+
+            }
+        }
+    }
+    public static void main(String[] args){
+          
+    }
+}
