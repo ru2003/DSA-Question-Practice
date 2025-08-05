91dfc46b5ed09946cd6c7463eea95b53452afb69
ru2003 <ruchitanchaudhari145@gmail.com>
2025-08-05 21:08:02 +0530

    new changes

---
+++ Real world question practice/_10BillingSystem.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class _10BillingSystem{
+    public static void main(String[] args){
+        
+    }
+}
---
+++ Real world question practice/_9TicketBooking.java
@@ -0,0 +1,24 @@
+/*
+ * ✅ 5. Simple Ticket Booking System
+🧠 Book tickets for movie/show – limited seats, validate availability, print booking info.
+
+Use: if-else, switch-case for show selection
+
+for loop to simulate multiple users
+
+Methods:
+
+showAvailableShows()
+
+bookTickets(int qty)
+
+printReceipt()
+ 
+ */
+
+ 
+public class _9TicketBooking {
+    public static void main(String[] args){
+
+    }
+}
