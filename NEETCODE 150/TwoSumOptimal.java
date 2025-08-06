2585f89af779b4fe95c6449ee5d6706de1ac3ed1
ru2003 <ruchitanchaudhari145@gmail.com>
2025-08-06 08:20:47 +0530

    new changes

--- NEETCODE 150/TwoSumOptimal.java
+++ NEETCODE 150/TwoSumOptimal.java
@@ -1,7 +1,22 @@
-
-
+/*
+ * time complexity = O(n),
+ * Space complexity = O(n)
+ */
+import java.util.*;
 public class TwoSumOptimal {
+    
+    public int[] twoSum(int [] nums,int target){
+
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int complement = target - nums[i];
+            if (map.containsKey(complement)){
+                return new int[] {map.get(complement),i};
+            }
+
+        }
+    }
     public static void main(String[] args){
-        
+
     }
 }
