2b78233a68a5291a7a8650dff6327e6af4bd19f5
ru2003 <ruchitanchaudhari145@gmail.com>
2025-09-25 14:15:51 +0530

    new changes

---
+++ Final revision - 20-09-2025/Arrays & String/Q1SumOfArray.java
@@ -0,0 +1,14 @@
+
+
+public class Q1SumOfArray{
+
+    public static void main(String[] args){
+        int [] arr = {2,3,4,56,8,90};
+        int sum = 0;
+        for(int i=0;i<arr.length;i++){
+            sum += arr[i];
+        }
+        System.out.println("The addition is= "+ sum);
+
+    }
+}
---
+++ Final revision - 20-09-2025/Arrays & String/Q2MaximumandMinimumElement.java
@@ -0,0 +1,7 @@
+//Find the maximum and minimum element.
+
+public class Q2MaximumandMinimumElement {
+    public static void main(String[] args){
+        
+    }
+}
