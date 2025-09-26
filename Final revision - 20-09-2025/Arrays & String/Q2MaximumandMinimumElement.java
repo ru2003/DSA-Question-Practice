9eb98b504d27a7d8af33a34ef6134b15af189352
ru2003 <ruchitanchaudhari145@gmail.com>
2025-09-26 11:36:12 +0530

    new changes

--- Final revision - 20-09-2025/Arrays & String/Q2MaximumandMinimumElement.java
+++ Final revision - 20-09-2025/Arrays & String/Q2MaximumandMinimumElement.java
@@ -6,8 +6,17 @@ public class Q2MaximumandMinimumElement {
         int[] arr = {3,7,98,78,2,1,90};
         int max = arr[0];
         int min = arr[0];
-        
+         
+        for(int i :arr){
+            if(arr[i]>max){
+                max=arr[i];
+            }
 
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
 
+   
     }
 }
