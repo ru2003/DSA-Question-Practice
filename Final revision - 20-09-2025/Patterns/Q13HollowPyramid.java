de6e3f705193b569e55abba0bf7936c8a1510da3
ru2003 <ruchitanchaudhari145@gmail.com>
2025-09-26 10:50:08 +0530

    new

---
+++ Final revision - 20-09-2025/Patterns/Q13HollowPyramid.java
@@ -0,0 +1,15 @@
+/*
+ * 
+ *    13.Hollow pyramid.
+
+   *
+  * *
+ *   *
+*******
+ */
+public class Q13HollowPyramid {
+    public static void main(String[] args){
+        int rows = 4;
+        
+    }
+}
