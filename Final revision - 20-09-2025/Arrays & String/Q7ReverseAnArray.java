06d507e8d8f2d61a93b9112e1b5bc875aff936bd
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-03 10:52:55 +0530

    new file

--- Final revision - 20-09-2025/Arrays & String/Q7ReverseAnArray.java
+++ Final revision - 20-09-2025/Arrays & String/Q7ReverseAnArray.java
@@ -8,10 +8,28 @@ public class Q7ReverseAnArray {
         
         System.out.println("Enter the number of element in the array: ");
         int n = sc.nextInt();
-
+         System.out.println("enter the element to add in the array: ");
         int[] array = new int[n];
-        
+        for(int i=0;i<n;i++){
+            array[i] = sc.nextInt();
+        }
+        //reverse an array:
+        int start = 0; int end = n-1;
+        while(start<end){
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
 
+            start++;
+            end--;
+
+
+        }
+        //print the reversed the array:
+        System.out.println("Print the reversed array: ");
+        for(int num:array){
+            System.out.print(num  + " ");
+        }
 
 
     }
