9b051a9b95d46e6e8524fc861c26229e94cb0ad4
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-03 10:23:32 +0530

    new file

--- Final revision - 20-09-2025/Arrays & String/Q7ReverseAnArray.java
+++ Final revision - 20-09-2025/Arrays & String/Q7ReverseAnArray.java
@@ -1,3 +1,13 @@
+/*7.Reverse an array. */
+
 public class Q7ReverseAnArray {
-    
+    public static void ReverseAnArray(int[] arr){
+       
+        
+    }
+    public static void main(String[] args){
+        int[] array = {12,23,33,45,89,90};
+
+
+    }
 }
