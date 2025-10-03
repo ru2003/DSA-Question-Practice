5bbf299367d6c317d4209777736580bfd78f1f8d
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-03 10:43:02 +0530

    new file

--- Final revision - 20-09-2025/Arrays & String/Q7ReverseAnArray.java
+++ Final revision - 20-09-2025/Arrays & String/Q7ReverseAnArray.java
@@ -1,12 +1,17 @@
 /*7.Reverse an array. */
 
+import java.util.*;
 public class Q7ReverseAnArray {
-    public static void ReverseAnArray(int[] arr){
-       
-        
-    }
+    
     public static void main(String[] args){
-        int[] array = {12,23,33,45,89,90};
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the number of element in the array: ");
+        int n = sc.nextInt();
+
+        int[] array = new int[n];
+        
+
 
 
     }
