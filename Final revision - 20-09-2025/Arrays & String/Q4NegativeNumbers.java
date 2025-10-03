f17e8db8cefd492397a1867678ceb0df02328aa9
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-03 09:26:58 +0530

    new file

--- Final revision - 20-09-2025/Arrays & String/Q4NegativeNumbers.java
+++ Final revision - 20-09-2025/Arrays & String/Q4NegativeNumbers.java
@@ -5,8 +5,28 @@ import java.util.*;
 public class Q4NegativeNumbers {
     public static void main(String[] args){
          Scanner sc = new Scanner(System.in);
+         System.out.println("Enter the size of the array : ");
          int n  = sc.nextInt();
-         
+
+         int[] arr = new int[n];
+
+         System.out.println("Enter " + n + " elements.");
+         for(int i=0; i<n;i++){
+            arr[i] = sc.nextInt();
+         }
+
+         System.out.println("Arrys elements are : ");
+         for(int i=0;i<n;i++){
+            System.out.print(arr[i] + " ");
+         }
+
+         for(int num : arr){
+            if(num < 0){
+                System.out.println("Negative Numbers are : " + num + " ");
+            }
+         }
+
+
           
     }
 }
---
+++ Final revision - 20-09-2025/Arrays & String/Q5SecondLargestElement.java
@@ -0,0 +1,10 @@
+/*Find the second largest lement */
+
+public class Q5SecondLargestElement {
+    public static void main(String[] args){
+        int Largest =0;
+        int secondLargest =0;
+
+        int[] arr ={2,34,56,8,9};
+    }
+}
