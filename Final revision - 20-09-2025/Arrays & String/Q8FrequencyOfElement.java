627254557dab1d9b3b5d410780be91dca1fb5bd8
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-04 13:59:51 +0530

    new changes

---
+++ Final revision - 20-09-2025/Arrays & String/Q8FrequencyOfElement.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class Q8FrequencyOfElement{
+    public static void main(String[] args){
+     int[] arr = {12,14,78,9,12,45,5,6,7,7,7,78};
+
+     //Use Hashmap to store frequency
+     Map<Integer, Integer> freqMap = new HashMap<>();
+
+
+    }
+}
