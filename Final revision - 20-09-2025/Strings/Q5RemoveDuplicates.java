0843acca712a51e0929a9b3f2dbc52398afa1c81
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-05 18:16:18 +0530

    new changes

---
+++ Final revision - 20-09-2025/Strings/Q5RemoveDuplicates.java
@@ -0,0 +1,23 @@
+package Strings;
+//5. Remove duplicates from a string.
+/*indexOf() is a String method used to find the position (index) of a character or a substring inside a string.
+1️⃣ Finding character position
+String s = "Ruchita";
+
+int index = s.indexOf('h');
+System.out.println(index);
+ * 
+ */
+public class Q5RemoveDuplicates {
+   public static void main(String[] args){
+         
+    String str = "programming";
+    String result ="";
+
+    for(int i=0; i<=str.length()-1 ; i++){
+         char ch = str.charAt(i);
+
+         
+    }
+   } 
+}
