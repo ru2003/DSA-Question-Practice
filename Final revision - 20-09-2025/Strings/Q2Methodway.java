b000d4bf85f314e63ce13cae477275966c9d09f4
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-05 14:28:03 +0530

    new changes

--- Final revision - 20-09-2025/Strings/Q2Methodway.java
+++ Final revision - 20-09-2025/Strings/Q2Methodway.java
@@ -1,5 +1,24 @@
 package Strings;
 
 public class Q2Methodway {
-    public static void main(String[] args)
+
+    public static boolean PallindromeorNot(String str){
+        String reversed = "";
+        for(int i=str.length()-1; i>=0 ; i--){
+            reversed += str.charAt(i);
+        }
+        return str.equals(reversed);
+
+    }
+    public static void main(String[] args){
+
+        String ruchita = "Ruchita";
+        String reverse="";
+
+        if(PallindromeorNot(ruchita)){
+            System.out.println("The given string is pallindrome.");
+        }else{
+            System.out.println("The given string is not the pallindrome.");
+        }
+    }
 }
