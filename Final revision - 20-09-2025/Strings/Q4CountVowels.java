0a9a3ae96d7e191db4fb734ed91027ee29027b95
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-05 14:43:13 +0530

    new changes

---
+++ Final revision - 20-09-2025/Strings/Q4CountVowels.java
@@ -0,0 +1,17 @@
+package Strings;
+//3. Count vowels and consonants.
+
+public class Q4CountVowels {
+    public static int vowelsConsonantsCount(String str){
+
+        int vowels = 0;
+        int consonants =0;
+
+        for(int i=0; i<=str.length()-1 ; i++ ){
+            
+        }
+    }
+    public static void main(String[] args){
+
+    }
+}
