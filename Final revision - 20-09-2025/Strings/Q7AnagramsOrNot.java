6d6230ba2de68b1dc3d531224096f933cae15b7a
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-05 18:50:15 +0530

    new changes

--- Final revision - 20-09-2025/Strings/Q7AnagramsOrNot.java
+++ Final revision - 20-09-2025/Strings/Q7AnagramsOrNot.java
@@ -5,6 +5,11 @@ public class Q7AnagramsOrNot {
           
         String str1 = "silent";
         String str2 = "listen";
+
+
+        //step 1 to remove spces and converting it into lowercase
+        str1 = str1.replaceAll("\\s","").toLowerCase();
+        str2 = str2.replaceAll("\\s","").toLowerCase();
         
 
     }
