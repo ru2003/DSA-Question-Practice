612db20e7ceec788f8400654f972ea6ea7475709
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-05 18:48:08 +0530

    new changes

---
+++ Final revision - 20-09-2025/Strings/Q6NonRepeatingChar.java
@@ -0,0 +1,7 @@
+package Strings;
+//6. Find first non-repeating character.
+public class Q6NonRepeatingChar {
+    public static void main(String[] args){
+        
+    }
+}
---
+++ Final revision - 20-09-2025/Strings/Q7AnagramsOrNot.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class Q7AnagramsOrNot {
+    public static void main(String[] args){
+          
+        String str1 = "silent";
+        String str2 = "listen";
+        
+
+    }
+}
