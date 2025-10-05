b6c07a43e6f05b48cbc11336372b601e490e45cb
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-05 14:51:27 +0530

    new changes

--- Final revision - 20-09-2025/Strings/Q4CountVowels.java
+++ Final revision - 20-09-2025/Strings/Q4CountVowels.java
@@ -2,16 +2,23 @@ package Strings;
 //3. Count vowels and consonants.
 
 public class Q4CountVowels {
-    public static int vowelsConsonantsCount(String str){
+    public static void vowelsConsonantsCount(String str){
 
         int vowels = 0;
         int consonants =0;
 
         for(int i=0; i<=str.length()-1 ; i++ ){
-            
+            //first u have to retrieve the each character and convert that into the lwercase ..
+            char ch =Character.toLowerCase(str.charAt(i));
+            if(ch == 'a' || ch =='e' || ch=='i' || ch=='o' || ch=='u'){
+                vowels++;
+            }else{
+                consonants++;
+            }
         }
     }
     public static void main(String[] args){
+        
 
     }
 }
