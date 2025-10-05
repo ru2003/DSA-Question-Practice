f052dbda758eb3f4ff7cdc0e4108a26f7be929de
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-05 14:17:54 +0530

    new changes

--- Final revision - 20-09-2025/Strings/Q2PallindromeOrNot.java
+++ Final revision - 20-09-2025/Strings/Q2PallindromeOrNot.java
@@ -1,7 +1,14 @@
 package Strings;
 //2. Check if a string is palindrome.
+import java.util.*;
+
 public class Q2PallindromeOrNot {
+
+    
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+       
 
     }
 }
