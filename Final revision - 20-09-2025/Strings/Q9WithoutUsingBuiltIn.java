90a9ee5509b00912a3421f1b101190dd09df9d6b
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-06 13:29:30 +0530

    new changes

--- Final revision - 20-09-2025/Strings/Q9WithoutUsingBuiltIn.java
+++ Final revision - 20-09-2025/Strings/Q9WithoutUsingBuiltIn.java
@@ -3,6 +3,11 @@ package Strings;
 public class Q9WithoutUsingBuiltIn {
     public static void main(String[] args){
         String str = "Ruchita123";
+
+        boolean allDigits = true;
+        boolean allDigita = true;
         
+        
+
     }
 }
