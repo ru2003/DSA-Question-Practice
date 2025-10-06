c61e8db24e5148c320568f8721ac53f217bd74f2
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-06 12:30:02 +0530

    new changes

--- Final revision - 20-09-2025/Strings/Q7AnagramsOrNot.java
+++ Final revision - 20-09-2025/Strings/Q7AnagramsOrNot.java
@@ -1,5 +1,7 @@
 package Strings;
 
+import java.util.Arrays;
+
 public class Q7AnagramsOrNot {
     public static void main(String[] args){
           
@@ -8,9 +10,29 @@ public class Q7AnagramsOrNot {
 
 
         //step 1 to remove spces and converting it into lowercase
-        str1 = str1.replaceAll("\\s","").toLowerCase();
-        str2 = str2.replaceAll("\\s","").toLowerCase();
+        str1 = str1.replaceAll("\\s+","").toLowerCase();
+        str2 = str2.replaceAll("\\s+","").toLowerCase();
+
+        //step 2: check if lengths are equal .
+        if (str1.length() != str2.length()){
+            System.out.println("not Anagrams.");
+            return ;
+        }
         
+        //step 3: convert into char arrays
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        if(Arrays.equals(arr1,arr2)){
+            System.out.println("The String are Anagram.");
+        }else{
+            System.out.println("The strings are not anagrams.");
+        }
+
+
 
     }
 }
---
+++ Final revision - 20-09-2025/Strings/Q8UpperCaseLower.java
@@ -0,0 +1,14 @@
+package Strings;
+//8. Convert string to uppercase/lowercase.
+public class Q8UpperCaseLower {
+    public static void main(String[] args){
+        String str = "Ruchita";
+
+        String Uppercase = str.toUpperCase();
+        String Lowercase = str.toLowerCase();
+
+        System.out.println("UpperCase " + Uppercase);
+         System.out.println("LowerCase " + Lowercase);
+    }
+
+}
