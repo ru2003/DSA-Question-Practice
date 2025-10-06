fbfa327be49c76d0f811c2799625592318293792
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-06 13:37:36 +0530

    new changes

--- Final revision - 20-09-2025/Strings/Q9WithoutUsingBuiltIn.java
+++ Final revision - 20-09-2025/Strings/Q9WithoutUsingBuiltIn.java
@@ -5,12 +5,23 @@ public class Q9WithoutUsingBuiltIn {
         String str = "Ruchita123";
 
         boolean allDigits = true;
-        boolean allDigita = true;
+        boolean allLetter = true;
         
        for(int i=0; i<=str.length()-1; i++){
           char ch = str.charAt(i);
-              
+          
+          if (!Character.isDigit(ch)) {
+            allDigits = true;
+          }
+          if (!Character.isLetter(ch)) {
+            allLetter = false;
+          }
+
        } 
+
+       if (allDigits){
+        System.out.println("String contains only digits.");
+       }
 
     }
 }
