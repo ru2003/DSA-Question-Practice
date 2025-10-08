686af9857d9152b86d43e5a1f7491653fd66f340
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-08 16:43:46 +0530

    new changes

--- Final revision - 20-09-2025/Strings/Q16CountDigitLetterSpecial.java
+++ Final revision - 20-09-2025/Strings/Q16CountDigitLetterSpecial.java
@@ -1,5 +1,5 @@
 package Strings;
-
+//16. Count digits, letters, and special characters in a string.
 import java.util.*;
 public class Q16CountDigitLetterSpecial {
     public static void main(String[] args){
@@ -7,8 +7,23 @@ public class Q16CountDigitLetterSpecial {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the String u want to work on: ");
         String str = sc.nextLine();
+        int digit =0;
+        char ch =0;
+        char specialCharacter = 0;
+
 
         
+
+        if(str.matches("0-9")){
+            digit++;
+        }else if(str.matches("a-zA-Z")){
+            ch++;
+        }else{
+           specialCharacter++;
+        }
+
+        System.out.println("therefore total count of each category is: "+ );
+
         
 
 
