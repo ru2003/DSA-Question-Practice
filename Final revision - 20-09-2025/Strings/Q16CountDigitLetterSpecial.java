d60876d434bb5cdd8c75b5327a148568f8aa2237
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-08 16:27:56 +0530

    new chnages

--- Final revision - 20-09-2025/Strings/Q16CountDigitLetterSpecial.java
+++ Final revision - 20-09-2025/Strings/Q16CountDigitLetterSpecial.java
@@ -1,8 +1,10 @@
 package Strings;
 
+import java.util.*;
 public class Q16CountDigitLetterSpecial {
     public static void main(String[] args){
-
-        
+         
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
     }
 }
