858c7e1a68c38ce593dd5927ced057365e060451
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-08 15:48:45 +0530

    new changes

---
+++ Final revision - 20-09-2025/Strings/Q15FirstlettertoUppercase.java
@@ -0,0 +1,14 @@
+package Strings;
+//
+//15. Convert each word’s first letter to uppercase (title case).
+import java.util.*;
+
+public class Q15FirstlettertoUppercase {
+    public static void main(String[] args){
+           Scanner sc = new Scanner(System.in);
+
+           String str = sc.nextLine();
+           
+
+    }
+}
