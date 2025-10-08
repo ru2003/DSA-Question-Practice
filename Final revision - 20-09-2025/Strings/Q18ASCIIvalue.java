addb09412709c50ac95cbbe672f7e93afd66892c
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-08 17:57:39 +0530

    new addition

--- Final revision - 20-09-2025/Strings/Q18ASCIIvalue.java
+++ Final revision - 20-09-2025/Strings/Q18ASCIIvalue.java
@@ -4,5 +4,16 @@ import java.util.*;
 public class Q18ASCIIvalue {
     public static void main(String[] args){
         
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String to convert into its ASCII => valueL: ");
+
+        String str = sc.nextLine();
+        for(int i=0; i<=str.length()-1; i++){
+            char ch = str.charAt(i);
+
+            int ASCIvalue = ch;
+            System.out.println("ASCII values are: " + ch + "=" +  ASCIvalue);
+        }
+
     }
 }
