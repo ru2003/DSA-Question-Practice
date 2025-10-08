dcaadc665b7e63830fe2c38be45fc20d408fef83
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-08 16:00:50 +0530

    new changes

--- Final revision - 20-09-2025/Strings/Q15FirstlettertoUppercase.java
+++ Final revision - 20-09-2025/Strings/Q15FirstlettertoUppercase.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Q15FirstlettertoUppercase {
     public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
-
+           System.out.println("enter the String u want to add: ");
            String str = sc.nextLine();
 
            //split the string into words based on space
@@ -17,10 +17,11 @@ public class Q15FirstlettertoUppercase {
               //convert first letter to capital and rest to lowercase
 
               String capitalized = word.substring(0,1).toUpperCase() + word.substring(1).toLowerCase();
-              result += capitalized;
-              
-
+              result += capitalized + " ";
            }
+
+           //print the result
+           System.out.println("Title Case:  "+ result.trim());
 
 
 
