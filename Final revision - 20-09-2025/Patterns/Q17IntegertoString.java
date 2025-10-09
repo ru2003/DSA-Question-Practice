75d7fa9f1d4b169f73b1ac30ccdbadd367583d3f
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-09 09:48:54 +0530

    new changes

--- Final revision - 20-09-2025/Patterns/Q17IntegertoString.java
+++
@@ -1,8 +0,0 @@
-
-
-
-public class Q17IntegertoString {
-    public static void main(String[] args){
-        
-    }    
-}
---
+++ Final revision - 20-09-2025/Strings/Q17IntegertoString.java
@@ -0,0 +1,32 @@
+package Strings;
+//17. Convert a number in string form to integer (without using Integer.parseInt).
+
+import java.util.*;
+
+public class Q17IntegertoString {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+       System.out.println("enter the string of Number: ");
+       
+       String str = sc.nextLine();
+       int number = 0;
+
+       //chaeck if its a valid digit
+       for(int i=0;i<=str.length()-1 ;i++){
+        char ch = str.charAt(i);
+
+        if(ch>='0' && ch<='9'){
+            number = number*19+(ch-'0');
+        }else{
+            System.out.println("Invalid character found: "+ch);
+            return;
+        }
+       }
+
+
+
+
+       
+
+    }    
+}
