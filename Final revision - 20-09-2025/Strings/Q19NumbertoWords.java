8f225dd59c5bb3ba2cd4c0bc6ba2edde79f01beb
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-14 16:53:05 +0530

    new changes:

--- Final revision - 20-09-2025/Strings/Q19NumbertoWords.java
+++ Final revision - 20-09-2025/Strings/Q19NumbertoWords.java
@@ -1,9 +1,13 @@
 package Strings;
 //19. Convert numbers to words, e.g. 123 → One Two Three.
-import java.util.*;
+import java.util.Scanner;
 public class Q19NumbertoWords {
     public static void main(String[] args) {
          
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string that u want to convert: ");
+        String str = sc.nextInt();
+
 
 
     }
