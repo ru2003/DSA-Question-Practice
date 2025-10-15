56c1fccba170fa309fd82177e9eb09d26eb831bd
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-15 09:21:20 +0530

    new

--- Final revision - 20-09-2025/Strings/Q21CountOccurenceofEchword.java
+++ Final revision - 20-09-2025/Strings/Q21CountOccurenceofEchword.java
@@ -1,5 +1,8 @@
 package Strings;
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+//21. Count occurrences of each character.
 public class Q21CountOccurenceofEchword {
     public static void main(String[] args){
         
