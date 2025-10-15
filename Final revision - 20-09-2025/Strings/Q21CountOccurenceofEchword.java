c2729e0bf8e065256429651fe62e51c47dccd085
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-15 09:22:54 +0530

    new

--- Final revision - 20-09-2025/Strings/Q21CountOccurenceofEchword.java
+++ Final revision - 20-09-2025/Strings/Q21CountOccurenceofEchword.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 //21. Count occurrences of each character.
 public class Q21CountOccurenceofEchword {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println("enter a String: ");
+
         
+
     }
 }
