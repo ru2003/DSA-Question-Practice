f1e1637a76b153cb415ae6e962fd1a0cd94b73f0
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-20 16:49:24 +0530

    new changes

--- Final revision - 20-09-2025/Strings/Q22CompressAString.java
+++ Final revision - 20-09-2025/Strings/Q22CompressAString.java
@@ -3,7 +3,19 @@ package Strings;
 public class Q22CompressAString {
      public static void main(String[]args){
         String input ="aaabbbssrrr";
+        String compressed  = compressString(input);
+
+        System.out.println("Compressed String: "+ compressed);
+
+     }
+     
+     public static String compressString(String str){
+        //if string is empty , return it
+        if(str == null || str.isEmpty()){
+            return str;
+        }
+
+        StringBuilder result = new StringBuilder();
         
      }
-    
 }
