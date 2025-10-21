d1a8ed17612b3f553acd65418b728584f275c165
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-21 08:58:58 +0530

    new changes

---
+++ Final revision - 20-09-2025/Strings/Q27CountPallindromicSubString.java
@@ -0,0 +1,5 @@
+package Strings;
+//27. Count palindromic substrings.
+public class Q27CountPallindromicSubString {
+    
+}
---
+++ Final revision - 20-09-2025/Strings/Q28Implementstrstr.java
@@ -0,0 +1,5 @@
+package Strings;
+//28. Implement basic strstr() function.
+public class Q28Implementstrstr {
+    
+}
