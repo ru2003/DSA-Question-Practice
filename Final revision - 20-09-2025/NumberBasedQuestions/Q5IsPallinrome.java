3a79772923cc6a91c18bb54ab33f8b09f096c5a8
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-25 23:56:56 +0530

    new addition

--- Final revision - 20-09-2025/NumberBasedQuestions/Q5IsPallinrome.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q5IsPallinrome.java
@@ -1,5 +1,10 @@
 package NumberBasedQuestions;
+//5. Check if a number is palindrome.
+import java.util.Scanner;
+
 
 public class Q5IsPallinrome {
-    
+    public static void main(String[] args){
+        
+    }
 }
