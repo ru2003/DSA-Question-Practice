69304f515ab9894d95ca2f05a2ab781739ff65d4
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-26 00:32:10 +0530

    new addition

--- Final revision - 20-09-2025/NumberBasedQuestions/Q4ArmStrongNum.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q4ArmStrongNum.java
@@ -1,10 +1,29 @@
 package NumberBasedQuestions;
 import java.util.*;
 public class Q4ArmStrongNum {
+     public static boolean isArmStrongNumber(int num){
+        int temp = num;
+        int count =0;
+         while( temp != 0){
+          temp = temp /10;
+          count++;
+        }
+        
+
+     }
     public static void main(String[] args){
        Scanner sc = new Scanner(System.in);
        int num = sc.nextInt();
-       
+       /*
+        * temp % 10 se last digit milega (agar use karna ho)
+          temp / 10 se last digit remove hota hai
+        */
+
+        if(isArmStrongNumber(num)){
+          System.out.println("The given number is ArmStrong Number.");
+        }else{
+          System.out.println("The given number is not the ArmStrong Number.");
+        }
 
     }
 }
