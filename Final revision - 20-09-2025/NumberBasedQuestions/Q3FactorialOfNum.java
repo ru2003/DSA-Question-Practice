aa4bff2e5acae650f3be7aa1e975624706347350
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-25 23:52:55 +0530

    new addition

--- Final revision - 20-09-2025/NumberBasedQuestions/Q3FactorialOfNum.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q3FactorialOfNum.java
@@ -1,5 +1,13 @@
 package NumberBasedQuestions;
 
 public class Q3FactorialOfNum {
-    
+    public static void main(String[] args){
+        int num = 9;
+        int factorial = 1;
+        
+        for(int i=1; i<=9; i++){
+            factorial *= i;
+        }
+        System.out.println("The factorial of the number is "+ factorial);
+    }
 }
