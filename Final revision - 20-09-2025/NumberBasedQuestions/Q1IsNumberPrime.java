7cc5a42d7606dfd281501eaa8b57ac3ce0acd876
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-25 22:50:22 +0530

    new addition

--- Final revision - 20-09-2025/NumberBasedQuestions/Q1IsNumberPrime.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q1IsNumberPrime.java
@@ -1,6 +1,9 @@
 package NumberBasedQuestions;
+import java.util.Scanner;
 public class Q1IsNumberPrime{
     public static void main(String[] args){
-
+         
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
     }
-}
+} 
