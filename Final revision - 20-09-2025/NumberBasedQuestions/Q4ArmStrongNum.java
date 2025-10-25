172b91935ef0a5a3ae6a6104a7ebc1ec824a48e5
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-26 00:13:33 +0530

    new addition

--- Final revision - 20-09-2025/NumberBasedQuestions/Q4ArmStrongNum.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q4ArmStrongNum.java
@@ -1,7 +1,10 @@
 package NumberBasedQuestions;
-
+import java.util.*;
 public class Q4ArmStrongNum {
     public static void main(String[] args){
-        
+       Scanner sc = new Scanner(System.in);
+       int num = sc.nextInt();
+       
+
     }
 }
