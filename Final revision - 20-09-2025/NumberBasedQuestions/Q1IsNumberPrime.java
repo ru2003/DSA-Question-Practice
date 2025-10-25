0be9c50bdd2bacdd15a7a96b50893bd1593051c4
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-25 23:00:05 +0530

    new addition

--- Final revision - 20-09-2025/NumberBasedQuestions/Q1IsNumberPrime.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q1IsNumberPrime.java
@@ -5,5 +5,24 @@ public class Q1IsNumberPrime{
          
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
+
+        if(isPrime(num)){
+            System.out.println("The given number is prime number.");
+        }else{
+            System.out.println("The given number is not the prime number.");
+        }
+
+        public static boolean isPrime(int number){
+              if(number<=1){
+                return false;
+              }
+
+              for(int i=2;i<=math.sqrt(number);i++){
+                  
+              }
+              
+
+
+        }
     }
 } 
