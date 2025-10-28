c955d27cd3de138df4d3bd9f878946b94ad4ecee
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-28 12:11:08 +0530

    new changes

--- Final revision - 20-09-2025/NumberBasedQuestions/Q4ArmStrongNum.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q4ArmStrongNum.java
@@ -12,13 +12,24 @@ public class Q4ArmStrongNum {
         temp = num ;
 
         int sum  = 0;
-        
+        while(temp != 0){
+          int digit = temp %10;
+          sum += Math.pow(digit,count);
+          temp = temp/10;
+        }
+        if(sum == num){
+          return true;
+        }else{
+          return false;
+        }
+
         
 
 
      }
     public static void main(String[] args){
        Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the number to check whether is it Armstrong number or not: ");
        int num = sc.nextInt();
        /*
         * temp % 10 se last digit milega (agar use karna ho)
