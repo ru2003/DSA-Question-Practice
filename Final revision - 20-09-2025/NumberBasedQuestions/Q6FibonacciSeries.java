4cd94f26aa964878705b17692daf72f0a4930e70
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-28 23:47:19 +0530

    new changes

--- Final revision - 20-09-2025/NumberBasedQuestions/Q6FibonacciSeries.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q6FibonacciSeries.java
@@ -6,9 +6,12 @@ public class Q6FibonacciSeries {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println(" Enter the number of series:  ");
+        int n = sc.nextInt();
          int first = 0;
          int second = 1;
-         int n = 5;
+         for(int i = 1;i<=n ;i++){
+             
+         }
 
 
 
