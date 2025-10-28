8eebdc4ae169b0c1838c9436fe96eb5b26de08d0
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-28 11:26:40 +0530

    new changes

--- Final revision - 20-09-2025/NumberBasedQuestions/Q4ArmStrongNum.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q4ArmStrongNum.java
@@ -10,7 +10,11 @@ public class Q4ArmStrongNum {
           count++;
         }
         temp = num ;
+
+        int sum  = 0;
         
+        
+
 
      }
     public static void main(String[] args){
