964634e9ffe7b52c313cf4cb904b458dc5f41ed7
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-29 00:18:59 +0530

    new changes

--- Final revision - 20-09-2025/NumberBasedQuestions/Q8SumOfDigits.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q8SumOfDigits.java
@@ -2,7 +2,12 @@ package NumberBasedQuestions;
 //8. Find the sum of digits of a number.
 public class Q8SumOfDigits {
     public static void main(String[] args){
-        
+
+        int num = 1234;
+        while(num != 0){
+            
+        }
+
     }
     
 }
