e79357f9b7dbaae17aac49ac7fe06e576d1be9a2
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-28 12:16:17 +0530

    new changes

--- Final revision - 20-09-2025/NumberBasedQuestions/Q5IsPallinrome.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q5IsPallinrome.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 
 
 public class Q5IsPallinrome {
+
+
+    public static boolean checktheNum(int num){
+      while(num != 0){
+        
+      }
+    }
     public static void main(String[] args){
         
     }
