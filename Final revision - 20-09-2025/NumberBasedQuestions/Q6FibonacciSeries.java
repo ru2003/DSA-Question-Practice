a5122a9bfddfc8da6e255aec9eece14a360cd445
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-28 23:45:36 +0530

    new changes

--- Final revision - 20-09-2025/NumberBasedQuestions/Q6FibonacciSeries.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q6FibonacciSeries.java
@@ -1,11 +1,18 @@
 package NumberBasedQuestions;
 // 6. Generate Fibonacci series.
+import java.util.Scanner;
 public class Q6FibonacciSeries {
 
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Enter the number of series:  ");
          int first = 0;
          int second = 1;
-         
+         int n = 5;
+
+
+
+
     }
     
 }
