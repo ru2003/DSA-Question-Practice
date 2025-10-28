2ef09e30b8fc486fe2c7a242b9de5059875ced43
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-28 12:18:20 +0530

    new changes

--- Final revision - 20-09-2025/NumberBasedQuestions/Q5IsPallinrome.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q5IsPallinrome.java
@@ -7,11 +7,20 @@ public class Q5IsPallinrome {
 
 
     public static boolean checktheNum(int num){
+        int reversed = 0
       while(num != 0){
-        
+        int digit = num %10;
+         reversed = reversed + digit;
+         num = num / 10;
+      }
+      if(num == reversed){
+        return true;
+      }else{
+        return false;
       }
     }
     public static void main(String[] args){
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number: ");
     }
 }
