56abe3c6aecb5f137952e72b35873f38ac24e4a9
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-29 00:41:05 +0530

    new changes

--- Final revision - 20-09-2025/NumberBasedQuestions/Q10CountEvenOddDigit.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q10CountEvenOddDigit.java
@@ -1,5 +1,13 @@
 package NumberBasedQuestions;
-
+//10. Count even and odd digits in a number.
 public class Q10CountEvenOddDigit {
+    public static int countEvenOdd(int number){
+         while (number != 0){
+            
+         }
+    }
+    public static void main(String[] args){
+
+    }
     
 }
