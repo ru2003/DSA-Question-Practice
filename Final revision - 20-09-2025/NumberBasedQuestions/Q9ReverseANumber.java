012b66f2c1407a60a801876537dbe4c079060df7
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-29 00:32:49 +0530

    new changes

--- Final revision - 20-09-2025/NumberBasedQuestions/Q9ReverseANumber.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q9ReverseANumber.java
@@ -1,5 +1,14 @@
 package NumberBasedQuestions;
 //9. Reverse a number.
 public class Q9ReverseANumber {
-    
+    public static void main (String[] args){
+        int num = 11234;
+        int reversed = 0;
+        while(num != 0 ){
+            int digit = num % 10;
+             reversed = reversed *10 + digit;
+             num = num/10;
+        }
+
+    }
 }
