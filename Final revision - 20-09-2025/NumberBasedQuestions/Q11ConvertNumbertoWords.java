b6fb22c213a48b96b8ff61447cd777b75ad52654
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-31 23:43:55 +0530

    new changes

--- Final revision - 20-09-2025/NumberBasedQuestions/Q11ConvertNumbertoWords.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q11ConvertNumbertoWords.java
@@ -6,11 +6,11 @@ public class Q11ConvertNumbertoWords {
     public static void main(String[] args){
          Scanner sc = new Scanner(System.in);
          System.out.println("Enter a number: ");
-         int num  = sc.nextInt(0);
+         int num  = sc.nextInt();
 
 
          int originalNum = num;
-         String result = " ";
+         String result = "";
 
 
          if(num ==0){
@@ -30,7 +30,43 @@ public class Q11ConvertNumbertoWords {
          while(rev > 0){
             int digit = rev % 10;
             
+            switch(digit){
+                case 0:
+                result +=  "zero ";
+                break;
+                case 1:
+                result += "One ";
+                break;
+                case 2:
+                result +=  "two ";
+                  break;
+                case 3:
+                result += "three ";
+                  break;
+                case 4:
+                result +=  "foour ";
+                  break;
+                case 5:
+                result += "five ";
+                  break;
+                case 6:
+                result +=  "six ";
+                  break;
+                case 7:
+                result += "seven ";
+                  break;
+                case 8:
+                result += "eight ";
+                  break;
+                 case 9:
+                result += "Nine ";
+                  break;
+            }
+            rev = rev/10;
          }
+
+         System.out.println("Number " + originalNum);
+         System.out.println("In words: "+ result.trim());
 
          
 
