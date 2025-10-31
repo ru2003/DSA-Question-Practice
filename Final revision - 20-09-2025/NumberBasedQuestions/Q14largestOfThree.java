602f7204b7d6109b5c4d6a66b8b5eccfded6cab7
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-31 17:39:03 +0530

    new changes

--- Final revision - 20-09-2025/NumberBasedQuestions/Q14largestOfThree.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q14largestOfThree.java
@@ -9,6 +9,10 @@ public class Q14largestOfThree {
 
         if(num1 > num2  && num1 > num3){
             System.out.println("nume is the largest.");
+        }else if(num2 > num1 && num2 > num3){
+            System.out.println("num2 is the greates.");
+        }else{
+            System.out.println("num3 is the greatest.");
         }
     }
 }
