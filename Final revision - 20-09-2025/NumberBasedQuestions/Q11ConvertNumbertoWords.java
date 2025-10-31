7b365b5239108426dfb0b5bac25e9b95fa9c2556
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-31 23:33:36 +0530

    new changes

--- Final revision - 20-09-2025/NumberBasedQuestions/Q11ConvertNumbertoWords.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q11ConvertNumbertoWords.java
@@ -17,6 +17,21 @@ public class Q11ConvertNumbertoWords {
             System.out.println("zero");
             return;
          }
+
+         int rev =0;
+         while(num > 0){
+            int digit = num %10;
+            rev = rev*10 + digit;
+            num = num /10;///jispar process krte hai ,like hum num peprocess kr rhe hai toh hum ,usse khi aur store krenge.
+         }
+         
+
+
+         while(rev > 0){
+            int digit = rev % 10;
+            
+         }
+
          
 
 
