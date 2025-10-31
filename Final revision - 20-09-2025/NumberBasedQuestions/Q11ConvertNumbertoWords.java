9c9406fdcd1452008f1946e76178fe6e3b36ebb8
ru2003 <ruchitanchaudhari145@gmail.com>
2025-10-31 17:37:47 +0530

    new changes

--- Final revision - 20-09-2025/NumberBasedQuestions/Q11ConvertNumbertoWords.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q11ConvertNumbertoWords.java
@@ -1,8 +1,18 @@
 package NumberBasedQuestions;
 //11. Convert numbers to words (e.g. 512 → “Five One Two”).
+import java.util.*;
 
 public class Q11ConvertNumbertoWords {
     public static void main(String[] args){
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter a number: ");
+         int num  = sc.nextInt(0);
+
+
+         int originalNum = num;
+         
+
+
 
     }
 }
--- Final revision - 20-09-2025/NumberBasedQuestions/Q14largestOfThree.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q14largestOfThree.java
@@ -1,5 +1,14 @@
 package NumberBasedQuestions;
 //
 public class Q14largestOfThree {
-    
+    public static void main(String[] args){
+
+        int num1 = 6;
+        int num2 = 10;
+        int num3 = 67;
+
+        if(num1 > num2  && num1 > num3){
+            System.out.println("nume is the largest.");
+        }
+    }
 }
