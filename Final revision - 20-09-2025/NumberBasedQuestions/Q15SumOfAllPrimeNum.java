2965c36f6ce81d1e77a8904ad9268e119c3d87d9
ru2003 <ruchitanchaudhari145@gmail.com>
2025-11-01 14:41:37 +0530

    new changes

--- Final revision - 20-09-2025/NumberBasedQuestions/Q15SumOfAllPrimeNum.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q15SumOfAllPrimeNum.java
@@ -2,6 +2,15 @@ package NumberBasedQuestions;
 //15. Sum of all prime numbers in a range.
 import java.util.*;
 public class Q15SumOfAllPrimeNum {
+
+    public static boolean isPrime(int num){
+        for (int i= 2; i< num;i++){
+            if(num % i ==0){
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args){
            
         Scanner sc = new Scanner(System.in);
@@ -9,7 +18,14 @@ public class Q15SumOfAllPrimeNum {
         int start  = sc.nextInt();
         System.out.println("enter the last number: ");
         int last = sc.nextInt();
-        
+          int sum =0;
+        for(int i=start ; i<=last; i++){
+            if(isPrime(i)){
+                sum += i;
+            }
+        }
+        System.out.println("The sum of prime number is: " + sum);
+
 
 
     }
