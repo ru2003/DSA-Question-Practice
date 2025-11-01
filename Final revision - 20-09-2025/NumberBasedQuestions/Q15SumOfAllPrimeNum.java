0bbb51d259ffeeb5afae61fc52bf49a5001fa365
ru2003 <ruchitanchaudhari145@gmail.com>
2025-11-01 14:58:28 +0530

    new changes

--- Final revision - 20-09-2025/NumberBasedQuestions/Q15SumOfAllPrimeNum.java
+++ Final revision - 20-09-2025/NumberBasedQuestions/Q15SumOfAllPrimeNum.java
@@ -4,6 +4,10 @@ import java.util.*;
 public class Q15SumOfAllPrimeNum {
 
     public static boolean isPrime(int num){
+
+        if(num <2 ){
+            return false;
+        }
         for (int i= 2; i< num;i++){
             if(num % i ==0){
                 return false;
