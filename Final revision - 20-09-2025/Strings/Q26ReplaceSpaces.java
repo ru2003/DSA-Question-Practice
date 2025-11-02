ea4bd313b503fa90bd5351d0550cf55cfbd4a95b
ru2003 <ruchitanchaudhari145@gmail.com>
2025-11-02 21:37:28 +0530

    new changes

--- Final revision - 20-09-2025/Strings/Q26ReplaceSpaces.java
+++ Final revision - 20-09-2025/Strings/Q26ReplaceSpaces.java
@@ -4,6 +4,20 @@ import java.util.*;
 public class Q26ReplaceSpaces {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("enter a String: ");
+        String str = sc.nextLine();
+        String result ="";
+        for(int i=0;i<str.length() ; i++){
+            char ch= str.charAt(i);
+/*➡️ ' ' (single quotes) → character literal
+➡️ " " (double quotes) → string literal */
+            if(ch == ' '){
+               result+="%20";
+            }else{
+                result +=ch;
+            }
+        }
+        System.out.println("Original String: "+str);
+        System.out.println("URL encoded string: "+ result);
     }
 }
