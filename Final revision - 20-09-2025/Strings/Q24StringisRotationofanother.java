cacbb0100771653ade2c6e619ed0636b9e8de53f
ru2003 <ruchitanchaudhari145@gmail.com>
2025-11-02 21:18:44 +0530

    new changes

--- Final revision - 20-09-2025/Strings/Q24StringisRotationofanother.java
+++ Final revision - 20-09-2025/Strings/Q24StringisRotationofanother.java
@@ -2,12 +2,30 @@ package Strings;
 //24. Check if a string is a rotation of another.
 import java.util.*;
 public class Q24StringisRotationofanother {
+
+    public static boolean isRotation( String str1,String  str2){
+        if(str1.length() != str2.length()){
+          return false;
+        }
+        
+        //step 2: concatenate s1 wit itself
+        String temp =  str1 + str2;
+
+        //check if s2 contains of temp
+        return temp.contains(str2);
+
+    }
     public static void main(String[] args){
          Scanner sc = new Scanner(System.in);
          System.out.println("enter the first string: ");
          String str1 =  sc.nextLine();
          System.out.println("Enter the second string: ");
+        String str2 = sc.nextLine();
 
-         
+          if(isRotation(str1, str2)){
+            System.out.println(str2+ " is a rotation of  "+str1);
+          }else{
+            System.out.println(str2 + " is not rotation of "+ str1);
+          }
     }
 }
--- Final revision - 20-09-2025/Strings/Q25RemoveVowels.java
+++ Final revision - 20-09-2025/Strings/Q25RemoveVowels.java
@@ -1,5 +1,13 @@
 package Strings;
 //25. Remove all vowels from a string.
+import java.util.*;
 public class Q25RemoveVowels {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String str = sc.nextLine();
+
+        String result = "";
+
+    }
 }
