0c04fb9243287e34b29b0469a1ced747d384d144
ru2003 <ruchitanchaudhari145@gmail.com>
2025-11-02 08:55:12 +0530

    new changes

--- Final revision - 20-09-2025/Strings/Q24StringisRotationofanother.java
+++ Final revision - 20-09-2025/Strings/Q24StringisRotationofanother.java
@@ -1,7 +1,13 @@
 package Strings;
 //24. Check if a string is a rotation of another.
+import java.util.*;
 public class Q24StringisRotationofanother {
     public static void main(String[] args){
-        
+         Scanner sc = new Scanner(System.in);
+         System.out.println("enter the first string: ");
+         String str1 =  sc.nextLine();
+         System.out.println("Enter the second string: ");
+
+         
     }
 }
