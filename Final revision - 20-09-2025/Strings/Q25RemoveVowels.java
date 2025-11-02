f7bf9e3dee6a0bc35e3104400a915209da1ee40d
ru2003 <ruchitanchaudhari145@gmail.com>
2025-11-02 21:33:10 +0530

    new changes

--- Final revision - 20-09-2025/Strings/Q25RemoveVowels.java
+++ Final revision - 20-09-2025/Strings/Q25RemoveVowels.java
@@ -9,5 +9,16 @@ public class Q25RemoveVowels {
 
         String result = "";
 
+        for (int i=0; i<=str.length()-1; i++){
+            char ch = str.charAt(i);
+
+            if(!(ch == 'a' ||ch == 'e' ||ch == 'i' ||ch == 'o' ||ch == 'u' ||ch == 'A' ||ch == 'E' ||ch == 'I' ||ch == 'O' ||ch == 'U'  )){
+                result += ch;
+            }
+
+        }
+        System.out.println("Original String: " + str);
+        System.out.println("After removing the vowels from the string: "+ result);
+
     }
 }
--- Final revision - 20-09-2025/Strings/Q26ReplaceSpaces.java
+++ Final revision - 20-09-2025/Strings/Q26ReplaceSpaces.java
@@ -1,5 +1,9 @@
 package Strings;
 //26. Replace spaces with “%20” (URL encoding).
+import java.util.*;
 public class Q26ReplaceSpaces {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+    }
 }
