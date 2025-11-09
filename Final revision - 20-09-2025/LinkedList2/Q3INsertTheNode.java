9d25fc8a87b78a48ec3f566e3efb3a184d5771c7
ru2003 <ruchitanchaudhari145@gmail.com>
2025-11-09 18:37:04 +0530

    new addition

--- Final revision - 20-09-2025/LinkedList2/Q3INsertTheNode.java
+++ Final revision - 20-09-2025/LinkedList2/Q3INsertTheNode.java
@@ -1,7 +1,26 @@
 package LinkedList2;
 import java.util.*;
+/*3. Insert a Node at the Beginning / End / Given Position */
 public class Q3INsertTheNode {
+
+
+
     public static void main(String[] args){
+         LinkedList list = new LinkedList();
+         
+         list.insertAtEnd(10);
+         list.insertAtEnd(20);
+         list.insertAtEnd(30);
+         list.display();
+
+         list.insertAtBEginning(5);
+         list.insertAtBeginning(2);
+         list.display();
+
+         list.insertAtPosition(99);
+         list.insertAtposition(78);
+         list.display();
+         
 
     }
 }
