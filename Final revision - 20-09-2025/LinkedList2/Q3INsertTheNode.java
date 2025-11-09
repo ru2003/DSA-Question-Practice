88b4dc421dcde6077b0169ec872a3df1337d5c1e
ru2003 <ruchitanchaudhari145@gmail.com>
2025-11-09 18:55:56 +0530

    new addition

--- Final revision - 20-09-2025/LinkedList2/Q3INsertTheNode.java
+++ Final revision - 20-09-2025/LinkedList2/Q3INsertTheNode.java
@@ -21,8 +21,18 @@ public class Q3INsertTheNode {
         head = newNode;
         }
 
-        public static void insertAtEnd(int data){
-            
+        public  void insertAtEnd(int data){
+            Node newNode = new Node(data);
+            if(head == null){
+                head = newNode;
+                return ;
+            }
+
+            Node temp = head;
+            while (temp.next != null){
+                temp  = temp.next;
+            }
+            temp.next = newNode;
         }
 
     }
