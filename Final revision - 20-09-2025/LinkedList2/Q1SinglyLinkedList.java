37090df810544648d1ace0ac5ead8496ba34f12a
ru2003 <ruchitanchaudhari145@gmail.com>
2025-11-09 14:41:12 +0530

    new changes

--- Final revision - 20-09-2025/LinkedList2/Q1SinglyLinkedList.java
+++ Final revision - 20-09-2025/LinkedList2/Q1SinglyLinkedList.java
@@ -3,6 +3,37 @@ package LinkedList2;
 
 public class Q1SinglyLinkedList {
 
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+
+    class LinkedList{
+        Node head;
+
+     //Insert at end 
+     public void insertAtEnd(int data){
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = newNode;
+            return ;
+        }
+        Node temp = head;
+        while(temp.next == null){
+            temp = temp.next;
+        }
+        temp.next = newNode;
+     }
+
+    }
+
     public static void main(String[] args){
         LinkedList list = new LinkedList();
 
