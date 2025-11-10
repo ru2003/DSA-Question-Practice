fafe54c91339b7b9b87880452f27fdc58c5452ff
ru2003 <ruchitanchaudhari145@gmail.com>
2025-11-10 11:31:11 +0530

    new addition

--- Final revision - 20-09-2025/LinkedList2/Q4DeleteAnode.java
+++ Final revision - 20-09-2025/LinkedList2/Q4DeleteAnode.java
@@ -10,7 +10,41 @@ public class Q4DeleteAnode {
             this.next = null;
         }
     }
-    public static void main(String[] args){
 
+    class LinkedList{
+        Node head;
+
+        //delete a node from beginning
+        public void deleteAtbeginning(){
+            if(head == null){
+                System.out.println("List is empty");
+                return ;
+            }
+            head = head.next;
+
+            //delete from end
+            public void deleteAtEnd() {
+                if(head == null){
+                    System.out.println("List is empty.");
+                    return ;
+
+                }
+
+                if(head.next == null){
+                    head = null;
+                    return ;
+                }
+
+                Node temp = head;
+                while(temp.next.next != null ){
+                    temp = temp.next;
+                }
+                temp.next = null;
+            }
+        }
+
+    }
+    public static void main(String[] args){
+         
     }
 }
