b8b14111a99fbcb2e31de6962945b2291560aa70
ru2003 <ruchitanchaudhari145@gmail.com>
2025-11-10 10:19:50 +0530

    new addition

---
+++ Final revision - 20-09-2025/LinkedList2/Q4DeleteAnode.java
@@ -0,0 +1,16 @@
+package LinkedList2;
+//4. Delete a Node at Beginning / End / Given Position
+public class Q4DeleteAnode {
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String[] args){
+
+    }
+}
