784de99a23aa43987b1b7b482613d809908bd2e3
ru2003 <ruchitanchaudhari145@gmail.com>
2025-11-11 10:50:21 +0530

    new addition

---
+++ Final revision - 20-09-2025/LinkedList2/Q5SearchforLinkedList.java
@@ -0,0 +1,33 @@
+package LinkedList2;
+
+public class Q5SearchforLinkedList {
+    /*
+     * 🪜 1️⃣ Brute Force Approach (Manual Traversal)
+💡 Idea
+
+Start from head
+
+Traverse each node one by one
+
+Compare node’s data with the target value
+
+If found → print position
+
+Else → after traversal, print “Not Found”
+     */
+
+
+
+     class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+     }
+    public static void  main(String[] args){
+         
+    }
+}
