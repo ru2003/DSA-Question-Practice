0f8bdc737d61797199732c8a26716b02f617a29e
ru2003 <ruchitanchaudhari145@gmail.com>
2025-11-11 12:49:56 +0530

    new addition

--- Final revision - 20-09-2025/LinkedList2/Q5OOPSApproch.java
+++ Final revision - 20-09-2025/LinkedList2/Q5OOPSApproch.java
@@ -14,7 +14,26 @@ public class Q5OOPSApproch {
 
     static class Linkedlist{
         Node head;
-        
+
+        public void deleteAtEnd(int data){
+            Node newNode = new Node(data);
+
+           if(head == null){
+            head = newNode;
+            return;
+            } 
+            Node temp  = head;
+            while(temp != null){
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+
+        //display the linked list
+        public void display(){
+            
+        }
+
 
     }
     public static void main(String[] args){
