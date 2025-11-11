7aa173fdc9d10a357aeeaf041a573614a8d42001
ru2003 <ruchitanchaudhari145@gmail.com>
2025-11-11 12:43:56 +0530

    new addition

--- Final revision - 20-09-2025/LinkedList2/Q5OOPSApproch.java
+++ Final revision - 20-09-2025/LinkedList2/Q5OOPSApproch.java
@@ -1,6 +1,22 @@
 package LinkedList2;
 
 public class Q5OOPSApproch {
+
+    static class Node{
+        int data;
+        Node next;
+        
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class Linkedlist{
+        Node head;
+        
+
+    }
     public static void main(String[] args){
         
     }
