8e1ff17e52d6b867de267d20f70ea2533a606cc0
ru2003 <ruchitanchaudhari145@gmail.com>
2025-11-12 10:42:09 +0530

    new addition

--- Final revision - 20-09-2025/Arrays/Q11MaximumMinimumELement.java
+++ Final revision - 20-09-2025/Arrays/Q11MaximumMinimumELement.java
@@ -14,7 +14,7 @@ public class Q11MaximumMinimumELement {
          
         int maximum = 0;
         for(int i=0 ; i<=arr.length-1;i++){
-          
+          if()
         }
 
     }
--- Final revision - 20-09-2025/LinkedList2/Q6LengthOFLinkedList.java
+++ Final revision - 20-09-2025/LinkedList2/Q6LengthOFLinkedList.java
@@ -3,7 +3,7 @@ package LinkedList2;
 public class Q6LengthOFLinkedList {
 
 
-    class Node{
+    static class Node{
         int data;
         Node next;
 
@@ -14,15 +14,63 @@ public class Q6LengthOFLinkedList {
 
     }
 
-    class linkedList{
+     static class linkedList{
         Node head;
 
         void insertAtEnd(int data){
             Node newNode = new Node( data);
 
-        }
+            if(head == null){
+                head = newNode;
+                return ;
+            }
+
+            Node temp = head;
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = newNode;
+         }
+
+
+        
+         //brute force approch to measure the length of te linked list
+         int getLength(){
+            int count = 0;
+
+            Node temp  = head;
+            while(temp != null){
+                count++;
+                temp = temp.next;
+                
+            }
+            return count;
+
+         }
+
+         void display() {
+            Node temp = head;
+            while(temp != null){
+                System.out.print(temp.data +" ");
+                temp = temp.next;
+            }
+            System.out.println();
+         }
+
     }
     public static void main(String[] args){
+        linkedList list = new linkedList();
         
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+        list.insertAtEnd(40);
+        list.display();
+
+        
+        System.out.println("Length of Linkedlist " + list.getLength());
+
+
+
     }
 }
