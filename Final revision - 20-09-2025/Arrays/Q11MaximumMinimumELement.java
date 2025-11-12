b1d405be11420b39ed81edefd8942e6900cbc6a0
ru2003 <ruchitanchaudhari145@gmail.com>
2025-11-12 09:58:59 +0530

    new addition

---
+++ Final revision - 20-09-2025/Arrays/Q11MaximumMinimumELement.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+//
+//11. Find the maximum and minimum element in an array.
+public class Q11MaximumMinimumELement {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of Element u want to add in the array: ");
+        int num  = sc.nextInt();
+        System.out.println("enter the element that u want to add in the array: ");
+        int[] arr = new int[num];
+        for(int i=0; i<num ;i++){
+            arr[i] = sc.nextInt();
+        }
+         
+        int maximum = 0;
+        for(int i=0 ; i<=arr.length-1;i++){
+          
+        }
+
+    }
+    
+}
--- Final revision - 20-09-2025/LinkedList2/Q6LengthOFLinkedList.java
+++ Final revision - 20-09-2025/LinkedList2/Q6LengthOFLinkedList.java
@@ -1,6 +1,27 @@
 package LinkedList2;
 
 public class Q6LengthOFLinkedList {
+
+
+    class Node{
+        int data;
+        Node next;
+
+     Node(int data) {
+        this.data = data;
+        this.next = null;
+     }
+
+    }
+
+    class linkedList{
+        Node head;
+
+        void insertAtEnd(int data){
+            Node newNode = new Node( data);
+
+        }
+    }
     public static void main(String[] args){
         
     }
