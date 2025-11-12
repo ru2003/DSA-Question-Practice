4dee4004ac2515353aa8dc0dbbb4dff277a4be6a
ru2003 <ruchitanchaudhari145@gmail.com>
2025-11-12 11:56:13 +0530

    new addition

--- Final revision - 20-09-2025/LinkedList2/Q7printtheLinkedList.java
+++ Final revision - 20-09-2025/LinkedList2/Q7printtheLinkedList.java
@@ -2,7 +2,7 @@ package LinkedList2;
 
 public class Q7printtheLinkedList {
 
-    class Node{
+    static class Node{
         int data;
         Node next;
 
@@ -11,8 +11,38 @@ public class Q7printtheLinkedList {
             this.next = null;
         }
     }
-    public static void main(String[] args){
 
+    static class LinkedList{
+      Node head; 
+        public void insertAtEnd(int data){
+            Node newNode = new Node(data);
+
+            if(head == null){
+                 head = newNode;
+            }
+            Node temp  = head;
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+
+        public void display(){
+            Node temp  = head;
+            while(temp != null){
+                System.out.print("Printing the Linkedlist: " + temp.data);
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+          LinkedList list = new LinkedList();
+
+          list.insertAtEnd(10);
+           list.insertAtEnd(10);
+            list.insertAtEnd(10);
+             list.insertAtEnd(10);
 
     }
 }
