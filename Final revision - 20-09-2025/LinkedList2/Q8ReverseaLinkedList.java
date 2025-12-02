9b5fb313370d85ed6e9eaaefb4ce2ea74fc4cff8
ru2003 <ruchitanchaudhari145@gmail.com>
2025-12-02 13:26:45 +0530

    new addition

--- Final revision - 20-09-2025/LinkedList2/Q8ReverseaLinkedList.java
+++ Final revision - 20-09-2025/LinkedList2/Q8ReverseaLinkedList.java
@@ -1,5 +1,20 @@
 package LinkedList2;
-
+//reverse a linked list ==> 3 pointer , 4 step process.
+// Time Complexity : O(n)  &&& Space Complexity : O(1)
 public class Q8ReverseaLinkedList {
-    
+
+     class Node{
+       int data;
+       Node next;
+
+       Node(int data){
+         this.data = data;
+         this.next = null;
+       }
+
+    }
+    public static void main(String[] args){
+
+        
+    }
 }
