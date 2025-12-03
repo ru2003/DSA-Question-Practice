2f42c51afb670ab35a9200c8e8f6badffd7ba1cf
ru2003 <ruchitanchaudhari145@gmail.com>
2025-12-03 07:54:30 +0530

    new changes

--- Final revision - 20-09-2025/LinkedList2/Q9MiddleOFLinkedList.java
+++ Final revision - 20-09-2025/LinkedList2/Q9MiddleOFLinkedList.java
@@ -23,8 +23,31 @@ public class Q9MiddleOFLinkedList {
                 return;
             }
 
-            Node temp = head;
+         
+        public Node findtheMiddle(){
+            if(head == null){
+                System.out.println("LnkedList is empty. ");
+                return -1;
 
+            }
+            
+            Node temp = head;
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = newNode;
+
+            Node slow = head;
+            Node fast = head;
+
+            while(fast != null && fast.next != null){
+                slow = slow.next;
+                fast = fast.next.next;
+
+            }
+            return slow.data;//slow is at the middle
+
+        
         }
         
     }
