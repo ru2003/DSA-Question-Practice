b52d7b51e319c089bfb9fd11b79c27542290c76a
ru2003 <ruchitanchaudhari145@gmail.com>
2025-12-03 07:29:17 +0530

    new changes

--- Final revision - 20-09-2025/LinkedList2/Q9MiddleOFLinkedList.java
+++ Final revision - 20-09-2025/LinkedList2/Q9MiddleOFLinkedList.java
@@ -1,7 +1,17 @@
 package LinkedList2;
 
 public class Q9MiddleOFLinkedList {
+
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
     public static void main(String[] args){
-        
+
     }
 }
