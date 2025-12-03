cf55845fc0b25e2faa7713dc98f73c86e31b816a
ru2003 <ruchitanchaudhari145@gmail.com>
2025-12-03 12:18:13 +0530

    new addition

--- Final revision - 20-09-2025/LinkedList2/Q10PallindromeOrNot.java
+++ Final revision - 20-09-2025/LinkedList2/Q10PallindromeOrNot.java
@@ -1,7 +1,54 @@
 package LinkedList2;
+/* class Node { ... }
+class LinkedList {
 
+    Node head;
+
+    insertAtEnd() { ... }
+
+    reverse() { ... }
+
+    isPalindrome() { ... }
+
+    display() { ... }
+
+}
+*/
 public class Q10PallindromeOrNot {
+     
+
+    static class Node{
+          int data;
+          Node next;
+
+          Node(int data){
+             this.data = data;
+             this.next = null;
+          }
+    }
+
+    //linkedlist
+    static class LinkedList{
+        Node head;
+
+        public void insertAtEnd(int data){
+            Node newNode = new Node(data);
+            if(head == null){
+                head = newNode;
+                return ;
+            }
+
+            Node temp  = head;
+            while(temp != null){
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+
+     
     public static void main(String [] args){
-        
+
     }
 }
