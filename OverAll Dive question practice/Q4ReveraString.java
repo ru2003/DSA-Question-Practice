92cee86a672469538149f072d787814a46331d21
ru2003 <ruchitanchaudhari145@gmail.com>
2025-12-21 20:48:51 +0530

    new changes

--- OverAll Dive question practice/Q4ReveraString.java
+++ OverAll Dive question practice/Q4ReveraString.java
@@ -1,6 +1,11 @@
 public class Q4ReveraString{
     public static void main(String[] args){
        String ruchita = "Hello";
-       String reversed =""; 
+       String reversed ="";
+       
+       for (int i=ruchita.length()-1 ;i>=0;i--){
+        reversed += ruchita.charAt(i);
+       }
+       System.out.println("Reversed String is: " + reversed);
     }
 }
