ef9c5aff6d795cbb13b303de2393981a5e63d8d2
ru2003 <ruchitanchaudhari145@gmail.com>
2025-12-21 20:28:38 +0530

    new changes

--- OverAll Dive question practice/Q2moveAllZeros.java
+++ OverAll Dive question practice/Q2moveAllZeros.java
@@ -7,19 +7,25 @@ public class Q2moveAllZeros {
 
          for(int num : arr){
             if(num != 0 ){
-                arr[index++] = arr[num];
+                arr[index++] = num;
                 /*
                 nums[i] ki value ko nums[index] par copy karo
-                
-                
-                
-                */
+                 
+               */
             }
-
          }
 
-         while(index < arr.length){
+         while(index <= arr.length-1){
             arr[index++] = 0;
+         }
+
+
+
+
+          System.out.println("array after moving al the zeros at end");
+         for(int num : arr){
+           
+            System.out.print(num + " ");
          }
 
     }
---
+++ OverAll Dive question practice/Q3MissingNumber.java
@@ -0,0 +1,14 @@
+//Find missing number (1 to N)
+
+public class Q3MissingNumber {
+    public static void main(String[] args){
+/*N = 5
+arr = {1, 2, 4, 5}
+Missing number = 3
+ */
+
+int[] arr = {1,2,4,5};
+
+
+    }
+}
