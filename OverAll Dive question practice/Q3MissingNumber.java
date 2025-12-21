5748e3a7670d78a3a9495ceb8f0e54dfe8c632e9
ru2003 <ruchitanchaudhari145@gmail.com>
2025-12-21 20:33:50 +0530

    new changes

--- OverAll Dive question practice/Q3MissingNumber.java
+++ OverAll Dive question practice/Q3MissingNumber.java
@@ -8,7 +8,18 @@ Missing number = 3
  */
 
 int[] arr = {1,2,4,5};
+int N = 5;
+int expectedsum = N*(N+1) /2;
+int actualsum =0;
+ 
+   for(int i=0;i<arr.length;i++){
+   actualsum += arr[i];
+}
 
+int missingNumber = expectedsum - actualsum;
+System.out.println("Missing Number: " + missingNumber);
 
     }
+
+    
 }
