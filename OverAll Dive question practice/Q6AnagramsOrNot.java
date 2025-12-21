1aa491aea564c5dc1588fe4cfd9f3779ce67f490
ru2003 <ruchitanchaudhari145@gmail.com>
2025-12-21 21:15:20 +0530

    new changes

--- OverAll Dive question practice/Q6AnagramsOrNot.java
+++ OverAll Dive question practice/Q6AnagramsOrNot.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Q6AnagramsOrNot {
     public static void main(String[] args){
 
@@ -11,5 +12,20 @@ public class Q6AnagramsOrNot {
         System.out.println("No Anagram.");
         return;
       }
+
+      char arr1[] = str1.toCharArray();
+      char arr2[] = str2.toCharArray();
+      
+      Arrays.sort(arr1);
+      Arrays.sort(arr2);
+
+      if(Arrays.equals(arr1,arr2 )){
+System.out.println("the Strings are anagrams.");
+      }else{
+        System.out.println("Strings are not anagrams.");
+      }
+
+
+
     }
 }
