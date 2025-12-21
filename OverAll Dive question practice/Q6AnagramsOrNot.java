1759f539107b8fcd0ff0deccf0958cefadf8784f
ru2003 <ruchitanchaudhari145@gmail.com>
2025-12-21 21:08:01 +0530

    new changes

--- OverAll Dive question practice/Q6AnagramsOrNot.java
+++ OverAll Dive question practice/Q6AnagramsOrNot.java
@@ -3,6 +3,13 @@ public class Q6AnagramsOrNot {
 
         String str1 = "silent";
         String str2 = "listen";
+        
+      str1 = str1.replaceAll("\\s" ,"").toLowerCase();
+      str2 = str2.replaceAll("\\s" , "").toLowerCase();
 
+      if(str1.length() != str2.length()){
+        System.out.println("No Anagram.");
+        return;
+      }
     }
 }
