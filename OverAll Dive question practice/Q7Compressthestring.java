77365fec1bd48dd61d1f4c0d7d157fb03eb6f147
ru2003 <ruchitanchaudhari145@gmail.com>
2025-12-21 21:46:25 +0530

    new changes

--- OverAll Dive question practice/Q7Compressthestring.java
+++ OverAll Dive question practice/Q7Compressthestring.java
@@ -17,5 +17,19 @@ public class Q7Compressthestring {
         StringBuilder result = new StringBuilder();
         int count = 1;
 
+        for(int i=0;i<str.length()-1 ;i++){
+            if(str.charAt(i) == str.charAt(i+1)){
+                count++;
+            }else{
+                result.append(str.charAt(i)).append(count);
+                count =1;
+            }
+        }
+
+        //append the last character and it count
+        result.append(str.charAt(str.length()-1)).append(count);
+        return result.toString();
+        
+
     }
 }
