542e74bcdf71edc1cf35b2757040329ed5803ce5
ru2003 <ruchitanchaudhari145@gmail.com>
2025-12-21 14:48:29 +0530

    new changes

--- OverAll Dive question practice/Q2moveAllZeros.java
+++ OverAll Dive question practice/Q2moveAllZeros.java
@@ -2,6 +2,42 @@
 
 public class Q2moveAllZeros {
     public static void main(String[] args){
-        
+        int index = 0;
+         int[] arr = {3,4,0,0,6,5,10};
+
+         for(int num : arr){
+            if(num != 0 ){
+                arr[index++] = arr[num];
+                /*
+                nums[i] ki value ko nums[index] par copy karo
+                
+                
+                
+                */
+            }
+
+         }
+
+         while(index < arr.length){
+            arr[index++] = 0;
+         }
+
     }
 }
+
+
+//⏱️ Time & Space Complexity
+/* 
+Time Complexity:
+
+One loop to read → O(n)
+
+One loop to fill zeros → O(n)
+
+Total → O(n)
+
+Space Complexity:
+
+No extra array used
+
+Only variables → O(1)*/
