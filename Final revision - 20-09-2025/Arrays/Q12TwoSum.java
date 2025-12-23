25d53c2c8d6e936d26083e384c43e71f3215d054
ru2003 <ruchitanchaudhari145@gmail.com>
2025-12-23 22:46:23 +0530

    new changes

---
+++ Final revision - 20-09-2025/Arrays/Q12TwoSum.java
@@ -0,0 +1,8 @@
+public class Q12TwoSum {
+    public static void main(String[] args){
+
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        
+    }
+}
--- Final revision - 20-09-2025/Strings/Q15FirstlettertoUppercase.java
+++ Final revision - 20-09-2025/Strings/Q15FirstlettertoUppercase.java
@@ -3,6 +3,7 @@ package Strings;
 //15. Convert each word’s first letter to uppercase (title case).
 import java.util.*;
 
+
 public class Q15FirstlettertoUppercase {
     public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
