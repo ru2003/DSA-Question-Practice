fd12756922b86136dfa723c13e759a422b927494
ru2003 <ruchitanchaudhari145@gmail.com>
2025-12-23 23:05:31 +0530

    new changes

--- Final revision - 20-09-2025/OOPS/Inheritance/multilevel.java
+++ Final revision - 20-09-2025/OOPS/Inheritance/multilevel.java
@@ -1,5 +1,16 @@
 package OOPS.Inheritance;
+/*✅ Question 1: Multilevel Inheritance
+📝 Problem:
 
+Create a class hierarchy:
+
+Employee
+
+Developer
+
+BackendDeveloper
+
+Each class should add one method. */
 public class multilevel {
     public static void main(String[] args){
         
